fcfb6704473e301909c23095358ad27481ac9e57
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-08-20 21:00:28 +0000

    part 1 of hopefully 2

--- modules/users/sidebar.php
+++ modules/users/sidebar.php
@@ -10,10 +10,32 @@
 if (!defined('IN_ACP3') && !defined('IN_ADM'))
 	exit;
 
-if (defined('IN_ADM'))
-	$tpl->assign('uri', uri('acp/users/login'));
-elseif (defined('IN_ACP3'))
-	$tpl->assign('uri', uri('users/login'));
+if (defined('IS_USER')) {
+	// Aktive Module einholen
+	$active_modules = $modules->active_modules();
+	$nav_mods = array();
 
-$field = $tpl->fetch('users/sidebar.html');
+	foreach ($active_modules as $name => $dir) {
+		if ($modules->check($dir, 'adm_list')) {
+			if ($dir == 'system') {
+				$tpl->assign('access_system', true);
+			} elseif ($dir == 'home') {
+				$tpl->assign('access_home', true);
+			} else {
+				$nav_mods[$name]['name'] = $name;
+				$nav_mods[$name]['dir'] = $dir;
+			}
+		}
+	}
+	$tpl->assign('nav_mods', $nav_mods);
+
+	$tpl->assign('users_sidebar', $tpl->fetch('users/sidebar_modules.html'));
+} else {
+	if (defined('IN_ADM'))
+		$tpl->assign('uri', uri('acp/users/login'));
+	elseif (defined('IN_ACP3'))
+		$tpl->assign('uri', uri('users/login'));
+
+	$tpl->assign('users_sidebar', $tpl->fetch('users/sidebar_login.html'));
+}
 ?>
