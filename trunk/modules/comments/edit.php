365162726326b718335b68922dbe8ed3a1224a0c
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-08-23 12:28:21 +0000

    it's now possible to use the breadcrumb::assign method inside the ACP

--- trunk/modules/comments/edit.php
+++ trunk/modules/comments/edit.php
@@ -17,7 +17,15 @@ if (!empty($modules->id) && $db->select('id', 'comments', 'id = \'' . $modules->
 	if (!isset($_POST['submit']) || isset($error_msg)) {
 		$tpl->assign('error_msg', isset($error_msg) ? $error_msg : '');
 
-		$comment = $db->select('name, message', 'comments', 'id = \'' . $modules->id . '\'');
+		$comment = $db->select('name, message, module', 'comments', 'id = \'' . $modules->id . '\'');
+
+		$comment[0]['module'] = $db->escape($comment[0]['module'], 3);
+
+		$breadcrumb->assign(lang('comments', 'comments'), uri('acp/comments'));
+		$breadcrumb->assign(lang($comment[0]['module'], $comment[0]['module']), uri('acp/comments/adm_list/module_' . $comment[0]['module']));
+		$breadcrumb->assign(lang('comments', 'edit'));
+
+		unset($comment[0]['module']);
 
 		$tpl->assign('form', isset($form) ? $form : $comment[0]);
 
