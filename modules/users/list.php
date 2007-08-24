92c39e17d67c668928245e7f3378358e4e05ec66
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-08-24 17:16:30 +0000

    a little fix

--- modules/users/list.php
+++ modules/users/list.php
@@ -3,15 +3,13 @@ if (!defined('IN_ACP3'))
 	exit;
 
 $users = $db->select('id, name, mail', 'users', 0, 'name ASC, id ASC', POS, CONFIG_ENTRIES);
-$c_users = count($users);
+$all_users = $db->select('id', 'users', 0, 0, 0, 0, 1);
 
-if ($c_users > 0) {
-	$tpl->assign('pagination', pagination($db->select('id', 'users', 0, 0, 0, 0, 1)));
-
+if (count($users) > 0) {
+	$tpl->assign('pagination', pagination($all_users));
 	$tpl->assign('users', $users);
 }
-
-$tpl->assign('LANG_users_found', sprintf(lang('users', 'users_found'), $c_users));
+$tpl->assign('LANG_users_found', sprintf(lang('users', 'users_found'), $all_users));
 
 $content = $tpl->fetch('users/list.html');
 ?>
