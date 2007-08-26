d4c1e01aaa5fa7d840244d52f638c538216b4052
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-08-26 14:42:13 +0000

    - added the smarty function "icon", so that it is easier to include the crystal project icons - optimised two SQL-Queries, to use fewer queries

--- modules/users/adm_list.php
+++ modules/users/adm_list.php
@@ -13,16 +13,12 @@ if (!defined('IN_ADM'))
 if (isset($_POST['entries']) || isset($modules->gen['entries'])) {
 	include 'modules/users/entry.php';
 } else {
-	$users = $db->select('id, nickname, access, mail', 'users', 0, 'nickname ASC', POS, CONFIG_ENTRIES);
+	$users = $db->select('u.id, u.nickname, u.mail, a.name AS access', 'users AS u, ' . CONFIG_DB_PRE . 'access AS a', 'u.access = a.id', 'u.nickname ASC', POS, CONFIG_ENTRIES);
 	$c_users = count($users);
 
 	if ($c_users > 0) {
 		$tpl->assign('pagination', pagination($db->select('id', 'users', 0, 0, 0, 0, 1)));
 
-		for ($i = 0; $i < $c_users; $i++) {
-			$access_name = $db->select('name', 'access', 'id = \'' . $users[$i]['access'] . '\'');
-			$users[$i]['access'] = $access_name[0]['name'];
-		}
 		$tpl->assign('users', $users);
 	}
 
