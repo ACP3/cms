0a1725c67e05d0fcf8ad67109fa788912393de78
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-09-15 11:04:21 +0000

    refactored the dropdown menues for the publication period feature

--- trunk/modules/files/edit.php
+++ trunk/modules/files/edit.php
@@ -17,21 +17,9 @@ if (!empty($modules->id) && $db->select('id', 'files', 'id = \'' . $modules->id 
 	if (!isset($_POST['submit']) || isset($errors) && is_array($errors)) {
 		$dl = $db->select('start, end, category_id, file, size, link_title, text', 'files', 'id = \'' . $modules->id . '\'');
 		$dl[0]['text'] = $db->escape($dl[0]['text'], 3);
-		// Datum
-		$start_date = explode('.', date_aligned(1, $dl[0]['start'], 'j.n.Y.G.i'));
-		$end_date = explode('.', date_aligned(1, $dl[0]['end'], 'j.n.Y.G.i'));
-
 		// Datumsauswahl
-		$tpl->assign('start_day', date_dropdown('day', 'start_day', 'start_day', $start_date[0]));
-		$tpl->assign('start_month', date_dropdown('month', 'start_month', 'start_month', $start_date[1]));
-		$tpl->assign('start_year', date_dropdown('year', 'start_year', 'start_year', $start_date[2]));
-		$tpl->assign('start_hour', date_dropdown('hour', 'start_hour', 'start_hour', $start_date[3]));
-		$tpl->assign('start_min', date_dropdown('min', 'start_min', 'start_min', $start_date[4]));
-		$tpl->assign('end_day', date_dropdown('day', 'end_day', 'end_day', $end_date[0]));
-		$tpl->assign('end_month', date_dropdown('month', 'end_month', 'end_month', $end_date[1]));
-		$tpl->assign('end_year', date_dropdown('year', 'end_year', 'end_year', $end_date[2]));
-		$tpl->assign('end_hour', date_dropdown('hour', 'end_hour', 'end_hour', $end_date[3]));
-		$tpl->assign('end_min', date_dropdown('min', 'end_min', 'end_min', $end_date[4]));
+		$tpl->assign('start_date', publication_period('start', $dl[0]['start']));
+		$tpl->assign('end_date', publication_period('end', $dl[0]['end']));
 
 		$unit = trim(strrchr($dl[0]['size'], ' '));
 
