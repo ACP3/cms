a5c7f13c3cfed8b3b634c07bfff7c5807b1ed301
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2007-12-30 18:23:11 +0000

    Merged the recent changes from the trunk - Beta 9 is near

--- tags/acp3_400_b9/update.php
+++ tags/acp3_400_b9/update.php
@@ -44,7 +44,7 @@ if (is_writable($path))	{
 	// Konfigurationsdatei in ein Array schreiben
 	$config = file($path);
 	$entries_to_change = array(
-		'define(\'CONFIG_VERSION\', \'' . CONFIG_VERSION . '\');' => 'define(\'CONFIG_VERSION\', \'4.0b9 SVN\');',
+		'define(\'CONFIG_VERSION\', \'' . CONFIG_VERSION . '\');' => 'define(\'CONFIG_VERSION\', \'4.0b9\');',
 	);
 
 	foreach ($config as $c_key => $c_value) {
