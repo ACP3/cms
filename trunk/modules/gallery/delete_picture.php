858c4f3e84bcfd59b56b3bd49d10203a3d72ba95
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-03-10 18:27:53 +0000

    part 4 of x

---
+++ trunk/modules/gallery/delete_picture.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Gallery
+ *
+ * @author Goratsch Webdesign
+ * @package ACP3
+ * @subpackage Modules
+ */
+
+if (!defined('IN_ADM'))
+	exit;
+
+if (isset($_POST['entries']) && is_array($_POST['entries']))
+	$entries = $_POST['entries'];
+elseif (isset($modules->gen['entries']) && preg_match('/^([\d|]+)$/', $modules->gen['entries']))
+	$entries = $modules->gen['entries'];
+
+if (is_array($entries)) {
+	$marked_entries = '';
+	foreach ($entries as $entry) {
+		$marked_entries.= $entry . '|';
+	}
+	$content = combo_box(lang('gallery', 'confirm_picture_delete'), uri('acp/gallery/delete_picture/entries_' . $marked_entries), uri('acp/gallery/edit_gallery/id_' . $modules->id));
+} elseif (preg_match('/^([\d|]+)$/', $entries) && isset($modules->gen['confirmed'])) {
+	$marked_entries = explode('|', $entries);
+	$bool = 0;
+	foreach ($marked_entries as $entry) {
+		if (!empty($entry) && $validate->is_number($entry) && $db->select('id', 'galpics', 'id = \'' . $entry . '\'', 0, 0, 0, 1) == '1')
+		// Datei ebenfalls löschen
+		$file = $db->select('file', 'galpics', 'id = \'' . $entry . '\'');
+		if (is_file('uploads/gallery/' . $file[0]['file'])) {
+			unlink('uploads/gallery/' . $file[0]['file']);
+		}
+
+		// Galerie Cache löschen
+		$bool = $db->delete('galpics', 'id = \'' . $entry . '\'');
+	}
+	$content = combo_box($bool ? lang('gallery', 'picture_delete_success') : lang('gallery', 'picture_delete_error'), uri('acp/gallery'));
+} else {
+	redirect('errors/404');
+}
+?>
--- trunk/modules/gallery/edit_gallery.php
+++ trunk/modules/gallery/edit_gallery.php
@@ -17,7 +17,31 @@ if (!empty($modules->id) && $db->select('id', 'gallery', 'id = \'' . $modules->i
 	$breadcrumb->assign($gallery[0]['name']);
 
 	if (isset($_POST['submit'])) {
-		include 'modules/gallery/entry.php';
+		$form = $_POST['form'];
+
+		if (!$validate->date($form))
+			$errors[] = lang('common', 'select_date');
+		if (strlen($form['name']) < 3)
+			$errors[] = lang('gallery', 'type_in_gallery_name');
+
+		if (isset($errors)) {
+			combo_box($errors);
+		} else {
+			$start_date = date_aligned(3, array($form['start_hour'], $form['start_min'], 0, $form['start_month'], $form['start_day'], $form['start_year']));
+			$end_date = date_aligned(3, array($form['end_hour'], $form['end_min'], 0, $form['end_month'], $form['end_day'], $form['end_year']));
+
+			$update_values = array(
+				'start' => $start_date,
+				'end' => $end_date,
+				'name' => $db->escape($form['name']),
+			);
+
+			$bool = $db->update('gallery', $update_values, 'id = \'' . $modules->id . '\'');
+
+			$cache->create('gallery_pics_id_' . $modules->id, $db->query('SELECT g.name, p.id FROM ' . CONFIG_DB_PRE . 'gallery g LEFT JOIN ' . CONFIG_DB_PRE . 'galpics p ON g.id = \'' . $modules->id . '\' AND p.gallery_id = \'' . $modules->id . '\' ORDER BY p.pic ASC, p.id ASC'));
+
+			$content = combo_box($bool ? lang('gallery', 'edit_success') : lang('gallery', 'edit_error'), uri('acp/gallery'));
+		}
 	}
 	if (!isset($_POST['entries']) && !isset($_POST['submit']) || isset($errors) && is_array($errors)) {
 		$tpl->assign('gallery_id', $modules->id);
