cb352fc5dcc0729385316bb25da0c89e2f149737
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-03-10 19:21:29 +0000

    Refactored the last module to the new structure + some fixes

--- trunk/modules/system/designs.php
+++ trunk/modules/system/designs.php
@@ -14,21 +14,38 @@ $breadcrumb->assign(lang('system', 'system'), uri('acp/system'));
 $breadcrumb->assign(lang('system', 'extensions'), uri('acp/system/extensions'));
 $breadcrumb->assign(lang('system', 'designs'));
 
-$designs = array();
-$directories = scandir('designs');
-$count_dir = count($directories);
-for ($i = 0; $i < $count_dir; $i++) {
-	$design_info = array();
-	if ($directories[$i] != '.' && $directories[$i] != '..' && file_exists('designs/' . $directories[$i] . '/info.php')) {
-		include 'designs/' . $directories[$i] . '/info.php';
-		$designs[$i]['name'] = $design_info['name'];
-		$designs[$i]['description'] = $design_info['description'];
-		$designs[$i]['author'] = $design_info['author'];
-		$designs[$i]['version'] = $design_info['version'];
-		$designs[$i]['action'] = CONFIG_DESIGN == $directories[$i] ? '<img src="' . ROOT_DIR . 'images/crystal/16/active.png" alt="" />' : '<a href="' . uri('acp/system/entry/action_designs/dir_' . urlencode($directories[$i])) . '"><img src="' . ROOT_DIR . 'images/crystal/16/inactive.png" alt="" /></a>';
-	}
-}
-$tpl->assign('designs', $designs);
+if (isset($modules->gen['dir'])) {
+	$dir = is_file('designs/' . $modules->gen['dir'] . '/info.php') ? $modules->gen['dir'] : 0;
+	$bool = false;
 
-$content = $tpl->fetch('system/designs.html');
+	if (!empty($dir)) {
+		$bool = $config->general(array('design' => $dir));
+	}
+	$text = $bool ? lang('system', 'designs_edit_success') : lang('system', 'designs_edit_error');
+
+	// Cache leeren und diverse Parameter für die Template Engine abändern
+	$cache->purge();
+	$tpl->template_dir = './designs/' . $dir . '/';
+	$tpl->assign('design_path', ROOT_DIR . 'designs/' . $dir . '/');
+
+	$content = combo_box($text, uri('acp/system/designs'));
+} else {
+	$designs = array();
+	$directories = scandir('designs');
+	$count_dir = count($directories);
+	for ($i = 0; $i < $count_dir; $i++) {
+		$design_info = array();
+		if ($directories[$i] != '.' && $directories[$i] != '..' && file_exists('designs/' . $directories[$i] . '/info.php')) {
+			include 'designs/' . $directories[$i] . '/info.php';
+			$designs[$i]['name'] = $design_info['name'];
+			$designs[$i]['description'] = $design_info['description'];
+			$designs[$i]['author'] = $design_info['author'];
+			$designs[$i]['version'] = $design_info['version'];
+			$designs[$i]['action'] = CONFIG_DESIGN == $directories[$i] ? '<img src="' . ROOT_DIR . 'images/crystal/16/active.png" alt="" />' : '<a href="' . uri('acp/system/designs/dir_' . urlencode($directories[$i])) . '"><img src="' . ROOT_DIR . 'images/crystal/16/inactive.png" alt="" /></a>';
+		}
+	}
+	$tpl->assign('designs', $designs);
+
+	$content = $tpl->fetch('system/designs.html');
+}
 ?>
--- trunk/modules/system/modules.php
+++ trunk/modules/system/modules.php
@@ -14,21 +14,59 @@ $breadcrumb->assign(lang('system', 'system'), uri('acp/system'));
 $breadcrumb->assign(lang('system', 'extensions'), uri('acp/system/extensions'));
 $breadcrumb->assign(lang('system', 'modules'));
 
-$mod_list = $modules->modulesList();
+if ($modules->action == 'activate') {
+	if (isset($modules->gen['dir']) && is_file('modules/' . $modules->gen['dir'] . '/module.xml')) {
+		$info = $modules->parseInfo($modules->gen['dir']);
+		if ($info['protected']) {
+			$text = lang('system', 'mod_deactivate_forbidden');
+		} else {
+			$path = 'modules/' . $modules->gen['dir'] . '/module.xml';
 
-foreach ($mod_list as $name => $info) {
-	if ($info['protected']) {
-		$mod_list[$name]['action'] = '<img src="' . ROOT_DIR . 'images/crystal/16/forbidden.png" alt="" />';
-	} elseif ($info['active']) {
-		$mod_list[$name]['action'] = '<a href="' . uri('acp/system/entry/action_moddeactivation/dir_' . $info['dir']) . '" title="' . lang('system', 'disable_module') . '"><img src="' . ROOT_DIR . 'images/crystal/16/active.png" alt="" /></a>';
+			$xml = simplexml_load_file($path);
+			$xml->info->active = '1';
+			$bool = $xml->asXML($path);
+
+			$text = $bool ? lang('system', 'mod_activate_success') : lang('system', 'mod_activate_error');
+		}
 	} else {
-		$mod_list[$name]['action'] = '<a href="' . uri('acp/system/entry/action_modactivation/dir_' . $info['dir']) . '" title="' . lang('system', 'enable_module') . '"><img src="' . ROOT_DIR . 'images/crystal/16/inactive.png" alt="" /></a>';
+		$text = lang('system', 'mod_activate_error');
+	}
+	$content = combo_box($text, uri('acp/system/modules'));
+} elseif ($modules->action == 'deactivate') {
+	if (isset($modules->gen['dir']) && is_file('modules/' . $modules->gen['dir'] . '/module.xml')) {
+		$info = $modules->parseInfo($modules->gen['dir']);
+		if ($info['protected']) {
+			$text = lang('system', 'mod_deactivate_forbidden');
+		} else {
+			$path = 'modules/' . $modules->gen['dir'] . '/module.xml';
+
+			$xml = simplexml_load_file($path);
+			$xml->info->active = '0';
+			$bool = $xml->asXML($path);
+
+			$text = $bool ? lang('system', 'mod_deactivate_success') : lang('system', 'mod_deactivate_error');
+		}
+	} else {
+		$text = lang('system', 'mod_deactivate_error');
+	}
+	$content = combo_box($text, uri('acp/system/modules'));
+} else {
+	$mod_list = $modules->modulesList();
+
+	foreach ($mod_list as $name => $info) {
+		if ($info['protected']) {
+			$mod_list[$name]['action'] = '<img src="' . ROOT_DIR . 'images/crystal/16/forbidden.png" alt="" />';
+		} elseif ($info['active']) {
+			$mod_list[$name]['action'] = '<a href="' . uri('acp/system/modules/action_deactivate/dir_' . $info['dir']) . '" title="' . lang('system', 'disable_module') . '"><img src="' . ROOT_DIR . 'images/crystal/16/active.png" alt="" /></a>';
+		} else {
+			$mod_list[$name]['action'] = '<a href="' . uri('acp/system/modules/action_activate/dir_' . $info['dir']) . '" title="' . lang('system', 'enable_module') . '"><img src="' . ROOT_DIR . 'images/crystal/16/inactive.png" alt="" /></a>';
+		}
+
 	}
 
+	$tpl->assign('LANG_modules_found', sprintf(lang('system', 'modules_found'), count($mod_list)));
+	$tpl->assign('modules', $mod_list);
+
+	$content = $tpl->fetch('system/modules.html');
 }
-
-$tpl->assign('LANG_modules_found', sprintf(lang('system', 'modules_found'), count($mod_list)));
-$tpl->assign('modules', $mod_list);
-
-$content = $tpl->fetch('system/modules.html');
 ?>
