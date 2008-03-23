109187586ddc7b06d83b7c0ec05f738873b12f50
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-03-23 21:38:22 +0000

    improved the captcha a little bit

--- modules/captcha/image.php
+++ modules/captcha/image.php
@@ -17,11 +17,21 @@ if (empty( $modules->gen['hash'] )) {
 	header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
 	header( 'Content-Type: image/gif' );
 	$hash = base64_decode($db->escape($modules->gen['hash'], 3));
+	$hashLength = strlen($hash);
 	
-	$im = imagecreate( 75, 23 );
-	$black = ImageColorAllocate( $im, 255, 255, 255 );
-	$white = ImageColorAllocate( $im, 0, 0, 0 );
-	ImageTTFText( $im, 15, 0, 10, 17, $white, 'modules/captcha/trebuc.ttf', $hash );
+	$im = imagecreate( $hashLength * 25, 30 );
+	// Hintergrundfarbe
+	ImageColorAllocate( $im, 255, 255, 255 );
+	// Textfarbe
+	$textColor = ImageColorAllocate( $im, 0, 0, 0 );
+
+	for ($i = 0; $i < $hashLength; $i++) {
+		$textSize = rand(10, 15);
+		$angle = rand(0, 30);
+		$posLeft = 22 * $i + 10;
+		$posTop = rand(20, 25);
+		ImageTTFText( $im, $textSize, $angle, $posLeft, $posTop, $textColor, 'modules/captcha/trebuc.ttf', $hash[$i] );
+	}
 	ImageGif( $im );
 	ImageDestroy( $im );
 	exit;
