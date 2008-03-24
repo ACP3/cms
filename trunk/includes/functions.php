e8c414310b676ba7013c1a43d9e14c2906f68817
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-03-24 20:19:58 +0000

    Optimized the salt() function

--- trunk/includes/functions.php
+++ trunk/includes/functions.php
@@ -355,8 +355,8 @@ function redirect($args, $new_page = 0)
  */
 function salt($str_length)
 {
-	$chars = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-	$c_chars = count($chars) - 1;
+	$chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	$c_chars = strlen($chars) - 1;
 	$key = '';
 	for ($i = 0; $i < $str_length; ++$i) {
 		$key.= $chars[mt_rand(0, $c_chars)];
