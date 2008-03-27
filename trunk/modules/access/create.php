62b010a3de62f2c99d829a1b864aa40bef43852d
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-03-27 15:03:53 +0000

    - refactoed the access level creation and editing a bit - added the missing UPDATE queries to the updater

--- trunk/modules/access/create.php
+++ trunk/modules/access/create.php
@@ -10,6 +10,8 @@
 if (!defined('IN_ADM'))
 	exit;
 
+include_once ACP3_ROOT . 'modules/access/functions.php';
+
 if (isset($_POST['submit'])) {
 	$form = $_POST['form'];
 
@@ -17,33 +19,16 @@ if (isset($_POST['submit'])) {
 		$errors[] = lang('common', 'name_to_short');
 	if (!empty($form['name']) && $db->select('id', 'access', 'name = \'' . $db->escape($form['name']) . '\'', 0, 0, 0, 1) == '1')
 		$errors[] = lang('access', 'access_level_already_exist');
-	// Überprüfen, ob zumindest einem Modul ein Zugriffslevel zugewiesen wurde
-	$empty = true;
-	foreach ($form['modules'] as $key) {
-		if (!empty($key)) {
-			$empty = false;
-			break;
-		}
-	}
-	if ($empty)
+	if (emptyCheck($form['modules']))
 		$errors[] = lang('access', 'select_modules');
 
 	if (isset($errors)) {
 		$tpl->assign('error_msg', comboBox($errors));
 	} else {
-		// String für die einzelnen Zugriffslevel auf die Module erstellen
-		$form['modules']['errors'] = '2';
-		ksort($form['modules']);
-		$insert_mods = '';
-
-		foreach ($form['modules'] as $module => $level) {
-			$insert_mods.= $module . ':' . $level . ',';
-		}
-
 		$insert_values = array(
 			'id' => '',
 			'name' => $db->escape($form['name']),
-			'modules' => substr($insert_mods, 0, -1),
+			'modules' => buildAccessLevel($form['modules']),
 		);
 
 		$bool = $db->insert('access', $insert_values);
@@ -56,21 +41,13 @@ if (!isset($_POST['submit']) || isset($errors) && is_array($errors)) {
 
 	$mod_list = $modules->modulesList();
 
-	function select_level($dir, $value)
-	{
-		if (isset($_POST['form']['modules'][$dir]) && $_POST['form']['modules'][$dir] == $value) {
-			return ' selected="selected"';
-		}
-		return '';
-	}
-
 	foreach ($mod_list as $name => $info) {
 		if ($info['dir'] == 'errors' || !$info['active']) {
 			unset($mod_list[$name]);
 		} else {
-			$mod_list[$name]['level_0_selected'] = select_level($info['dir'], '0');
-			$mod_list[$name]['level_1_selected'] = select_level($info['dir'], '1');
-			$mod_list[$name]['level_2_selected'] = select_level($info['dir'], '2');
+			$mod_list[$name]['level_0_selected'] = selectAccessLevel($info['dir'], '0');
+			$mod_list[$name]['level_1_selected'] = selectAccessLevel($info['dir'], '1');
+			$mod_list[$name]['level_2_selected'] = selectAccessLevel($info['dir'], '2');
 		}
 	}
 	$tpl->assign('mod_list', $mod_list);
--- trunk/modules/access/edit.php
+++ trunk/modules/access/edit.php
@@ -10,6 +10,8 @@
 if (!defined('IN_ADM'))
 	exit;
 
+include_once ACP3_ROOT . 'modules/access/functions.php';
+
 if (!empty($modules->id) && $db->select('id', 'access', 'id = \'' . $modules->id . '\'', 0, 0, 0, 1) == '1') {
 	if (isset($_POST['submit'])) {
 		$form = $_POST['form'];
@@ -18,32 +20,15 @@ if (!empty($modules->id) && $db->select('id', 'access', 'id = \'' . $modules->id
 			$errors[] = lang('common', 'name_to_short');
 		if (!empty($form['name']) && $db->select('id', 'access', 'id != \'' . $modules->id . '\' AND name = \'' . $db->escape($form['name']) . '\'', 0, 0, 0, 1) == '1')
 			$errors[] = lang('access', 'access_level_already_exist');
-		// Überprüfen, ob zumindest einem Modul ein Zugriffslevel zugewiesen wurde
-		$empty = true;
-		foreach ($form['modules'] as $key) {
-			if (!empty($key)) {
-				$empty = false;
-				break;
-			}
-		}
-		if ($empty)
+		if (emptyCheck($form['modules']))
 			$errors[] = lang('access', 'select_modules');
 
 		if (isset($errors)) {
 			$tpl->assign('error_msg', comboBox($errors));
 		} else {
-			// String für die einzelnen Zugriffslevel auf die Module erstellen
-			$form['modules']['errors'] = '2';
-			ksort($form['modules']);
-			$insert_mods = '';
-
-			foreach ($form['modules'] as $module => $level) {
-				$insert_mods.= $module . ':' . $level . ',';
-			}
-
 			$update_values = array(
 				'name' => $db->escape($form['name']),
-				'modules' => substr($insert_mods, 0, -1),
+				'modules' => buildAccessLevel($form['modules']),
 			);
 
 			$bool = $db->update('access', $update_values, 'id = \'' . $modules->id . '\'');
@@ -60,16 +45,6 @@ if (!empty($modules->id) && $db->select('id', 'access', 'id = \'' . $modules->id
 		$mods_arr = explode(',', $access[0]['modules']);
 		$c_mods_arr = count($mods_arr);
 
-		function select_level($dir, $db_value, $value)
-		{
-			if (isset($_POST['form']['modules'][$dir])) {
-				return $_POST['form']['modules'][$dir] == $value ? ' selected="selected"' : '';
-			} elseif ($db_value == $value) {
-				return ' selected="selected"';
-			}
-			return '';
-		}
-
 		foreach ($mod_list as $name => $info) {
 			if ($info['dir'] == 'errors' || !$info['active']) {
 				unset($mod_list[$name]);
@@ -77,9 +52,9 @@ if (!empty($modules->id) && $db->select('id', 'access', 'id = \'' . $modules->id
 				for ($i = 0; $i < $c_mods_arr; $i++) {
 					if ($info['dir'] == substr($mods_arr[$i], 0, -2)) {
 						$db_value = substr($mods_arr[$i], -1, 1);
-						$mod_list[$name]['level_0_selected'] = select_level($info['dir'], $db_value, '0');
-						$mod_list[$name]['level_1_selected'] = select_level($info['dir'], $db_value, '1');
-						$mod_list[$name]['level_2_selected'] = select_level($info['dir'], $db_value, '2');
+						$mod_list[$name]['level_0_selected'] = selectAccessLevel($info['dir'], '0', $db_value);
+						$mod_list[$name]['level_1_selected'] = selectAccessLevel($info['dir'], '1', $db_value);
+						$mod_list[$name]['level_2_selected'] = selectAccessLevel($info['dir'], '2', $db_value);
 						break;
 					}
 				}
