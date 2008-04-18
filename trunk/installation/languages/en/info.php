2a3681822769d2fbac574fd8b1f4d89adf4c043b
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-04-18 23:23:13 +0000

    Reworked the lang() function of the installer

--- trunk/installation/languages/en/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang_info = array(
-	'name' => 'English',
-	'description' => 'Official english language pack for the ACP3.',
-	'author' => 'Goratsch Webdesign',
-);
-?>
--- trunk/installation/modules/install/requirements.php
+++ trunk/installation/modules/install/requirements.php
@@ -3,18 +3,18 @@ if (!defined('IN_INSTALL'))
 	exit;
 
 // Allgemeine Voraussetzungen
-$requirements[0]['name'] = lang('php_version');
+$requirements[0]['name'] = lang('installation', 'php_version');
 $requirements[0]['color'] = version_compare(phpversion(), '5.0.5', '>=') ? '090' : 'f00';
 $requirements[0]['found'] = phpversion();
 $requirements[0]['required'] = '5.0.5+';
-$requirements[1]['name'] = lang('mysql_version');
+$requirements[1]['name'] = lang('installation', 'mysql_version');
 $requirements[1]['color'] = version_compare(mysql_get_client_info(), '4.0', '>=') ? '090' : 'f00';
 $requirements[1]['found'] = mysql_get_client_info();
 $requirements[1]['required'] = '4.0+';
-$requirements[2]['name'] = lang('safe_mode');
+$requirements[2]['name'] = lang('installation', 'safe_mode');
 $requirements[2]['color'] = (bool)ini_get('safe_mode') ? 'f00' : '090';
-$requirements[2]['found'] = (bool)ini_get('safe_mode') ? lang('on') : lang('off');
-$requirements[2]['required'] = lang('off');
+$requirements[2]['found'] = (bool)ini_get('safe_mode') ? lang('installation', 'on') : lang('installation', 'off');
+$requirements[2]['required'] = lang('installation', 'off');
 
 $tpl->assign('requirements', $requirements);
 
@@ -55,16 +55,16 @@ foreach ($defaults as $row) {
 	// Überprüfen, ob es eine Datei oder ein Ordner ist
 	if (is_file(ACP3_ROOT . $row)) {
 		$files_dirs[$i]['color_1'] = '090';
-		$files_dirs[$i]['exists'] = lang('file_found');
+		$files_dirs[$i]['exists'] = lang('installation', 'file_found');
 	} elseif (is_dir(ACP3_ROOT . $row)) {
 		$files_dirs[$i]['color_1'] = '090';
-		$files_dirs[$i]['exists'] = lang('folder_found');
+		$files_dirs[$i]['exists'] = lang('installation', 'folder_found');
 	} else {
 		$files_dirs[$i]['color_1'] = 'f00';
-		$files_dirs[$i]['exists'] = lang('file_folder_not_found');
+		$files_dirs[$i]['exists'] = lang('installation', 'file_folder_not_found');
 	}
 	$files_dirs[$i]['color_2'] = is_writable(ACP3_ROOT . $row) ? '090' : 'f00';
-	$files_dirs[$i]['writeable'] = $files_dirs[$i]['color_2'] == '090' ? lang('writeable') : lang('not_writeable');
+	$files_dirs[$i]['writeable'] = $files_dirs[$i]['color_2'] == '090' ? lang('installation', 'writeable') : lang('installation', 'not_writeable');
 	if ($files_dirs[$i]['color_1'] == 'f00' || $files_dirs[$i]['color_2'] == 'f00') {
 		$check_again = true;
 	}
@@ -74,13 +74,13 @@ foreach ($defaults as $row) {
 $tpl->assign('files_dirs', $files_dirs);
 
 // PHP Einstellungen
-$php_settings[0]['setting'] = lang('error_messages');
+$php_settings[0]['setting'] = lang('installation', 'error_messages');
 $php_settings[0]['color'] = (bool)ini_get('display_errors') ? 'f00' : '090';
-$php_settings[0]['value'] = (bool)ini_get('display_errors') ? lang('on') : lang('off');
-$php_settings[1]['setting'] = lang('register_globals');
+$php_settings[0]['value'] = (bool)ini_get('display_errors') ? lang('installation', 'on') : lang('installation', 'off');
+$php_settings[1]['setting'] = lang('installation', 'register_globals');
 $php_settings[1]['color'] = (bool)ini_get('register_globals') ? 'f00' : '090';
-$php_settings[1]['value'] = (bool)ini_get('register_globals') ? lang('on') : lang('off');
-$php_settings[2]['setting'] = lang('maximum_uploadsize');
+$php_settings[1]['value'] = (bool)ini_get('register_globals') ? lang('installation', 'on') : lang('installation', 'off');
+$php_settings[2]['setting'] = lang('installation', 'maximum_uploadsize');
 $php_settings[2]['color'] = ini_get('post_max_size') > 0 ? '090' : 'f00';
 $php_settings[2]['value'] = ini_get('post_max_size');
 
