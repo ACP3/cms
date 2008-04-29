f90420bde8476d713386bfc52bae45646b734a92
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-04-29 20:16:00 +0000

    first part at improving the pages module

--- modules/pages/functions.php
+++ modules/pages/functions.php
@@ -6,36 +6,57 @@
  * @package ACP3
  * @subpackage Modules
  */
+function generatePagesCache()
+{
+	global $db;
+	
+	$pages = $db->select('p.id, p.start, p.end, p.mode, p.parent, p.sort, p.title, p.target, b.index_name AS block_name', 'pages AS p, ' . CONFIG_DB_PRE . 'pages_blocks AS b', 'p.block_id = b.id', 'b.id ASC, p.sort ASC, p.title ASC');
+	$c_pages = count($pages);
+	$items = array();
+	
+	if ($c_pages > 0) {
+		
+		for ($i = 0; $i < $c_pages; ++$i) {
+			foreach ($pages[$i] as $key => $value) {
+				$items[$pages[$i]['id']][$key] = $value;
+			}
+			$items[$pages[$i]['parent']]['childs'][$pages[$i]['id']] =& $items[$pages[$i]['id']];
+		}
+	}
+	cache::create('pages', $items[0]['childs']);
+}
 /**
  * Auflistung der übergeordneten Seiten
  *
+ * @param array $pages
  * @param integer $id
  * @param integer $parent
  * @return array
  */
-function pagesList($id = 0, $parent = 0, $newVar = 0)
+function pagesList($pages, $id = 0, $parent = 0)
 {
-	global $db;
 	static $output = array(), $key = 0, $spaces = '';
 
-	$pages = $db->select('id, title', 'pages', '(mode = \'1\' OR mode = \'2\') AND parent = \'' . $id . '\' AND id != \'' . $newVar . '\'', 'block_id ASC, sort ASC, title ASC');
 	$c_pages = count($pages);
 
 	if ($c_pages > 0) {
 		if ($id != 0)
 			$spaces.= '&nbsp;&nbsp;';
 
-		for ($i = 0; $i < $c_pages; ++$i) {
-			$output[$key]['id'] = $pages[$i]['id'];
-			$output[$key]['selected'] = selectEntry('parent', $pages[$i]['id'], $parent);
-			$output[$key]['title'] = $spaces . $pages[$i]['title'];
+		$i = 0;
+		foreach ($pages as $row) {
+			$output[$key]['id'] = $row['id'];
+			$output[$key]['selected'] = selectEntry('parent', $row['id'], $parent);
+			$output[$key]['title'] = $spaces . $row['title'];
 			$key++;
-
-			pagesList($pages[$i]['id'], $parent, $newVar);
+			if (!empty($row['childs'])) {
+				pagesList($row['childs'], $row['id'], $parent);
+			}
 
 			if ($i == $c_pages - 1) {
 				$spaces = substr($spaces, 0, -12);
 			}
+			++$i;
 		}
 	}
 	return $output;
@@ -75,10 +96,10 @@ function processNavbar()
 {
 	global $db, $modules;
 
-	if (!cache::check('pages')) {
-		cache::create('pages', $db->select('p.id, p.start, p.end, p.mode, p.title, p.target, b.index_name AS block_name', 'pages AS p, ' . CONFIG_DB_PRE . 'pages_blocks AS b', 'p.block_id != \'0\' AND p.block_id = b.id', 'p.sort ASC, p.title ASC'));
+	if (!cache::check('pages_navbar')) {
+		cache::create('pages_navbar', $db->select('p.id, p.start, p.end, p.mode, p.title, p.target, b.index_name AS block_name', 'pages AS p, ' . CONFIG_DB_PRE . 'pages_blocks AS b', 'p.block_id != \'0\' AND p.block_id = b.id', 'p.sort ASC, p.title ASC'));
 	}
-	$pages = cache::output('pages');
+	$pages = cache::output('pages_navbar');
 	$c_pages = count($pages);
 
 	if ($c_pages > 0) {
