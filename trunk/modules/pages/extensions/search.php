f2b1320c4494ba6d87a6a01aef6df0172e010915
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-07-13 17:33:52 +0000

    typo/bug fix

--- trunk/modules/pages/extensions/search.php
+++ trunk/modules/pages/extensions/search.php
@@ -28,7 +28,7 @@ $c_result_pages = count($result_pages);
 if ($c_result_pages > 0) {
 	$results_mods['pages']['title'] = $lang->t('pages', 'pages');
 	for ($i = 0; $i < $c_result_pages; ++$i) {
-		$results_mods['pages']['results'][$i]['hyperlink'] = uri('pages/list/id_' . $result_pages[$i]['id']);
+		$results_mods['pages']['results'][$i]['hyperlink'] = uri('pages/list/item_' . $result_pages[$i]['id']);
 		$results_mods['pages']['results'][$i]['headline'] = $result_pages[$i]['title'];
 
 		$striped_text = strip_tags($result_pages[$i]['text']);
