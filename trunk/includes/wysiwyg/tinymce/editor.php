fbfb01be321974c445b7eb0c168ec5c1c6b3d5f4
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-09-14 15:27:28 +0000

    updated TinyMCE to version 3.2 & added the its gzip compressor

--- trunk/includes/wysiwyg/tinymce/editor.php
+++ trunk/includes/wysiwyg/tinymce/editor.php
@@ -6,9 +6,23 @@ function editor($params) {
 	$out = '';
 
 	if (!$init) {
-		$out.= '<script type="text/javascript" src="' . ROOT_DIR . 'includes/wysiwyg/tinymce/tiny_mce.js"></script>' . "\n";
+		$out.= '<script type="text/javascript" src="' . ROOT_DIR . 'includes/wysiwyg/tinymce/tiny_mce_gzip.js"></script>' . "\n";
 	}
 
+	// Gzip Komprimierung aktiviieren
+	$out.= '<script type="text/javascript">' . "\n";
+	$out.= "tinyMCE_GZ.init({\n";
+	$out.= 'themes: "advanced",' . "\n";
+	$out.= 'languages: "en",' . "\n";
+
+	if (isset($params['toolbar']) && $params['toolbar'] == 'simple') {
+		$out.= 'plugins : "inlinepopups,contextmenu",' . "\n";
+	} else {
+		$out.= 'plugins : "safari,style,layer,table,advhr,advimage,advlink,emotions,inlinepopups,preview,media,searchreplace,contextmenu,paste,noneditable,visualchars,nonbreaking,xhtmlxtras",' . "\n";
+	}
+	$out.= "});\n";
+	$out.= "</script>\n";
+	// Normale Initialisierung
 	$out.= '<script type="text/javascript">' . "\n";
 	$out.= "tinyMCE.init({\n";
 	$out.= 'mode : "exact",' . "\n";
@@ -28,11 +42,10 @@ function editor($params) {
 		$out.= 'theme_advanced_buttons3 : "styleselect,formatselect,fontselect,fontsizeselect,|,forecolor,backcolor,|,visualaid,|,tablecontrols",' . "\n";
 		$out.= 'theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,|,help",' . "\n";
 	}
-	$out.= 'width : "100%",' . "\n";
 	$out.= 'height : "' . $params['height'] . '",' . "\n";
 	$out.= "});\n";
 	$out.= "</script>\n";
-	$out.= '<textarea name="' . $params['name'] . '" id="' . $id . '" cols="50" rows="5">' . (!empty($params['value']) ? $params['value'] : '') . "</textarea>\n";
+	$out.= '<textarea name="' . $params['name'] . '" id="' . $id . '" cols="50" rows="5" style="width:100%">' . (!empty($params['value']) ? $params['value'] : '') . "</textarea>\n";
 
 	$init = true;
 
