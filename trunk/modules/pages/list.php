afd38581261c52017417c1014df0fb70505bfc9b
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2008-09-17 18:27:26 +0000

    updated jQuery UI to version 1.6rc1 + added some comments to pages/list.php

--- trunk/modules/pages/list.php
+++ trunk/modules/pages/list.php
@@ -18,9 +18,11 @@ if (validate::isNumber($uri->item) && $db->select('id', 'pages', 'id = \'' . $ur
 	}
 	$page = cache::output('pages_list_id_' . $uri->item);
 
+	// Statische Seite
 	if ($page[0]['mode'] == '1') {
 		$tpl->assign('text', $db->escape($page[0]['text'], 3));
 		$content = $tpl->fetch('pages/list.html');
+	// Dynamische Seite (ACP3 intern)
 	} elseif ($page[0]['mode'] == '2') {
 		$params = explode('/', $db->escape($page[0]['uri'], 3));
 		$c_params = count($params);
@@ -33,11 +35,14 @@ if (validate::isNumber($uri->item) && $db->select('id', 'pages', 'id = \'' . $ur
 				}
 			}
 		}
+		// Moduldatei laden...
 		if (!empty($params[0]) && !empty($params[1]) && modules::check($params[0], $params[1])) {
 			include ACP3_ROOT . 'modules/' . $params[0] . '/' . $params[1] . '.php';
+		// ...ansonsten zur Fehlerseite weiterleiten
 		} else {
 			redirect('errors/404');
 		}
+	// Zu externer Seite weiterleiten
 	} else {
 		redirect(0, $db->escape($page[0]['uri'], 3));
 	}
