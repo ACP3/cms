61c900f2a829a191479922fd18e080869f39bd1a
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-01-09 13:17:36 +0000

    updated jquery to 1.3b2 and jQuery UI to 1.6rc4 and use the new datepicker + tabs widget

--- trunk/includes/js/acp3.js
+++ trunk/includes/js/acp3.js
@@ -20,28 +20,5 @@ function mark_entries(name, action)
 	});
 }
 jQuery(function($) {
-	/**
-	 * Tabs
-	 **/
-	var match = 0;
-	$('#tabs').prepend('<div id="wrapper"></div>');
-	$('#tabs > fieldset').each(function(i) {
-		var header = $(this).children('legend').text();
-		var index = 'tab-' + i;
-		$('#tabs #wrapper').append($('<a href="#' + index + '">' + header + '</a>'));
-		$(this).attr('id', index);
-		if (window.location.hash.substr(1) == index) {
-			match = i;
-		}
-	});
-	$('#tabs > fieldset > legend').remove();
-	$('#tabs > fieldset:not(:eq(' + match + '))').hide();
-	
-	$('#tabs #wrapper a').click(function() {
-		$('#tabs #wrapper a').removeClass('selected');
-		$(this).addClass('selected');
-		$('#tabs > fieldset:visible').hide();
-		$('#tabs fieldset').filter(this.hash).show();
-		return false;
-	}).filter(':eq(' + match + ')').click();
+	$('#tabs').tabs();
 })
