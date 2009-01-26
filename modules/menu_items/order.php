ea0d201c500cac81c465b77d7d442198bb1ec817
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-01-26 18:19:12 +0000

    reworked sorting

--- modules/menu_items/order.php
+++ modules/menu_items/order.php
@@ -23,22 +23,19 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.right_id = ' . $pages[0]['left_id'] . ' - 1 AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
 				$c_elem = count($elem);
 				$c_pages = count($pages);
-				$diff = $pages[0]['right_id'] - $pages[0]['left_id'] + 1;
+				$elem_ids = $pages_ids = '';
+				$diff_left = $pages[0]['left_id'] - $elem[0]['left_id'];
+				$diff_right = $pages[0]['right_id'] - $elem[0]['right_id'];
 
 				for ($i = 0; $i < $c_elem; ++$i) {
-					$update_values = array(
-						'left_id' => $elem[0]['left_id'] + $i + $diff + ($i > 1 ? 1 : 0),
-						'right_id' => $elem[0]['left_id'] + $i + $diff + $elem[$i]['right_id'] - $elem[$i]['left_id'] + ($i > 1 ? 1 : 0),
-					);
-					$db->update('menu_items', $update_values, 'id = \'' . $elem[$i]['id'] . '\'');
+					$elem_ids.= 'id = \'' . $elem[$i]['id'] . '\' OR ';
 				}
 				for ($i = 0; $i < $c_pages; ++$i) {
-					$update_values = array(
-						'left_id' => $elem[0]['left_id'] + $i + ($i > 1 ? 1 : 0),
-						'right_id' => $elem[0]['left_id'] + $i + $pages[$i]['right_id'] - $pages[$i]['left_id'] + ($i > 1 ? 1 : 0),
-					);
-					$db->update('menu_items', $update_values, 'id = \'' . $pages[$i]['id'] . '\'');
+					$pages_ids.= 'id = \'' . $pages[$i]['id'] . '\' OR ';
 				}
+
+				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($elem_ids, 0, -4), 0);
+				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($pages_ids, 0, -4), 0);
 			}
 			break;
 		case 'down':
@@ -47,22 +44,19 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.left_id = ' . $pages[0]['right_id'] . ' + 1 AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
 				$c_elem = count($elem);
 				$c_pages = count($pages);
-				$diff = $elem[0]['right_id'] - $elem[0]['left_id'] + 1;
+				$elem_ids = $pages_ids = '';
+				$diff_left = $elem[0]['left_id'] - $pages[0]['left_id'];
+				$diff_right = $elem[0]['right_id'] - $pages[0]['right_id'];
 
 				for ($i = 0; $i < $c_elem; ++$i) {
-					$update_values = array(
-						'left_id' => $pages[0]['left_id'] + $i + ($i > 1 ? 1 : 0),
-						'right_id' => $pages[0]['left_id'] + $i + $elem[$i]['right_id'] - $elem[$i]['left_id'] + ($i > 1 ? 1 : 0),
-					);
-					$db->update('menu_items', $update_values, 'id = \'' . $elem[$i]['id'] . '\'');
+					$elem_ids.= 'id = \'' . $elem[$i]['id'] . '\' OR ';
 				}
 				for ($i = 0; $i < $c_pages; ++$i) {
-					$update_values = array(
-						'left_id' => $pages[0]['left_id'] + $i + $diff + ($i > 1 ? 1 : 0),
-						'right_id' => $pages[0]['left_id'] + $i + $diff + $pages[$i]['right_id'] - $pages[$i]['left_id'] + ($i > 1 ? 1 : 0),
-					);
-					$db->update('menu_items', $update_values, 'id = \'' . $pages[$i]['id'] . '\'');
+					$pages_ids.= 'id = \'' . $pages[$i]['id'] . '\' OR ';
 				}
+
+				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($elem_ids, 0, -4), 0);
+				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($pages_ids, 0, -4), 0);
 			}
 			break;
 		default:
