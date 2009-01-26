9638794f560a5093ef7b9fe095c34d94d952f412
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-01-26 21:22:03 +0000

    - moving an subtree from left to right works without any bugs :) - todo: moving from right to left, selected element as new root - small changes and bug fixes for sorting subtrees

--- modules/menu_items/order.php
+++ modules/menu_items/order.php
@@ -16,11 +16,13 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 	breadcrumb::assign($lang->t('menu_items', 'menu_items'), uri('acp/menu_items'));
 	breadcrumb::assign($lang->t('common', 'edit_order'));
 
+	$bool = $bool2 = null;
+
 	switch ($uri->mode) {
 		case 'up':
 			$pages = $db->query('SELECT c.id, c.block_id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.id = \'' . $uri->id . '\' AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
-			if ($db->select('COUNT(id)', 'menu_items', 'right_id < ' . $pages[0]['left_id'] . ' AND block_id = \'' . $pages[0]['block_id'] . '\'', 0, 0, 0, 1) > 0) {
-				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.right_id = ' . $pages[0]['left_id'] . ' - 1 AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
+			if ($db->select('COUNT(id)', 'menu_items', 'right_id = ' . ($pages[0]['left_id'] - 1) . ' AND block_id = \'' . $pages[0]['block_id'] . '\'', 0, 0, 0, 1) > 0) {
+				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.right_id = ' . ($pages[0]['left_id'] - 1) . ' AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
 				$c_elem = count($elem);
 				$c_pages = count($pages);
 				$elem_ids = $pages_ids = '';
@@ -34,14 +36,14 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 					$pages_ids.= 'id = \'' . $pages[$i]['id'] . '\' OR ';
 				}
 
-				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($elem_ids, 0, -4), 0);
-				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($pages_ids, 0, -4), 0);
+				$bool = $db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($elem_ids, 0, -4), 0);
+				$bool2 = $db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($pages_ids, 0, -4), 0);
 			}
 			break;
 		case 'down':
 			$pages = $db->query('SELECT c.id, c.block_id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.id = \'' . $uri->id . '\' AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
-			if ($db->select('COUNT(id)', 'menu_items', 'left_id > ' . $pages[0]['right_id'] . ' AND block_id = \'' . $pages[0]['block_id'] . '\'', 0, 0, 0, 1) > 0) {
-				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.left_id = ' . $pages[0]['right_id'] . ' + 1 AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
+			if ($db->select('COUNT(id)', 'menu_items', 'left_id = ' . ($pages[0]['right_id'] + 1) . ' AND block_id = \'' . $pages[0]['block_id'] . '\'', 0, 0, 0, 1) > 0) {
+				$elem = $db->query('SELECT c.id, c.left_id, c.right_id FROM ' . CONFIG_DB_PRE . 'menu_items AS p, ' . CONFIG_DB_PRE . 'menu_items AS c WHERE p.left_id = ' . ($pages[0]['right_id'] + 1) . ' AND c.left_id BETWEEN p.left_id AND p.right_id ORDER BY c.left_id');
 				$c_elem = count($elem);
 				$c_pages = count($pages);
 				$elem_ids = $pages_ids = '';
@@ -55,18 +57,16 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 					$pages_ids.= 'id = \'' . $pages[$i]['id'] . '\' OR ';
 				}
 
-				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($elem_ids, 0, -4), 0);
-				$db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($pages_ids, 0, -4), 0);
+				$bool = $db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id - ' . $diff_left . ', right_id = right_id - ' . $diff_left . ' WHERE ' . substr($elem_ids, 0, -4), 0);
+				$bool2 = $db->query('UPDATE ' . CONFIG_DB_PRE . 'menu_items SET left_id = left_id + ' . $diff_right . ', right_id = right_id + ' . $diff_right . ' WHERE ' . substr($pages_ids, 0, -4), 0);
 			}
 			break;
-		default:
-			$error = true;
 	}
-	if (!isset($error)) {
+	if ($bool !== null && $bool2 !== null) {
 		setNavbarCache();
-
 		redirect('acp/menu_items');
 	}
+} else {
+	redirect('acp/errors/404');
 }
-redirect('acp/errors/404');
 ?>
