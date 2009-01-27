2052d61d61c19d6e507ba57690f0b6c52656352b
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-01-27 16:40:44 +0000

    forgot to add some files

---
+++ modules/news/settings.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * News
+ *
+ * @author Goratsch Webdesign
+ * @package ACP3
+ * @subpackage Modules
+ */
+
+if (!defined('IN_ADM'))
+	exit;
+
+if (isset($_POST['submit'])) {
+	$form = $_POST['form'];
+	
+	if (!isset($form['comments']) || $form['comments'] != 1 && $form['comments'] != 0)
+		$errors[] = $lang->t('news', 'select_allow_comments');
+	if (!isset($form['readmore']) || $form['readmore'] != 1 && $form['readmore'] != 0)
+		$errors[] = $lang->t('news', 'select_activate_readmore');
+	if (!validate::isNumber($form['readmore_chars']) || $form['readmore_chars'] == 0)
+		$errors[] = $lang->t('news', 'type_in_readmore_chars');
+
+	if (isset($errors)) {
+		$tpl->assign('error_msg', comboBox($errors));
+	} else {
+		$bool = config::module('news', $form);
+		
+		$content = comboBox($bool ? $lang->t('common', 'settings_success') : $lang->t('common', 'settings_error'), uri('acp/news'));
+	}
+}
+if (!isset($_POST['submit']) || isset($errors) && is_array($errors)) {
+	$settings = config::output('news');
+
+	$readmore[0]['value'] = '1';
+	$readmore[0]['checked'] = selectEntry('readmore', '1', $settings['readmore'], 'checked');
+	$readmore[0]['lang'] = $lang->t('common', 'yes');
+	$readmore[1]['value'] = '0';
+	$readmore[1]['checked'] = selectEntry('readmore', '0', $settings['readmore'], 'checked');
+	$readmore[1]['lang'] = $lang->t('common', 'no');
+	$tpl->assign('readmore', $readmore);
+	
+	$comments[0]['value'] = '1';
+	$comments[0]['checked'] = selectEntry('comments', '1', $settings['comments'], 'checked');
+	$comments[0]['lang'] = $lang->t('common', 'yes');
+	$comments[1]['value'] = '0';
+	$comments[1]['checked'] = selectEntry('comments', '0', $settings['comments'], 'checked');
+	$comments[1]['lang'] = $lang->t('common', 'no');
+	$tpl->assign('comments', $comments);
+
+	$tpl->assign('readmore_chars', isset($form) ? $form['readmore_chars'] : $settings['readmore_chars']);
+
+	$content = $tpl->fetch('news/settings.html');
+}
+?>
