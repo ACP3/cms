fcc0199a165bf1c6b767fd53e941d31917f164e1
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-01-27 07:54:47 +0000

    regex bug fix

--- trunk/modules/menu_items/edit.php
+++ trunk/modules/menu_items/edit.php
@@ -24,7 +24,7 @@ if (validate::isNumber($uri->id) && $db->select('COUNT(id)', 'menu_items', 'id =
 			$errors[] = $lang->t('menu_items', 'title_to_short');
 		if (!validate::isNumber($form['target']) ||
 			$form['mode'] == '1' && (!is_dir(ACP3_ROOT . 'modules/' . $form['module']) || preg_match('=/=', $form['module'])) ||
-			$form['mode'] == '2' && !preg_match('/(?i:[a-z_-]+\/){2,}/', $form['uri']) ||
+			$form['mode'] == '2' && !preg_match('/^(?i:[a-z_-]+\/){2,}$/', $form['uri']) ||
 			$form['mode'] == '3' && empty($form['uri']))
 			$errors[] = $lang->t('menu_items', 'type_in_uri_and_target');
 
