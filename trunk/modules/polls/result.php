43552f70ded7aa46771f52554d3742da3546dc8b
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-04-06 20:02:15 +0000

    fixed some sql queries in polls module

--- trunk/modules/polls/result.php
+++ trunk/modules/polls/result.php
@@ -17,11 +17,11 @@ if (validate::isNumber($uri->id) && $db->countRows('*', 'poll_question', 'id = \
 	$question = $db->select('question', 'poll_question');
 	$answers = $db->select('id, text', 'poll_answers', 'poll_id = \'' . $uri->id . '\'', 'id ASC');
 	$c_answers = count($answers);
-	$total_votes = $db->select('COUNT(answer_id)', 'poll_votes', 'poll_id = \'' . $uri->id . '\'');
+	$total_votes = $db->countRows('answer_id', 'poll_votes', 'poll_id = \'' . $uri->id . '\'');
 
 	for ($i = 0; $i < $c_answers; ++$i) {
 		$answers[$i]['text'] = $answers[$i]['text'];
-		$answers[$i]['votes'] = $db->select('COUNT(answer_id)', 'poll_votes', 'poll_id = \'' . $uri->id . '\' AND answer_id = \'' . $answers[$i]['id'] . '\'');
+		$answers[$i]['votes'] = $db->countRows('answer_id', 'poll_votes', 'poll_id = \'' . $uri->id . '\' AND answer_id = \'' . $answers[$i]['id'] . '\'');
 		$answers[$i]['percent'] = $total_votes > '0' ? round(100 * $answers[$i]['votes'] / $total_votes, 2) : '0';
 	}
 	$tpl->assign('question', $question[0]['question']);
