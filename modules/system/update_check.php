f2f941d3ec3988a2bd5bfe223088c04ed746fa1e
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-04-29 13:53:17 +0000

    some little fixes

--- modules/system/update_check.php
+++ modules/system/update_check.php
@@ -18,14 +18,16 @@ breadcrumb::assign($lang->t('system', 'update_check'));
 $file = @file_get_contents('http://www.acp3-cms.net/update.txt');
 if ($file) {
 	$content = explode('||', $file);
-	$content[2] = CONFIG_VERSION;
+	if (count($content) == 2) {
+		$content[2] = CONFIG_VERSION;
 
-	if (version_compare($content[2], $content[0], '>=')) {
-		$tpl->assign('update_text', $lang->t('system', 'acp3_up_to_date'));
-	} else {
-		$tpl->assign('update_text', sprintf($lang->t('system', 'acp3_not_up_to_date'), '<a href="' . $content[1] . '" onclick="window.open(this.href); return false">', '</a>'));
+		if (version_compare($content[2], $content[0], '>=')) {
+			$tpl->assign('update_text', $lang->t('system', 'acp3_up_to_date'));
+		} else {
+			$tpl->assign('update_text', sprintf($lang->t('system', 'acp3_not_up_to_date'), '<a href="' . $content[1] . '" onclick="window.open(this.href); return false">', '</a>'));
+		}
+		$tpl->assign('update', $content);
 	}
-	$tpl->assign('update', $content);
 }
 $content = $tpl->fetch('system/update_check.html');
 ?>
