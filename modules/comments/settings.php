37a045e2a5d885e51e9007e81b72c9ef7a8363ce
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-11-02 18:09:41 +0000

    forgot to add some files...

---
+++ modules/comments/settings.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Comments
+ *
+ * @author Goratsch Webdesign
+ * @package ACP3
+ * @subpackage Modules
+ */
+
+if (!defined('IN_ADM'))
+	exit;
+
+if (isset($_POST['submit'])) {
+	$form = $_POST['form'];
+	
+	if (empty($form['dateformat']) || ($form['dateformat'] != 'long' && $form['dateformat'] != 'short'))
+		$errors[] = $lang->t('common', 'select_date_format');
+
+	if (isset($errors)) {
+		$tpl->assign('error_msg', comboBox($errors));
+	} else {
+		$bool = config::module('comments', $form);
+		
+		$content = comboBox($bool ? $lang->t('common', 'settings_success') : $lang->t('common', 'settings_error'), uri('acp/files'));
+	}
+}
+if (!isset($_POST['submit']) || isset($errors) && is_array($errors)) {
+	$settings = config::output('comments');
+
+	$dateformat[0]['value'] = 'short';
+	$dateformat[0]['selected'] = selectEntry('dateformat', 'short', $settings['dateformat']);
+	$dateformat[0]['lang'] = $lang->t('common', 'date_format_short');
+	$dateformat[1]['value'] = 'long';
+	$dateformat[1]['selected'] = selectEntry('dateformat', 'long', $settings['dateformat']);
+	$dateformat[1]['lang'] = $lang->t('common', 'date_format_long');
+	$tpl->assign('dateformat', $dateformat);
+
+	$content = $tpl->fetch('comments/settings.html');
+}
