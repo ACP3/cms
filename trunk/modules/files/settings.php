4efd5f0866a7f1bcd23b17f49f4c50948cf57f96
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-11-02 18:09:41 +0000

    forgot to add some files...

---
+++ trunk/modules/files/settings.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Files
+ *
+ * @author Goratsch Webdesign
+ * @package ACP3
+ * @subpackage Modules
+ */
+
+if (!defined('IN_ADM'))
+	exit;
+
+if (isset($_POST['submit'])) {
+	$form = $_POST['form'];
+
+	if (!isset($form['comments']) || $form['comments'] != 1 && $form['comments'] != 0)
+		$errors[] = $lang->t('files', 'select_allow_comments');
+	if (empty($form['dateformat']) || ($form['dateformat'] != 'long' && $form['dateformat'] != 'short'))
+		$errors[] = $lang->t('common', 'select_date_format');
+
+	if (isset($errors)) {
+		$tpl->assign('error_msg', comboBox($errors));
+	} else {
+		$bool = config::module('files', $form);
+
+		$content = comboBox($bool ? $lang->t('common', 'settings_success') : $lang->t('common', 'settings_error'), uri('acp/files'));
+	}
+}
+if (!isset($_POST['submit']) || isset($errors) && is_array($errors)) {
+	$settings = config::output('files');
+
+	$comments[0]['value'] = '1';
+	$comments[0]['checked'] = selectEntry('comments', '1', $settings['comments'], 'checked');
+	$comments[0]['lang'] = $lang->t('common', 'yes');
+	$comments[1]['value'] = '0';
+	$comments[1]['checked'] = selectEntry('comments', '0', $settings['comments'], 'checked');
+	$comments[1]['lang'] = $lang->t('common', 'no');
+	$tpl->assign('comments', $comments);
+
+	$dateformat[0]['value'] = 'short';
+	$dateformat[0]['selected'] = selectEntry('dateformat', 'short', $settings['dateformat']);
+	$dateformat[0]['lang'] = $lang->t('common', 'date_format_short');
+	$dateformat[1]['value'] = 'long';
+	$dateformat[1]['selected'] = selectEntry('dateformat', 'long', $settings['dateformat']);
+	$dateformat[1]['lang'] = $lang->t('common', 'date_format_long');
+	$tpl->assign('dateformat', $dateformat);
+
+	$content = $tpl->fetch('files/settings.html');
+}
