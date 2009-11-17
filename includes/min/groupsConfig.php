a4b500397cf29fd8d78241be2e305d6359a1e7c6
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2009-11-17 08:29:03 +0000

    moved some files around

--- includes/min/groupsConfig.php
+++ includes/min/groupsConfig.php
@@ -24,17 +24,17 @@ if ($_GET['g'] == 'css') {
 		}
 	}
 
-	$styles['css'][] = DESIGN_PATH . '/jquery-css/jquery-ui.css';
-	$styles['css'][] = DESIGN_PATH . '/jquery-css/jquery-colorbox.css';
+	$styles['css'][] = DESIGN_PATH . 'jquery/jquery-ui.css';
+	$styles['css'][] = DESIGN_PATH . 'jquery/jquery-colorbox.css';
 
 	return $styles;
 } elseif ($_GET['g'] == 'js') {
 	$scripts = array();
-	$scripts['js'][] = DESIGN_PATH . '/jquery-js/jquery.js';
-	$scripts['js'][] = DESIGN_PATH . '/jquery-js/jquery.cookie.js';
-	$scripts['js'][] = DESIGN_PATH . '/jquery-js/jquery.ui.js';
-	$scripts['js'][] = DESIGN_PATH . '/jquery-js/jquery.colorbox.js';
-	$scripts['js'][] = DESIGN_PATH . '/script.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.cookie.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.ui.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.colorbox.js';
+	$scripts['js'][] = DESIGN_PATH . 'script.js';
 
 	return $scripts;
 }
