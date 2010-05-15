e5a67916669177f3c50f00e206da0ca392aaac82
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-05-15 14:22:47 +0000

    - aligned the parameter name of the "lang" smarty function with the $lang-class - little adjustment to selection of entries in tables (part 4/4)

--- includes/smarty/plugins/function.lang.php
+++ includes/smarty/plugins/function.lang.php
@@ -2,7 +2,7 @@
 function smarty_function_lang($params)
 {
 	global $lang;
-	$values = explode('|', $params['values']);
+	$values = explode('|', $params['t']);
 	return $lang->t($values[0], $values[1]);
 }
 /* vim: set expandtab: */
