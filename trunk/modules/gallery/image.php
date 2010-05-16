4aa72a3da820e92e3b16c54b2e1a8807ef947cf7
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-05-16 14:50:49 +0000

    refactored image.php in gallery module

--- trunk/modules/gallery/image.php
+++ trunk/modules/gallery/image.php
@@ -18,69 +18,55 @@ if (validate::isNumber($uri->id) && ($uri->action == 'thumb' || $uri->action == 
 
 	if (is_file($path)) {
 		$picInfo = getimagesize($path);
-		$width = $picInfo[0];
-		$height = $picInfo[1];
 		$type = $picInfo[2];
-		$action = $uri->action;
 
-		$settings = config::output('gallery');
+		if ($type == '1' || $type == '2' || $type == '3') {
+			$width = $picInfo[0];
+			$height = $picInfo[1];
+			$action = $uri->action;
+			$settings = config::output('gallery');
 
-		header('Cache-Control: public');
-		header('Pragma: public');
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
-		header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 31536000) . ' GMT');
+			header('Cache-Control: public');
+			header('Pragma: public');
+			header('Last-Modified: ' . gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
+			header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 31536000) . ' GMT');
+			header('Content-type: ' . $picInfo['mime']);
 
-		if (extension_loaded('gd') &&
-			($type == '1' || $type == '2' || $type == '3') &&
-			($action == 'thumb' && $width > $settings['thumbwidth'] && $height > $settings['thumbheight']) ||
-			($action == 'normal' && $width > $settings['width'] && $height > $settings['height'])) {
-			if ($width > $height) {
-				$newWidth = $action == 'thumb' ? $settings['thumbwidth'] : $settings['width'];
-				$newHeight = intval($height * $newWidth / $width);
+			if (extension_loaded('gd') &&
+				($action == 'thumb' && $width > $settings['thumbwidth'] && $height > $settings['thumbheight']) ||
+				($action == 'normal' && $width > $settings['width'] && $height > $settings['height'])) {
+				if ($width > $height) {
+					$newWidth = $action == 'thumb' ? $settings['thumbwidth'] : $settings['width'];
+					$newHeight = intval($height * $newWidth / $width);
+				} else {
+					$newHeight = $action == 'thumb' ? $settings['thumbheight'] : $settings['height'];
+					$newWidth = intval($width * $newHeight / $height);
+				}
+
+				$newPic = imagecreatetruecolor($newWidth, $newHeight);
+				switch ($type) {
+					case '1':
+						$oldPic = imagecreatefromgif($path);
+						imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+						imagegif($newPic);
+						break;
+					case '2':
+						$oldPic = imagecreatefromjpeg($path);
+						imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+						imagejpeg($newPic, NULL, 90);
+						break;
+					case '3':
+						imagealphablending($newPic, false);
+						$oldPic = imagecreatefrompng($path);
+						imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+						imagesavealpha($newPic, true);
+						imagepng($newPic);
+						break;
+				}
+				imagedestroy($newPic);
 			} else {
-				$newHeight = $action == 'thumb' ? $settings['thumbheight'] : $settings['height'];
-				$newWidth = intval($width * $newHeight / $height);
+				readfile($path);
 			}
-
-			$newPic = imagecreatetruecolor($newWidth, $newHeight);
-			switch ($type) {
-				case '1':
-					header('Content-type: image/gif');
-					$oldPic = imagecreatefromgif($path);
-					imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-					imagegif($newPic);
-					break;
-				case '2':
-					header('Content-type: image/jpeg');
-					$oldPic = imagecreatefromjpeg($path);
-					imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-					imagejpeg($newPic, NULL, 90);
-					break;
-				case '3':
-					header('Content-type: image/png');
-					imagealphablending($newPic, false);
-					$oldPic = imagecreatefrompng($path);
-					imagecopyresampled($newPic, $oldPic, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-					imagesavealpha($newPic, true);
-					imagepng($newPic);
-					break;
-			}
-			imagedestroy($newPic);
-		} else {
-			switch ($type) {
-				case '1':
-					header('Content-type: image/gif');
-					break;
-				case '2':
-					header('Content-type: image/jpeg');
-					break;
-				case '3':
-					header('Content-type: image/png');
-					break;
-				default:
-					exit;
-			}
-			readfile($path);
 		}
 	}
 	exit;
