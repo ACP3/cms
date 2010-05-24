011260c41f26619581b7d6dead4109febe079ac9
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-05-24 15:50:34 +0000

    added width and height to gallery thumbs to speed up page rendering

--- trunk/modules/gallery/functions.php
+++ trunk/modules/gallery/functions.php
@@ -16,7 +16,29 @@
 function setGalleryCache($id)
 {
 	global $db;
-	return cache::create('gallery_pics_id_' . $id, $db->select('id, description', 'gallery_pictures', 'gallery_id = \'' . $id . '\'', 'pic ASC, id ASC'));
+
+	$pictures = $db->select('id, file, description', 'gallery_pictures', 'gallery_id = \'' . $id . '\'', 'pic ASC, id ASC');
+	$c_pictures = count($pictures);
+
+	$settings = config::output('gallery');
+
+	for ($i = 0; $i < $c_pictures; ++$i) {
+		$picInfos = getimagesize(ACP3_ROOT . 'uploads/gallery/' . $pictures[$i]['file']);
+		if ($picInfos[0] > $settings['thumbwidth'] && $picInfos[1] > $settings['thumbheight']) {
+			if ($picInfos[0] > $picInfos[1]) {
+				$newWidth = $settings['thumbwidth'];
+				$newHeight = intval($picInfos[1] * $newWidth / $picInfos[0]);
+			} else {
+				$newHeight = $settings['thumbheight'];
+				$newWidth = intval($picInfos[0] * $newHeight / $picInfos[1]);
+			}
+		}
+
+       	$pictures[$i]['width'] = isset($newWidth) ? $newWidth : $width;
+       	$pictures[$i]['height'] = isset($newHeight) ? $newHeight : $height;
+	}
+
+	return cache::create('gallery_pics_id_' . $id, $pictures);
 }
 /**
  * Bindet die gecachete Galerie anhand ihrer ID ein
