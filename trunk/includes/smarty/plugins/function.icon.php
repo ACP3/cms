3b1cbcfa34895a21d117930ad32ea28c5d7513ad
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-05-30 14:00:58 +0000

    - refactored smarty function icon() to be more flexible and use it in various places - added image dimensions emoticons to speed up page rendering - fixed the editing of archived newsletters

--- trunk/includes/smarty/plugins/function.icon.php
+++ trunk/includes/smarty/plugins/function.icon.php
@@ -1,7 +1,22 @@
 <?php
 function smarty_function_icon($params)
 {
-	return ROOT_DIR . 'images/crystal/' . $params['path'] . '.png';
+	$path = 'images/crystal/' . $params['path'] . '.png';
+	$width = $height = '';
+
+	if (validate::isNumber($params['width']) && validate::isNumber($params['height'])) {
+		$width = ' width="' . $params['width'] . '"';
+		$height = ' height="' . $params['height'] . '"';
+	} elseif (is_file(ACP3_ROOT . $path)) {
+		$picInfos = getimagesize(ACP_ROOT . $path);
+		$width = ' width="' . $picInfos[0] . '"';
+		$height = ' height="' . $picInfos[1] . '"';
+	}
+
+	$alt = !empty($params['alt']) ? ' alt="' . $params['alt'] . '"' : '';
+	$title = !empty($params['title']) ? ' title="' . $params['title'] . '"' : '';
+
+	return '<img src="' . ROOT_DIR . $path . '"' . $width . $height . $alt . $title . ' />';
 }
 /* vim: set expandtab: */
 ?>
