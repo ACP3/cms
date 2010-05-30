bb9fee6e1c91f86923d4383264547b7d0da8e81a
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-05-30 14:34:42 +0000

    - fixed a bug with the smarty function icon() introduced with last commit - added image dimension checking to smarty function access_check()

--- trunk/includes/smarty/plugins/function.check_access.php
+++ trunk/includes/smarty/plugins/function.check_access.php
@@ -8,8 +8,11 @@ function smarty_function_check_access($params, &$smarty)
 		global $lang;
 
 		$access_check = array();
-		if (isset($params['icon']))
-			$access_check['icon'] = ROOT_DIR . 'images/crystal/' . $params['icon'] . '.png';
+
+		if (isset($params['icon'])) {
+			$path = 'images/crystal/' . $params['icon'] . '.png';
+			$access_check['icon'] = ROOT_DIR . $path;
+		}
 		if (isset($params['title']))
 			$access_check['title'] = $params['title'];
 		if (isset($params['uri']))
@@ -20,6 +23,22 @@ function smarty_function_check_access($params, &$smarty)
 		} else {
 			$access_check['lang'] = $lang->t($action[0], $action[1]);
 		}
+
+		// Dimensionen der Grafik
+		if ($params['mode'] == 'link' && isset($params['icon'])) {
+			$access_check['width'] = $access_check['height'] = '';
+
+			if (!empty($params['width']) && !empty($params['height']) &&
+				validate::isNumber($params['width']) && validate::isNumber($params['height'])) {
+				$access_check['width'] = ' width="' . $params['width'] . '"';
+				$access_check['height'] = ' height="' . $params['height'] . '"';
+			} elseif (is_file(ACP3_ROOT . $path)) {
+				$picInfos = getimagesize(ACP3_ROOT . $path);
+				$access_check['width'] = ' width="' . $picInfos[0] . '"';
+				$access_check['height'] = ' height="' . $picInfos[1] . '"';
+			}
+		}
+
 		$access_check['mode'] = $params['mode'];
 		$smarty->assign('access_check', $access_check);
 		return $smarty->fetch('common/access_check.html');
