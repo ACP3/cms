1e6a3c002c8ed6cadfbe54ace535304ac24bfa85
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-07-29 21:52:58 +0000

    - updated Minify to version 2.1.4 Beta

--- includes/min/groupsConfig.php
+++ includes/min/groupsConfig.php
@@ -13,13 +13,29 @@ require_once '../config.php';
 define('DESIGN_PATH', dirname(__FILE__) . '/../../designs/' . CONFIG_DESIGN . '/');
 
 if ($_GET['g'] == 'css') {
+	define('ACP3_ROOT', '../../');
+
+	set_include_path(get_include_path() . PATH_SEPARATOR . ACP3_ROOT . 'includes/classes/');
+	spl_autoload_extensions('.class.php');
+	spl_autoload_register();
+
+	// Klassen initialisieren
+	$db = new db();
+	$handle = $db->connect(CONFIG_DB_HOST, CONFIG_DB_NAME, CONFIG_DB_USER, CONFIG_DB_PASSWORD, CONFIG_DB_PRE);
+	if ($handle !== true) {
+		exit($handle);
+	}
+
+	$auth = new auth();
+	$lang = new lang();
+
 	$modules = scandir(DESIGN_PATH);
 	$styles = array();
 	$styles['css'][] = DESIGN_PATH . 'layout.css';
 
 	foreach ($modules as $module) {
 		$path = DESIGN_PATH . $module . '/style.css';
-		if (is_file($path) && $module != '.' && $module != '..') {
+		if (is_file($path) && $module != '.' && $module != '..' && modules::isActive($module)) {
 			$styles['css'][] = $path;
 		}
 	}
@@ -30,10 +46,10 @@ if ($_GET['g'] == 'css') {
 	return $styles;
 } elseif ($_GET['g'] == 'js') {
 	$scripts = array();
-	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.min.js';
 	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.cookie.js';
-	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.ui.js';
-	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.colorbox.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.ui.min.js';
+	$scripts['js'][] = DESIGN_PATH . 'jquery/jquery.colorbox.min.js';
 	$scripts['js'][] = DESIGN_PATH . 'script.js';
 
 	return $scripts;
