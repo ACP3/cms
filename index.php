ae0504ca7c1e14074314fdcd5250a62a4d6ae849
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2010-10-16 17:04:45 +0000

    updated TinyMCE to version 3.3.9.2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @package ACP3
  * @subpackage Core
  */
-define('ACP3_ROOT', './');
+define('ACP3_ROOT', dirname(__FILE__) . '/');
 require ACP3_ROOT . 'includes/common.php';
 
 modules::outputPage();
