7ee3178e0b272c7702cf1c6fb9b03ff83cab2424
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-07-01 08:33:26 +0000

    some little changes to smarty functions

--- includes/smarty/plugins/function.wysiwyg.php
+++ includes/smarty/plugins/function.wysiwyg.php
@@ -8,14 +8,14 @@ function smarty_function_wysiwyg($params) {
 	$path = ACP3_ROOT . 'includes/wysiwyg/' . CONFIG_WYSIWYG . '/editor.php';
 	$params['name'] = 'form[' . $params['name'] . ']';
 	$params['id'] = !empty($params['id']) ? $params['id'] : substr($params['name'], 5, -1);
-	
+
+	// WYSIWYG Editor einbinden
 	if (CONFIG_WYSIWYG != 'textarea' && is_file($path) && !preg_match('=/=', CONFIG_WYSIWYG)) {
 		require_once $path;
 
 		return editor($params);
+	// Einfache textarea erzeugen
 	} else {
-		global $uri;
-
 		$out = '';
 
 		// Falls aktiv, die Emoticons einbinden
