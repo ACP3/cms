a648ae26119f2e756ccf7eef538242287ede2dd9
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-07-01 09:12:17 +0000

    updated Smarty to version 3.1 RC 1

--- includes/smarty/plugins/function.mark.php
+++ includes/smarty/plugins/function.mark.php
@@ -1,8 +1,8 @@
 <?php
 function smarty_function_mark($params, $template)
 {
-	$template->assign('checkbox_name', $params['name']);
-	return $template->fetch('common/mark.html');
+	$template->smarty->assign('checkbox_name', $params['name']);
+	return $template->smarty->fetch('common/mark.html');
 }
 /* vim: set expandtab: */
 ?>
