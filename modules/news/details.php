59958d3a15832e86fa8e86eebe36a520a64f21d3
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-07-07 07:48:39 +0000

    unescape some more variables, after reading them from the database

--- modules/news/details.php
+++ modules/news/details.php
@@ -28,8 +28,9 @@ if (validate::isNumber($uri->id) && $db->countRows('*', 'news', 'id = \'' . $uri
 	$settings = config::output('news');
 
 	$news[0]['date'] = $date->format($news[0]['start'], $settings['dateformat']);
-	$news[0]['headline'] = $news[0]['headline'];
+	$news[0]['headline'] = $db->escape($news[0]['headline'], 3);
 	$news[0]['text'] = rewriteInternalUri($db->escape($news[0]['text'], 3));
+	$news[0]['link_title'] = $db->escape($news[0]['link_title'], 3);
 	$news[0]['uri'] = $db->escape($news[0]['uri'], 3);
 	if (!empty($news[0]['uri']) && strpos($news[0]['uri'], 'http://') === false) {
 		$news[0]['uri'] = 'http://' . $news[0]['uri'];
