28f8284d0ffa8fdd4fda01f406472a7a7b71d12a
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-09-17 15:08:42 +0000

    updated CKEditor to version 3.6.2

--- trunk/includes/wysiwyg/ckeditor/config.js
+++ trunk/includes/wysiwyg/ckeditor/config.js
@@ -5,23 +5,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	config.extraPlugins = 'MediaEmbed';
-	config.toolbar = 'Full';
-
-	config.toolbar_Full =
-	[
-		{ name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
-		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker','Scayt' ] },
-		{ name: 'forms', items : [ 'Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField' ] },
-		'/',
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
-		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-		{ name: 'insert', items : [ 'Image','Flash','MediaEmbed','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
-		'/',
-		{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
-		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
-		{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
-	];
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
