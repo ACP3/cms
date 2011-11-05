2a00710aefe1f6bc519ebac1cb51e399f6086b7c
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-11-05 14:06:32 +0000

    added html5 version of standard theme

---
+++ designs/acp3_html5/system/script.js
@@ -0,0 +1,18 @@
+function mark_options(action)
+{
+	if (action == 'add') {
+		$('form #tables option').attr('selected', 'selected');
+	} else {
+		$('form #tables option').removeAttr('selected');
+	}
+}
+
+$(function() {
+	$('input[name="form[export_type]"]').click(function() {
+		if (($(this).attr('id') == 'complete' || $(this).attr('id') == 'structure')) {
+			$('#options-container').show();
+		} else {
+			$('#options-container').hide();
+		}
+	}).click();
+});
