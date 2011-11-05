82273d6d41af1b13de7f19a3798f14cba931d88d
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2011-11-05 14:06:32 +0000

    added html5 version of standard theme

---
+++ trunk/designs/acp3_html5/users/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('ul.admin > li:has(ul) > a').click(function() {
+		if (!$(this).next('ul').is(':animated')) {
+			$(this).next('ul').slideToggle('slow');
+		}
+		return false;
+	});
+});
