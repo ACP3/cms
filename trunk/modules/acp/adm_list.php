c8aee46952cef4f5d3816bfd2b014d1c6a4bc5fd
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2012-01-06 22:40:59 +0000

    removed some dead code

--- trunk/modules/acp/adm_list.php
+++ trunk/modules/acp/adm_list.php
@@ -25,18 +25,4 @@ foreach ($mod_list as $name => $info) {
 }
 $tpl->assign('modules', $mods);
 
-//Server Infos
-if (modules::check('system', 'server_config') == 1) {
-	$server_info[0]['col_left'] = $lang->t('system', 'architecture');
-	$server_info[0]['col_right'] = @php_uname('m');
-	$server_info[1]['col_left'] = $lang->t('system', 'operating_system');
-	$server_info[1]['col_right'] = @php_uname('s') . ' ' . @php_uname('r');
-	$server_info[2]['col_left'] = $lang->t('system', 'server_software');
-	$server_info[2]['col_right'] = $_SERVER['SERVER_SOFTWARE'];
-	$server_info[3]['col_left'] = $lang->t('system', 'php_version');
-	$server_info[3]['col_right'] = phpversion();
-
-	$tpl->assign('server_info', $server_info);
-}
-
 $content = modules::fetchTemplate('acp/adm_list.html');
