07eb6b09d0bb6fa17e786c7c32039769c97b2d19
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-02-14 20:43:33 +0000

    - fixed the maintenance mode and placed it in that way, that it spits out the message as early as possible - removed the maintenance.css and placed the styles into the maintenance.tpl, as they are not that many - removed the user language from the Smarty compile_id, as it doesn't have any effect

--- includes/common.php
+++ includes/common.php
@@ -35,6 +35,35 @@ set_include_path(get_include_path() . PATH_SEPARATOR . INCLUDES_DIR . 'classes/'
 spl_autoload_extensions('.class.php');
 spl_autoload_register();
 
+// Smarty einbinden
+require INCLUDES_DIR . 'smarty/Smarty.class.php';
+$tpl = new Smarty();
+$tpl->error_reporting = $reporting_level;
+$tpl->compile_id = CONFIG_DESIGN;
+$tpl->setCompileCheck(defined('DEBUG') === true && DEBUG === true);
+$tpl->setTemplateDir(ACP3_ROOT . 'designs/' . CONFIG_DESIGN . '/')
+	->addPluginsDir(INCLUDES_DIR . 'smarty/custom/')
+	->setCompileDir(ACP3_ROOT . 'uploads/cache/tpl_compiled/')
+	->setCacheDir(ACP3_ROOT . 'uploads/cache/tpl_cached/');
+if (is_writable($tpl->getCompileDir()) === false || is_writable($tpl->getCacheDir()) === false) {
+	exit('Bitte geben Sie dem "cache"-Ordner den CHMOD 777!');
+}
+
+// Einige Template Variablen setzen
+$tpl->assign('PHP_SELF', PHP_SELF);
+$tpl->assign('REQUEST_URI', htmlentities($_SERVER['REQUEST_URI']));
+$tpl->assign('ROOT_DIR', ROOT_DIR);
+$tpl->assign('DESIGN_PATH', DESIGN_PATH);
+$tpl->assign('LANG', CONFIG_LANG);
+
+// Falls der Wartungsmodus aktiv ist, Wartungsnachricht ausgeben
+if (defined('IN_ADM') === false && CONFIG_MAINTENANCE_MODE === true) {
+	$tpl->assign('PAGE_TITLE', CONFIG_SEO_TITLE);
+	$tpl->assign('CONTENT', CONFIG_MAINTENANCE_MESSAGE);
+	$tpl->display('maintenance.tpl');
+	exit;
+}
+
 // Klassen initialisieren
 $db = new db();
 $handle = $db->connect(CONFIG_DB_HOST, CONFIG_DB_NAME, CONFIG_DB_USER, CONFIG_DB_PASSWORD, CONFIG_DB_PRE);
@@ -49,31 +78,4 @@ $date = new date();
 
 acl::initialize($auth->getUserId());
 
-// Smarty einbinden
-require INCLUDES_DIR . 'smarty/Smarty.class.php';
-$tpl = new Smarty();
-$tpl->error_reporting = $reporting_level;
-$tpl->compile_id = CONFIG_DESIGN . '_' . $auth->getUserLanguage();
-$tpl->setCompileCheck(defined('DEBUG') === true && DEBUG === true);
-$tpl->setTemplateDir(ACP3_ROOT . 'designs/' . CONFIG_DESIGN . '/')
-	->addPluginsDir(INCLUDES_DIR . 'smarty/custom/')
-	->setCompileDir(ACP3_ROOT . 'uploads/cache/tpl_compiled/')
-	->setCacheDir(ACP3_ROOT . 'uploads/cache/tpl_cached/');
-if (is_writable($tpl->getCompileDir()) === false || is_writable($tpl->getCacheDir()) === false) {
-	exit('Bitte geben Sie dem "cache"-Ordner den CHMOD 777!');
-}
-
 require_once INCLUDES_DIR . 'functions.php';
-
-// Einige Template Variablen setzen
-$tpl->assign('PHP_SELF', PHP_SELF);
-$tpl->assign('REQUEST_URI', htmlentities($_SERVER['REQUEST_URI']));
-$tpl->assign('ROOT_DIR', ROOT_DIR);
-$tpl->assign('DESIGN_PATH', DESIGN_PATH);
-$tpl->assign('LANG', CONFIG_LANG);
-
-// Falls der Wartungsmodus aktiv ist, Wartungsnachricht ausgeben
-if (defined('IN_ADM') === false && CONFIG_MAINTENANCE_MODE === true) {
-	view::setContent(CONFIG_MAINTENANCE_MESSAGE);
-	view::assignLayout('maintenance.tpl');
-}
