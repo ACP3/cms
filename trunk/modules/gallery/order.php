bac421ce54fab6a1f0e8b0779adb78b2ae61a41b
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-02-14 16:51:36 +0000

    this should work a little bit better... -.-

--- trunk/modules/gallery/order.php
+++ trunk/modules/gallery/order.php
@@ -14,9 +14,9 @@ if (validate::isNumber($uri->id) === true) {
 	require_once MODULES_DIR . 'gallery/functions.php';
 
 	if (($uri->action === 'up' || $uri->action === 'down') && $db->countRows('*', 'gallery_pictures', 'id = \'' . $uri->id . '\'') == 1) {
-		$gallery = $db->select('g.name, g.id', 'gallery AS g, {pre}gallery_pictures AS p', 'p.id = \'' . $uri->id . '\' AND p.gallery_id = g.id');
+		moveOneStep($uri->action, 'gallery_pictures', 'id', 'pic', $uri->id, 'gallery');
 
-		moveOneStep($uri->action, 'gallery_pictures', 'id', 'pic', $uri->id);
+		$gallery = $db->select('g.id', 'gallery AS g, {pre}gallery_pictures AS p', 'p.id = \'' . $uri->id . '\' AND p.gallery_id = g.id');
 		setGalleryCache($gallery[0]['id']);
 
 		$uri->redirect('acp/gallery/edit_gallery/id_' . $gallery[0]['id']);
