136f1f38b5d50dc1a178dcd9bc9fbb34e6ed0b77
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-02-14 12:14:32 +0000

    - reworked the moveOneStep() function - some minor changes

--- trunk/modules/system/sql_optimisation.php
+++ trunk/modules/system/sql_optimisation.php
@@ -22,7 +22,7 @@ if ($uri->action === 'do') {
 	foreach ($mod_list as $name => $info) {
 		if (is_array($info['tables']) === true) {
 			foreach ($info['tables'] as $table) {
-				$table_status = $db->query('SHOW TABLE STATUS FROM ' . CONFIG_DB_NAME . ' LIKE \'' . $db->prefix . $table . '\'');
+				$table_status = $db->query('SHOW TABLE STATUS FROM ' . CONFIG_DB_NAME . ' LIKE \'{pre}' . $table . '\'');
 				$c_table_status = count($table_status);
 				for ($j = 0; $j < $c_table_status; ++$j) {
 					$tables[$i]['name'] = $table_status[$j]['Name'];
