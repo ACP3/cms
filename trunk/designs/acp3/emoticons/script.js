e17faa595141dfb5084062853649324d0b77d72d
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-02-18 23:30:54 +0000

    emoticons are now getting inserted at the last caret position (Fixes issue #14)

--- trunk/designs/acp3/emoticons/script.js
+++
@@ -1,7 +0,0 @@
-// Emoticons in <textarea> einfügen
-function emoticon(fieldId, emotion)
-{
-	$('#' + fieldId).focus();
-	var currentVal = $('#' + fieldId).val();
-	$('#' + fieldId).val(currentVal + ' ' + emotion + ' ');
-}
