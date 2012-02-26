d7b4e37f76a4e60a345c5659b2950b2e1903014c
fettesvieh <fettesvieh@849d3b1b-9637-0410-8226-95211594b0a1>
2012-02-26 23:57:08 +0000

    use the new auto sizing feature for iframes

--- designs/acp3/guestbook/script.js
+++ designs/acp3/guestbook/script.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	$('#create-link').attr('href', $('#create-link').attr('href') + 'layout_simple/');
 	$('#create-link').fancybox({
 		type: 'iframe',
-		width: '40%',
-		height: '68%',
+		autoSize: true,
 		padding: 0,
 		afterClose: function() {
 			window.location.reload();
