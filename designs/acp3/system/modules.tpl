2c823e0e66b6ccc5b444420808fefefb47a0f0aa
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-07-30 19:35:47 +0000

    - wip: separate installed and installable modules - language files cleanup

--- designs/acp3/system/modules.tpl
+++ designs/acp3/system/modules.tpl
@@ -1,33 +1,66 @@
-<p>
-	{$LANG_modules_found}
-</p>
-<table class="acp-table">
-	<thead>
-		<tr>
-			<th>{lang t="system|module_name"}</th>
-			<th>{lang t="common|description"}</th>
-			<th>{lang t="common|author"}</th>
-			<th>{lang t="system|version"}</th>
-			<th>{lang t="common|options"}</th>
-		</tr>
-	</thead>
-	<tbody>
-{foreach $modules as $row}
-		<tr>
-			<td>{$row.name}</td>
-			<td>{$row.description}</td>
-			<td>{$row.author}</td>
-			<td>{$row.version}</td>
-			<td>
+<div id="tabs">
+	<ul>
+		<li><a href="#tab-1">{lang t="system|installed_modules"}</a></li>
+		<li><a href="#tab-2">{lang t="system|installable_modules"}</a></li>
+	</ul>
+	<div id="tab-1">
+		<table class="acp-table">
+			<thead>
+				<tr>
+					<th>{lang t="system|module_name"}</th>
+					<th>{lang t="common|description"}</th>
+					<th>{lang t="system|version"}</th>
+					<th>{lang t="common|author"}</th>
+					<th>{lang t="common|options"}</th>
+				</tr>
+			</thead>
+			<tbody>
+{foreach $installed_modules as $row}
+				<tr>
+					<td>{$row.name}</td>
+					<td>{$row.description}</td>
+					<td>{$row.version}</td>
+					<td>{$row.author}</td>
+					<td>
 {if $row.protected === true}
-				{icon path="16/editdelete" width="16" height="16"}
+						{icon path="16/editdelete" width="16" height="16"}
 {elseif $row.active === true}
-				<a href="{uri args="acp/system/modules/dir_`$row.dir`/action_deactivate"}" title="{lang t="system|disable_module"}">{icon path="16/apply" width="16" height="16"}</a>
+						<a href="{uri args="acp/system/modules/dir_`$row.dir`/action_deactivate"}" title="{lang t="system|disable_module"}">{icon path="16/apply" width="16" height="16"}</a>
 {else}
-				<a href="{uri args="acp/system/modules/dir_`$row.dir`/action_activate"}" title="{lang t="system|enable_module"}">{icon path="16/cancel" width="16" height="16"}</a>
+						<a href="{uri args="acp/system/modules/dir_`$row.dir`/action_activate"}" title="{lang t="system|enable_module"}">{icon path="16/cancel" width="16" height="16"}</a>
 {/if}
-			</td>
-		</tr>
+					</td>
+				</tr>
 {/foreach}
-	</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+	<div id="tab-2">
+{if !empty($new_modules)}
+		<table class="acp-table">
+			<thead>
+				<tr>
+					<th>{lang t="system|module_name"}</th>
+					<th>{lang t="common|description"}</th>
+					<th>{lang t="system|version"}</th>
+					<th>{lang t="common|author"}</th>
+					<th>{lang t="common|options"}</th>
+				</tr>
+			</thead>
+			<tbody>
+{foreach $new_modules as $row}
+				<tr>
+					<td>{$row.name}</td>
+					<td>{$row.description}</td>
+					<td>{$row.version}</td>
+					<td>{$row.author}</td>
+					<td><a href="{uri args="acp/system/modules/dir_`$row.dir`/action_install"}" title="{lang t="system|install_module"}">{icon path="16/edit_add" width="16" height="16"}</a></td>
+				</tr>
+{/foreach}
+			</tbody>
+		</table>
+{else}
+		<p>{lang t="system|no_modules_available_for_installation"}</p>
+{/if}
+	</div>
+</div>
--- modules/system/modules.php
+++ modules/system/modules.php
@@ -13,38 +13,49 @@ if (defined('IN_ADM') === false)
 $breadcrumb->append($lang->t('system', 'extensions'), $uri->route('acp/system/extensions'))
 		   ->append($lang->t('system', 'modules'));
 
-if ($uri->action === 'activate') {
-	$info = ACP3_Modules::parseInfo($uri->dir);
-	if ($info['protected']) {
-		$text = $lang->t('system', 'mod_deactivate_forbidden');
-	} else {
-		$bool = $db->update('modules', array('active' => 1), 'name = \'' . $uri->dir . '\'');
-		ACP3_Modules::setModulesCache();
-		ACP3_ACL::setResourcesCache();
+switch ($uri->action) {
+	case 'activate':
+	case 'deactivate':
+		$info = ACP3_Modules::parseInfo($uri->dir);
+		if (empty($info)) {
+			$text = $lang->t('system', 'module_not_found');
+		} elseif ($info['protected']) {
+			$text = $lang->t('system', 'mod_deactivate_forbidden');
+		} else {
+			$bool = $db->update('modules', array('active' => $uri->action === 'activate' ? 1 : 0), 'name = \'' . $db->escape($uri->dir) . '\'');
+			ACP3_Modules::setModulesCache();
+			ACP3_ACL::setResourcesCache();
 
-		$text = $bool !== false ? $lang->t('system', 'mod_activate_success') : $lang->t('system', 'mod_activate_error');
-	}
-	ACP3_View::setContent(confirmBox($text, $uri->route('acp/system/modules')));
-} elseif ($uri->action === 'deactivate') {
-	$info = ACP3_Modules::parseInfo($uri->dir);
-	if ($info['protected']) {
-		$text = $lang->t('system', 'mod_deactivate_forbidden');
-	} else {
-		$bool = $db->update('modules', array('active' => 0), 'name = \'' . $uri->dir . '\'');
-		ACP3_Modules::setModulesCache();
-		ACP3_ACL::setResourcesCache();
+			$text = $bool !== false ? $lang->t('system', $uri->action === 'activate' ? 'mod_activate_success' : 'mod_deactivate_success') : $lang->t('system', $uri->action === 'activate' ? 'mod_activate_error' : 'mod_deactivate_error');
+		}
+		ACP3_View::setContent(confirmBox($text, $uri->route('acp/system/modules')));
+		break;
+	case 'install':
+		// Nur noch nicht installierte Module berücksichtigen
+		if ($db->countRows('*', 'modules', 'name = \'' . $db->escape($uri->dir) . '\'') == 0) {
+			
+		} else {
+			$text = $lang->t('system', 'module_already_installed');
+		}
+		ACP3_View::setContent(confirmBox($text, $uri->route('acp/system/modules')));
+		break;
+	default:
+		// Languagecache neu erstellen, für den Fall, dass neue Module hinzugefügt wurden
+		$lang->setLangCache();
 
-		$text = $bool !== false ? $lang->t('system', 'mod_deactivate_success') : $lang->t('system', 'mod_deactivate_error');
-	}
-	ACP3_View::setContent(confirmBox($text, $uri->route('acp/system/modules')));
-} else {
-	// Languagecache neu erstellen
-	$lang->setLangCache();
+		$modules = ACP3_Modules::getAllModules();
+		$installed_modules = $new_modules = array();
 
-	$mod_list = ACP3_Modules::getAllModules();
+		foreach ($modules as $key => $values) {
+			if ($db->countRows('*', 'modules', 'name = \'' . $values['dir'] . '\'') == 1) {
+				$installed_modules[$key] = $values;
+			} else {
+				$new_mods[$key] = $values;
+			}
+		}
 
-	$tpl->assign('LANG_modules_found', sprintf($lang->t('system', 'modules_found'), count($mod_list)));
-	$tpl->assign('modules', $mod_list);
+		$tpl->assign('installed_modules', $installed_modules);
+		$tpl->assign('new_modules', $new_modules);
 
-	ACP3_View::setContent(ACP3_View::fetchTemplate('system/modules.tpl'));
+		ACP3_View::setContent(ACP3_View::fetchTemplate('system/modules.tpl'));
 }
