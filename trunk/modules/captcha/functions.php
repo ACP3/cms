d68748d148b0c78122d078f501666c0cff1b88c0
tinogo@blue-cable.de <tinogo@blue-cable.de@849d3b1b-9637-0410-8226-95211594b0a1>
2012-08-08 20:50:13 +0000

    forgot to add this file

---
+++ trunk/modules/captcha/functions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Captcha
+ *
+ * @author Tino Goratsch
+ * @package ACP3
+ * @subpackage Modules
+ */
+
+if (defined('IN_ACP3') === false)
+	exit();
+
+/**
+ * Erzeugt das Captchafeld für das Template
+ *
+ * @param integer $captcha_length
+ *  Anzahl der Zeichen, welche das Captcha haben soll
+ * @return string
+ */
+function captcha($captcha_length = 5)
+{
+	global $auth, $tpl;
+
+	// Wenn man als User angemeldet ist, Captcha nicht anzeigen
+	if ($auth->isUser() === false) {
+		$_SESSION['captcha'] = salt($captcha_length);
+
+		$captcha = array();
+		$captcha['width'] = $captcha_length * 25;
+		$captcha['height'] = 30;
+		$tpl->assign('captcha', $captcha);
+		return ACP3_View::fetchTemplate('captcha/captcha.tpl');
+	}
+	return '';
+}
