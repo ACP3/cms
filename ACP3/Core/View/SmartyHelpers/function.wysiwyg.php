03ca7f775a2f2fbc6579b13dcff91a48b6c068c9
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2013-05-13 13:20:30 +0000

    WYSIWYG-Editor selection in System config page works again

--- ACP3/Core/View/SmartyHelpers/function.wysiwyg.php
+++ ACP3/Core/View/SmartyHelpers/function.wysiwyg.php
@@ -8,7 +8,7 @@ function smarty_function_wysiwyg($params) {
 	$params['id'] = !empty($params['id']) ? $params['id'] : $params['name'];
 
 	$wysiwyg = new \ACP3\Core\WYSIWYG();
-	\ACP3\Core\WYSIWYG::factory('TinyMCE', $params);
+	\ACP3\Core\WYSIWYG::factory(CONFIG_WYSIWYG, $params);
 	return $wysiwyg->display();
 }
 /* vim: set expandtab: */
