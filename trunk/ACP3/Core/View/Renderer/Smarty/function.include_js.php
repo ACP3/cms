55b28ba9a3cbf6d1888eda177ef955661336cab6
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-03-12 21:13:00 +0000

    - updated Smarty to version 3.1.17 - updated Minify to version 2.2.0

--- trunk/ACP3/Core/View/Renderer/Smarty/function.include_js.php
+++ trunk/ACP3/Core/View/Renderer/Smarty/function.include_js.php
@@ -15,9 +15,9 @@ function smarty_function_include_js($params)
         $module = ucfirst($params['module']);
         $moduleLower = strtolower($params['module']);
         $file = $params['file'];
-        if (is_file(DESIGN_PATH_INTERNAL . $moduleLower . '/' . $file . '.js') === true) {
+        if (file_exists(DESIGN_PATH_INTERNAL . $moduleLower . '/' . $file . '.js') === true) {
             return sprintf($script, DESIGN_PATH . $moduleLower . '/' . $file . '.js');
-        } elseif (is_file(MODULES_DIR . $module . '/View/' . $file . '.js') === true) {
+        } elseif (file_exists(MODULES_DIR . $module . '/View/' . $file . '.js') === true) {
             return sprintf($script, ROOT_DIR . 'ACP3/Modules/' . $module . '/View/' . $file . '.js');
         }
     } else {
