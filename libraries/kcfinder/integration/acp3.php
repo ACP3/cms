43009e7a9b49ebba7d620f538f28a0bc87f4f277
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-04-23 17:39:24 +0000

    updated KCFinder to version 3.11

--- libraries/kcfinder/integration/acp3.php
+++ libraries/kcfinder/integration/acp3.php
@@ -1,53 +1,58 @@
 <?php
+namespace kcfinder\cms;
 
-// gets a valid acp3_path
-function get_acp3_path() {
-	return dirname(__DIR__ . '/../../../../') . '/';
+/**
+ * Class ACP3
+ * @package kcfinder\cms
+ */
+class ACP3
+{
+    /**
+     * @var bool
+     */
+    protected static $authenticated = false;
+
+    /**
+     * @return bool
+     */
+    static function checkAuth()
+    {
+        $currentCwd = getcwd();
+        if (!self::$authenticated) {
+            define('IN_ACP3', true);
+            if (!defined('ACP3_ROOT_DIR')) {
+                define('ACP3_ROOT_DIR', dirname(__DIR__ . '/../../../../') . '/');
+            }
+
+            require_once ACP3_ROOT_DIR . 'ACP3/Application.php';
+
+            \ACP3\Application::defineDirConstants();
+            \ACP3\Application::startupChecks();
+            \ACP3\Application::includeAutoLoader();
+            \ACP3\Application::initializeClasses();
+
+            chdir(ACP3_ROOT_DIR);
+
+            // if user has access permission...
+            if (\ACP3\Core\Registry::get('Auth')->isUser()) {
+                if (!isset($_SESSION['KCFINDER'])) {
+                    $_SESSION['KCFINDER'] = array();
+                    $_SESSION['KCFINDER']['disabled'] = false;
+                }
+
+                // User has permission, so make sure KCFinder is not disabled!
+                $_SESSION['KCFINDER']['disabled'] = false;
+
+                chdir($currentCwd);
+
+                self::$authenticated = true;
+            }
+        }
+
+        chdir($currentCwd);
+        return self::$authenticated;
+    }
 }
 
-function CheckAuthentication($acp3_path) {
-
-	static $authenticated;
-
-	if (!isset($authenticated)) {
-
-		$current_cwd = getcwd();
-
-		define('IN_ACP3', true);
-		if (!defined('ACP3_ROOT_DIR'))
-			define('ACP3_ROOT_DIR', $acp3_path);
-
-		require_once ACP3_ROOT_DIR . 'ACP3/Application.php';
-
-		\ACP3\Application::defineDirConstants();
-		\ACP3\Application::startupChecks();
-		\ACP3\Application::includeAutoLoader();
-		\ACP3\Application::initializeClasses();
-
-		// Simulate being in the drupal root folder so we can share the session
-		chdir(ACP3_ROOT_DIR);
-
-		// if user has access permission...
-		if (\ACP3\Core\Registry::get('Auth')->isUser()) {
-			if (!isset($_SESSION['KCFINDER'])) {
-				$_SESSION['KCFINDER'] = array();
-				$_SESSION['KCFINDER']['disabled'] = false;
-			}
-
-			// User has permission, so make sure KCFinder is not disabled!
-			if (!isset($_SESSION['KCFINDER']['disabled'])) {
-				$_SESSION['KCFINDER']['disabled'] = false;
-			}
-
-			chdir($current_cwd);
-
-			return true;
-		}
-
-		chdir($current_cwd);
-		return false;
-	}
-}
-
-CheckAuthentication(get_acp3_path());
+\kcfinder\cms\ACP3::checkAuth();
 ?>
