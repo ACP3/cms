373ad46b8add428624e43c88f132c5a8d8a38e16
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-05-05 11:44:44 +0000

    minor bug and styling fixes

--- ACP3/Core/View/Renderer/Smarty/function.mark.php
+++ ACP3/Core/View/Renderer/Smarty/function.mark.php
@@ -1,8 +1,19 @@
 <?php
+/**
+ * @param $params
+ * @return mixed
+ */
 function smarty_function_mark($params)
 {
-    \ACP3\Core\Registry::get('View')->assign('checkbox_name', $params['name']);
-    \ACP3\Core\Registry::get('View')->assign('mark_all_id', !empty($params['mark_all_id']) ? $params['mark_all_id'] : 'mark-all');
-    return \ACP3\Core\Registry::get('View')->fetchTemplate('system/mark.tpl');
+    static $init = false;
+
+    $view = \ACP3\Core\Registry::get('View');
+    $view->assign('checkbox_name', $params['name']);
+    $view->assign('mark_all_id', !empty($params['mark_all_id']) ? $params['mark_all_id'] : 'mark-all');
+    $view->assign('is_initialized', $init);
+
+    $init = true;
+
+    return $view->fetchTemplate('system/mark.tpl');
 }
 /* vim: set expandtab: */
--- ACP3/Modules/System/View/mark.tpl
+++ ACP3/Modules/System/View/mark.tpl
@@ -7,6 +7,8 @@
             markEntries('#{$mark_all_id}', '{$checkbox_name}', $(this).is(':checked') ? 'add' : 'remove');
         }).highlightTableRow('{$checkbox_name}');
 
+        {if $is_initialized === false}
         $('form #adm-list input[type="image"]').deleteMarkedResults('{$checkbox_name}', '{lang t="system|confirm_delete"}', '{lang t="system|no_entries_selected"}');
+        {/if}
     });
 </script>
