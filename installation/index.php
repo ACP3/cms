5855b44962f259aedba3da4a6bec7235bb913f3e
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-08-09 10:42:01 +0000

    moved all installer related files into the installation folder

--- installation/index.php
+++ installation/index.php
@@ -9,7 +9,7 @@ define('IN_ACP3', true);
 define('IN_INSTALL', true);
 
 define('ACP3_ROOT_DIR', realpath(__DIR__ . '/../') . '/');
-require ACP3_ROOT_DIR . 'ACP3/Installer/Application.php';
+require ACP3_ROOT_DIR . 'installation/Installer/Application.php';
 
 $application = new \ACP3\Installer\Application();
 $application->runInstaller();
