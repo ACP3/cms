a9a6b3b85abc19004afc177f9624c16eeaa4e1c0
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-11-02 09:04:54 +0000

    - added a cache to the minify module to cache found assets paths to reduce filepath lookup costs

---
+++ ACP3/Modules/Minify/Cache.php
@@ -0,0 +1,44 @@
+<?php
+namespace ACP3\Modules\Minify;
+
+use ACP3\Core;
+
+/**
+ * Class Cache
+ * @package ACP3\Modules\Minify
+ */
+class Cache
+{
+    const CACHE_ID = 'assets';
+    /**
+     * @var \ACP3\Core\Cache
+     */
+    protected $cache;
+
+    public function __construct()
+    {
+        $this->cache = new Core\Cache('minify');
+    }
+
+    /**
+     * @return array
+     */
+    public function getCache()
+    {
+        if ($this->cache->contains(self::CACHE_ID) === true) {
+            return $this->cache->fetch(self::CACHE_ID);
+        }
+
+        return [];
+    }
+
+    /**
+     * @param array $paths
+     * @return bool
+     */
+    public function setCache(array $paths)
+    {
+        return $this->cache->save(self::CACHE_ID, $paths);
+    }
+
+}
