f896bece183e50238df83cf24c3f2e87f2915a6e
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2014-12-31 18:08:49 +0000

    - more di work

--- ACP3/Modules/Contact/Controller/Index.php
+++ ACP3/Modules/Contact/Controller/Index.php
@@ -3,6 +3,7 @@
 namespace ACP3\Modules\Contact\Controller;
 
 use ACP3\Core;
+use ACP3\Modules\Captcha;
 use ACP3\Modules\Contact;
 
 /**
@@ -16,6 +17,14 @@ class Index extends Core\Modules\Controller\Frontend
      */
     protected $secureHelper;
     /**
+     * @var \ACP3\Core\Helpers\SendEmail
+     */
+    protected $sendEmailHelper;
+    /**
+     * @var \ACP3\Modules\Contact\Validator
+     */
+    protected $contactValidator;
+    /**
      * @var \ACP3\Core\Config
      */
     protected $contactConfig;
@@ -23,24 +32,46 @@ class Index extends Core\Modules\Controller\Frontend
      * @var \ACP3\Core\Config
      */
     protected $seoConfig;
+    /**
+     * @var \ACP3\Modules\Captcha\Helpers
+     */
+    protected $captchaHelpers;
 
     /**
-     * @param \ACP3\Core\Context\Frontend $context
-     * @param \ACP3\Core\Helpers\Secure   $secureHelper
-     * @param \ACP3\Core\Config           $contactConfig
-     * @param \ACP3\Core\Config           $seoConfig
+     * @param \ACP3\Core\Context\Frontend     $context
+     * @param \ACP3\Core\Helpers\Secure       $secureHelper
+     * @param \ACP3\Core\Helpers\SendEmail    $sendEmailHelper
+     * @param \ACP3\Modules\Contact\Validator $contactValidator
+     * @param \ACP3\Core\Config               $contactConfig
+     * @param \ACP3\Core\Config               $seoConfig
      */
     public function __construct(
         Core\Context\Frontend $context,
         Core\Helpers\Secure $secureHelper,
+        Core\Helpers\SendEmail $sendEmailHelper,
+        Contact\Validator $contactValidator,
         Core\Config $contactConfig,
         Core\Config $seoConfig)
     {
         parent::__construct($context);
 
         $this->secureHelper = $secureHelper;
+        $this->sendEmailHelper = $sendEmailHelper;
+        $this->contactValidator = $contactValidator;
         $this->contactConfig = $contactConfig;
         $this->seoConfig = $seoConfig;
+    }
+
+    /**
+     * @param \ACP3\Modules\Captcha\Helpers $captchaHelpers
+     *
+     * @return $this
+     */
+    public function setCaptchaHelpers(Captcha\Helpers $captchaHelpers)
+    {
+        $this->captchaHelpers = $captchaHelpers;
+
+        return $this;
     }
 
     public function actionIndex()
@@ -71,7 +102,7 @@ class Index extends Core\Modules\Controller\Frontend
         $this->view->assign('copy_checked', $this->get('core.helpers.forms')->selectEntry('copy', 1, 0, 'checked'));
 
         if ($this->acl->hasPermission('frontend/captcha/index/image') === true) {
-            $this->view->assign('captcha', $this->get('captcha.helpers')->captcha());
+            $this->view->assign('captcha', $this->captchaHelpers->captcha());
         }
 
         $this->secureHelper->generateFormToken($this->request->query);
@@ -86,20 +117,19 @@ class Index extends Core\Modules\Controller\Frontend
             $seoSettings = $this->seoConfig->getSettings();
             $settings = $this->contactConfig->getSettings();
 
-            $validator = $this->get('contact.validator');
-            $validator->validate($formData);
+            $this->contactValidator->validate($formData);
 
             $formData['message'] = Core\Functions::strEncode($formData['message'], true);
 
             $subject = sprintf($this->lang->t('contact', 'contact_subject'), $seoSettings['title']);
             $body = str_replace(['{name}', '{mail}', '{message}', '\n'], [$formData['name'], $formData['mail'], $formData['message'], "\n"], $this->lang->t('contact', 'contact_body'));
-            $bool = $this->get('core.helpers.sendEmail')->execute('', $settings['mail'], $formData['mail'], $subject, $body);
+            $bool = $this->sendEmailHelper->execute('', $settings['mail'], $formData['mail'], $subject, $body);
 
             // Nachrichtenkopie an Absender senden
             if (isset($formData['copy'])) {
                 $subjectCopy = sprintf($this->lang->t('contact', 'sender_subject'), $seoSettings['title']);
                 $bodyCopy = sprintf($this->lang->t('contact', 'sender_body'), $seoSettings['title'], $formData['message']);
-                $this->get('core.helpers.sendEmail')->execute($formData['name'], $formData['mail'], $settings['mail'], $subjectCopy, $bodyCopy);
+                $this->sendEmailHelper->execute($formData['name'], $formData['mail'], $settings['mail'], $subjectCopy, $bodyCopy);
             }
 
             $this->secureHelper->unsetFormToken($this->request->query);
