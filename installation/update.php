abc8c76117db96df78b2af0d720e4b3ff4724822
tino.goratsch@hotmail.com <tino.goratsch@hotmail.com@849d3b1b-9637-0410-8226-95211594b0a1>
2015-01-15 15:12:55 +0000

    - code optimization

--- installation/update.php
+++ installation/update.php
@@ -5,12 +5,6 @@
  * @author Tino Goratsch
  */
 
-define('IN_ACP3', true);
-define('IN_INSTALL', true);
 define('IN_UPDATER', true);
 
-define('ACP3_ROOT_DIR', realpath(__DIR__ . '/../') . '/');
-require ACP3_ROOT_DIR . 'installation/Installer/Application.php';
-
-$application = new \ACP3\Installer\Application();
-$application->run();
+require 'index.php';
