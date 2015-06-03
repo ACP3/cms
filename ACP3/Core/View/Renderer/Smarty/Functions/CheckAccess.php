ef93a2c9b7281e8eee461864a204a9fedaefe1a3
Tino Goratsch <mail@goratschwebdesign.de>
2015-06-03 22:40:36 +0200

    - updated Smarty to version 3.1.24 - refactored some code - removed the obsolete Smarty plugin function "icon", as we are not using the crystal_project icons anymore

--- ACP3/Core/View/Renderer/Smarty/Functions/CheckAccess.php
+++ ACP3/Core/View/Renderer/Smarty/Functions/CheckAccess.php
@@ -10,38 +10,16 @@ use ACP3\Core;
 class CheckAccess extends AbstractFunction
 {
     /**
-     * @var \ACP3\Core\ACL
+     * @var \ACP3\Core\Helpers\View\CheckAccess
      */
-    protected $acl;
-    /**
-     * @var \ACP3\Core\Lang
-     */
-    protected $lang;
-    /**
-     * @var \ACP3\Core\Router
-     */
-    protected $router;
-    /**
-     * @var \ACP3\Core\View\Renderer\Smarty\Functions\Icon
-     */
-    protected $icon;
+    protected $checkAccess;
 
     /**
-     * @param \ACP3\Core\ACL                                 $acl
-     * @param \ACP3\Core\Lang                                $lang
-     * @param \ACP3\Core\Router                              $router
-     * @param \ACP3\Core\View\Renderer\Smarty\Functions\Icon $icon
+     * @param \ACP3\Core\Helpers\View\CheckAccess $checkAccess
      */
-    public function __construct(
-        Core\ACL $acl,
-        Core\Lang $lang,
-        Core\Router $router,
-        Icon $icon
-    ) {
-        $this->lang = $lang;
-        $this->acl = $acl;
-        $this->router = $router;
-        $this->icon = $icon;
+    public function __construct(Core\Helpers\View\CheckAccess $checkAccess)
+    {
+        $this->checkAccess = $checkAccess;
     }
 
     /**
@@ -57,65 +35,6 @@ class CheckAccess extends AbstractFunction
      */
     public function process(array $params, \Smarty_Internal_Template $smarty)
     {
-        if (isset($params['mode']) && isset($params['path'])) {
-            $action = [];
-            $query = explode('/', strtolower($params['path']));
-
-            if (isset($query[0]) && $query[0] === 'acp') {
-                $action[0] = (isset($query[1]) ? $query[1] : 'acp');
-                $action[1] = (isset($query[2]) ? $query[2] : 'index');
-                $action[2] = isset($query[3]) ? $query[3] : 'index';
-
-                $area = 'admin';
-            } else {
-                $action[0] = $query[0];
-                $action[1] = isset($query[1]) ? $query[1] : 'index';
-                $action[2] = isset($query[2]) ? $query[2] : 'index';
-
-                $area = 'frontend';
-            }
-
-            $permissionPath = $area . '/' . $action[0] . '/' . $action[1] . '/' . $action[2];
-
-            if ($this->acl->hasPermission($permissionPath) === true) {
-                $accessCheck = [];
-                $accessCheck['uri'] = $this->router->route($params['path']);
-
-                if (isset($params['icon'])) {
-                    $iconParams = $this->icon->getImageDimensions(
-                        $params['icon'],
-                        isset($params['width']) ? $params['width'] : '',
-                        isset($params['height']) ? $params['height'] : ''
-                    );
-
-                    $accessCheck['icon'] = $iconParams['path'];
-
-                    if ($params['mode'] === 'link') {
-                        $accessCheck['width'] = $iconParams['width'];
-                        $accessCheck['height'] = $iconParams['height'];
-                    }
-                }
-                if (isset($params['title'])) {
-                    $accessCheck['title'] = $params['title'];
-                }
-                if (isset($params['lang'])) {
-                    $langArray = explode('|', $params['lang']);
-                    $accessCheck['lang'] = $this->lang->t($langArray[0], $langArray[1]);
-                } else {
-                    $accessCheck['lang'] = $this->lang->t($action[0], $area . '_' . $action[1] . '_' . $action[2]);
-                }
-                if (isset($params['class'])) {
-                    $accessCheck['class'] = $params['class'];
-                }
-
-                $accessCheck['mode'] = $params['mode'];
-                $smarty->assign('access_check', $accessCheck);
-                return $smarty->fetch('asset:system/access_check.tpl');
-            } elseif ($params['mode'] === 'link' && isset($params['title'])) {
-                return $params['title'];
-            }
-        }
-
-        return '';
+        return $this->checkAccess->outputLinkOrButton($params);
     }
 }
--- ACP3/Modules/ACP3/System/Resources/View/access_check.tpl
+++ ACP3/Modules/ACP3/System/Resources/View/access_check.tpl
@@ -1,8 +1,5 @@
 {if $access_check.mode == 'link'}
     <a href="{$access_check.uri}" title="{$access_check.lang}">
-        {if isset($access_check.icon)}
-            <img src="{$access_check.icon}"{$access_check.width}{$access_check.height} alt="{$access_check.lang}">
-        {/if}
         {if isset($access_check.class)}
             <i class="{$access_check.class}" aria-hidden="true"></i>
         {/if}
@@ -10,8 +7,6 @@
             {$access_check.title}
         {/if}
     </a>
-{elseif $access_check.mode == 'input'}
-    <input type="image" src="{$access_check.icon}" alt="{$access_check.lang}" title="{$access_check.lang}" style="width:32px;height:32px">
 {elseif $access_check.mode == 'button'}
     <button type="submit" class="btn btn-link" title="{$access_check.lang}">
         {if isset($access_check.class)}
