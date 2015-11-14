fa317b1473a353bbd6dc65f4dccab3a945bc45f1
Tino Goratsch <mail@goratschwebdesign.de>
2015-11-14 09:52:13 +0100

    updated CKEditor to version 4.5.5

--- ACP3/Modules/ACP3/WYSIWYGCKEditor/Resources/Assets/js/ckeditor/plugins/codemirror/lang/de.js
+++ ACP3/Modules/ACP3/WYSIWYGCKEditor/Resources/Assets/js/ckeditor/plugins/codemirror/lang/de.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -7,6 +7,6 @@ CKEDITOR.plugins.setLang( 'codemirror', 'de', {
     searchCode: 'Quellcode durchsuchen',
 	autoFormat: 'Auswahl formatieren',
 	commentSelectedRange: 'Auswahl auskommentieren',
-	uncommentSelectedRange: 'Auskommentierung entferen',
+	uncommentSelectedRange: 'Auskommentierung entfernen',
 	autoCompleteToggle: 'HTML Tag Autovervollständigen de-/aktivieren'
 });
