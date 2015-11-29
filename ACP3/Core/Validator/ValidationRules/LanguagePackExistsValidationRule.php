7a4805abed61b18786cd43360b1940f2ff53392d
Tino Goratsch <mail@goratschwebdesign.de>
2015-11-29 17:41:29 +0100

    - converted the System module to use the new validation facility

---
+++ ACP3/Core/Validator/ValidationRules/LanguagePackExistsValidationRule.php
@@ -0,0 +1,40 @@
+<?php
+namespace ACP3\Core\Validator\ValidationRules;
+
+use ACP3\Core\Lang;
+
+/**
+ * Class LanguagePackExistsValidationRule
+ * @package ACP3\Core\Validator\ValidationRules
+ */
+class LanguagePackExistsValidationRule extends AbstractValidationRule
+{
+    const NAME = 'language_pack_exists';
+
+    /**
+     * @var \ACP3\Core\Lang
+     */
+    protected $lang;
+
+    /**
+     * LanguagePackExistsValidationRule constructor.
+     *
+     * @param \ACP3\Core\Lang $lang
+     */
+    public function __construct(Lang $lang)
+    {
+        $this->lang = $lang;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isValid($data, $field = '', array $extra = [])
+    {
+        if (is_array($data) && array_key_exists($field, $data)) {
+            return $this->isValid($data[$field], $field, $extra);
+        }
+
+        return $this->lang->languagePackExists($data);
+    }
+}
--- ACP3/Modules/ACP3/System/Validator/Settings.php
+++ ACP3/Modules/ACP3/System/Validator/Settings.php
@@ -2,6 +2,7 @@
 namespace ACP3\Modules\ACP3\System\Validator;
 
 use ACP3\Core;
+use ACP3\Modules\ACP3\System\Validator\ValidationRules\IsWysiwygEditorValidationRule;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -11,45 +12,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class Settings extends Core\Validator\AbstractValidator
 {
     /**
-     * @var Core\Validator\Rules\Date
-     */
-    protected $dateValidator;
-    /**
-     * @var Core\Validator\Rules\Router
-     */
-    protected $routerValidator;
-    /**
-     * @var \Symfony\Component\DependencyInjection\ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * Settings constructor.
-     *
-     * @param \ACP3\Core\Lang                                           $lang
-     * @param \ACP3\Core\Validator\Validator                            $validator
-     * @param \ACP3\Core\Validator\Rules\Misc                           $validate
-     * @param \ACP3\Core\Validator\Rules\Date                           $dateValidator
-     * @param \ACP3\Core\Validator\Rules\Router                         $routerValidator
-     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-     */
-    public function __construct(
-        Core\Lang $lang,
-        Core\Validator\Validator $validator,
-        Core\Validator\Rules\Misc $validate,
-        Core\Validator\Rules\Date $dateValidator,
-        Core\Validator\Rules\Router $routerValidator,
-        ContainerInterface $container
-    )
-    {
-        parent::__construct($lang, $validator, $validate);
-
-        $this->dateValidator = $dateValidator;
-        $this->routerValidator = $routerValidator;
-        $this->container = $container;
-    }
-
-    /**
      * @param array $formData
      *
      * @throws Core\Exceptions\InvalidFormToken
@@ -57,60 +19,183 @@ class Settings extends Core\Validator\AbstractValidator
      */
     public function validate(array $formData)
     {
-        $this->validateFormKey();
+        $this->validator->addConstraint(Core\Validator\ValidationRules\FormTokenValidationRule::NAME);
 
-        $this->errors = [];
-        if ($this->routerValidator->isInternalURI($formData['homepage']) === false) {
-            $this->errors['homepage'] = $this->lang->t('system', 'incorrect_homepage');
-        }
-        if ($this->validate->isNumber($formData['entries']) === false) {
-            $this->errors['entries'] = $this->lang->t('system', 'select_records_per_page');
-        }
-        if ($this->validate->isNumber($formData['flood']) === false) {
-            $this->errors['flood'] = $this->lang->t('system', 'type_in_flood_barrier');
-        }
-        if (empty($formData['wysiwyg']) ||
-            $this->container->has($formData['wysiwyg']) === false ||
-            !($this->container->get($formData['wysiwyg']) instanceof Core\WYSIWYG\AbstractWYSIWYG)
-        ) {
-            $this->errors['wysiwyg'] = $this->lang->t('system', 'select_editor');
-        }
-        if ($this->lang->languagePackExists($formData['language']) === false) {
-            $this->errors['language'] = $this->lang->t('system', 'select_language');
-        }
-        if (empty($formData['date_format_long'])) {
-            $this->errors['date-format-long'] = $this->lang->t('system', 'type_in_long_date_format');
-        }
-        if (empty($formData['date_format_short'])) {
-            $this->errors['date-format-short'] = $this->lang->t('system', 'type_in_short_date_format');
-        }
-        if ($this->dateValidator->timeZone($formData['date_time_zone']) === false) {
-            $this->errors['date-time-zone'] = $this->lang->t('system', 'select_time_zone');
-        }
-        if ($this->validate->isNumber($formData['maintenance_mode']) === false) {
-            $this->errors['maintenance-mode'] = $this->lang->t('system', 'select_online_maintenance');
-        }
-        if (strlen($formData['maintenance_message']) < 3) {
-            $this->errors['maintenance-message'] = $this->lang->t('system', 'maintenance_message_to_short');
-        }
-        if ($this->validate->isNumber($formData['cache_images']) === false) {
-            $this->errors['cache-images'] = $this->lang->t('system', 'select_cache_images');
-        }
-        if ($this->validate->isNumber($formData['cache_minify']) === false) {
-            $this->errors['cache-minify'] = $this->lang->t('system', 'type_in_minify_cache_lifetime');
-        }
+        $this->validateGeneralSettings($formData);
+        $this->validateDateSettings($formData);
+        $this->validateMaintenanceSettings($formData);
+        $this->validatePerformanceSettings($formData);
+        $this->validateMailerSettings($formData);
+
+        $this->validator->validate();
+    }
+
+    /**
+     * @param array $formData
+     */
+    protected function validateGeneralSettings(array $formData)
+    {
+        $this->validator
+            ->addConstraint(
+                Core\Validator\ValidationRules\InternalUriValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'homepage',
+                    'message' => $this->lang->t('system', 'incorrect_homepage')
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\IntegerValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'entries',
+                    'message' => $this->lang->t('system', 'select_records_per_page')
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\IntegerValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'flood',
+                    'message' => $this->lang->t('system', 'type_in_flood_barrier')
+                ])
+            ->addConstraint(
+                IsWysiwygEditorValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'wysiwyg',
+                    'message' => $this->lang->t('system', 'select_editor')
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\LanguagePackExistsValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'language',
+                    'message' => $this->lang->t('system', 'select_language')
+                ]);
+    }
+
+    /**
+     * @param array $formData
+     */
+    protected function validateDateSettings(array $formData)
+    {
+        $this->validator
+            ->addConstraint(
+                Core\Validator\ValidationRules\NotEmptyValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'date_format_long',
+                    'message' => $this->lang->t('system', 'type_in_long_date_format')
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\NotEmptyValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'date_format_short',
+                    'message' => $this->lang->t('system', 'type_in_short_date_format')
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\TimeZoneExistsValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'date_time_zone',
+                    'message' => $this->lang->t('system', 'select_time_zone')
+                ]);
+    }
+
+    /**
+     * @param array $formData
+     */
+    protected function validateMaintenanceSettings(array $formData)
+    {
+        $this->validator
+            ->addConstraint(
+                Core\Validator\ValidationRules\InArrayValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'maintenance_mode',
+                    'message' => $this->lang->t('system', 'select_online_maintenance'),
+                    'extra' => [
+                        'haystack' => [0, 1]
+                    ]
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\NotEmptyValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'maintenance_message',
+                    'message' => $this->lang->t('system', 'maintenance_message_to_short')
+                ]);
+    }
+
+    /**
+     * @param array $formData
+     */
+    protected function validatePerformanceSettings(array $formData)
+    {
+        $this->validator
+            ->addConstraint(
+                Core\Validator\ValidationRules\InArrayValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'cache_images',
+                    'message' => $this->lang->t('system', 'select_cache_images'),
+                    'extra' => [
+                        'haystack' => [0, 1]
+                    ]
+                ])
+            ->addConstraint(
+                Core\Validator\ValidationRules\IntegerValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'cache_minify',
+                    'message' => $this->lang->t('system', 'type_in_minify_cache_lifetime')
+                ]);
+    }
+
+    /**
+     * @param array $formData
+     */
+    protected function validateMailerSettings(array $formData)
+    {
+        $this->validator
+            ->addConstraint(
+                Core\Validator\ValidationRules\InArrayValidationRule::NAME,
+                [
+                    'data' => $formData,
+                    'field' => 'mailer_type',
+                    'message' => $this->lang->t('system', 'select_mailer_type'),
+                    'extra' => [
+                        'haystack' => ['mail', 'smtp']
+                    ]
+                ]);
+
         if ($formData['mailer_type'] === 'smtp') {
-            if (empty($formData['mailer_smtp_host'])) {
-                $this->errors['mailer-smtp-host'] = $this->lang->t('system', 'type_in_mailer_smtp_host');
-            }
-            if ($this->validate->isNumber($formData['mailer_smtp_port']) === false) {
-                $this->errors['mailer-smtp-port'] = $this->lang->t('system', 'type_in_mailer_smtp_port');
-            }
-            if ($formData['mailer_smtp_auth'] == 1 && empty($formData['mailer_smtp_user'])) {
-                $this->errors['mailer-smtp-username'] = $this->lang->t('system', 'type_in_mailer_smtp_username');
+            $this->validator
+                ->addConstraint(
+                    Core\Validator\ValidationRules\NotEmptyValidationRule::NAME,
+                    [
+                        'data' => $formData,
+                        'field' => 'mailer_smtp_host',
+                        'message' => $this->lang->t('system', 'type_in_mailer_smtp_host')
+                    ])
+                ->addConstraint(
+                    Core\Validator\ValidationRules\IntegerValidationRule::NAME,
+                    [
+                        'data' => $formData,
+                        'field' => 'mailer_smtp_port',
+                        'message' => $this->lang->t('system', 'type_in_mailer_smtp_port')
+                    ]);
+
+            if ($formData['mailer_smtp_auth'] == 1) {
+                $this->validator
+                    ->addConstraint(
+                        Core\Validator\ValidationRules\NotEmptyValidationRule::NAME,
+                        [
+                            'data' => $formData,
+                            'field' => 'mailer_smtp_user',
+                            'message' => $this->lang->t('system', 'type_in_mailer_smtp_username')
+                        ]);
             }
         }
-
-        $this->_checkForFailedValidation();
     }
 }
