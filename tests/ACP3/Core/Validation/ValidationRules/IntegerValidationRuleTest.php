285248910b27a9670af197c0a8187e0ce4a26d9e
Tino Goratsch <mail@goratschwebdesign.de>
2015-12-05 16:02:11 +0100

    added unit tests for the ChangePasswordValidationRule and IntegerValidationRule

---
+++ tests/ACP3/Core/Validation/ValidationRules/IntegerValidationRuleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class IntegerValidationRuleTest extends AbstractValidationRuleTest
+{
+
+    protected function setUp()
+    {
+        $this->validationRule = new \ACP3\Core\Validation\ValidationRules\IntegerValidationRule();
+    }
+
+    /**
+     * @return array
+     */
+    public function validationRuleProvider()
+    {
+        return [
+            'valid-data-integer' => [1, '', [], true],
+            'valid-data-string' => ['1', '', [], true],
+            'valid-data-array-integer' => [['foo' => 1], 'foo', [], true],
+            'valid-data-array-string' => [['foo' => '1'], 'foo', [], true],
+            'invalid-data-string' => ['foobar', '', [], false],
+            'invalid-data-float' => [0.01, '', [], false],
+            'invalid-data-float-as-string' => ['0.01', '', [], false],
+            'invalid-data-array' => [['foo' => 'foobar'], 'foo', [], false],
+            'invalid-no-data' => [null, null, [], false]
+        ];
+    }
+}
