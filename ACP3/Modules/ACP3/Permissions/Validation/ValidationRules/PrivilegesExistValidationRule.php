29ac8344d2c7efdd38e3bffc8982adf20c34c30b
Tino Goratsch <mail@goratschwebdesign.de>
2016-03-25 16:03:04 +0100

    minor improvements

--- ACP3/Modules/ACP3/Permissions/Validation/ValidationRules/PrivilegesExistValidationRule.php
+++ ACP3/Modules/ACP3/Permissions/Validation/ValidationRules/PrivilegesExistValidationRule.php
@@ -48,17 +48,12 @@ class PrivilegesExistValidationRule extends AbstractValidationRule
      */
     public function privilegesExist(array $privilegeIds)
     {
-        $allPrivileges = $this->privilegeRepository->getAllPrivileges();
         $valid = false;
-
-        foreach ($allPrivileges as $privilege) {
+        foreach ($this->privilegeRepository->getAllPrivileges() as $privilege) {
             $valid = false;
             foreach ($privilegeIds as $module) {
                 foreach ($module as $privilegeId => $permission) {
-                    if ($privilegeId == $privilege['id']
-                        && $permission >= ACL\PermissionEnum::DENY_ACCESS
-                        && $permission <= ACL\PermissionEnum::INHERIT_ACCESS
-                    ) {
+                    if ($this->isValidPrivilege($privilegeId, $privilege, $permission)) {
                         $valid = true;
                         break 2;
                     }
@@ -67,5 +62,19 @@ class PrivilegesExistValidationRule extends AbstractValidationRule
         }
 
         return $valid;
+    }
+
+    /**
+     * @param int   $privilegeId
+     * @param array $privilege
+     * @param int   $permission
+     *
+     * @return bool
+     */
+    protected function isValidPrivilege($privilegeId, array $privilege, $permission)
+    {
+        return $privilegeId == $privilege['id']
+        && $permission >= ACL\PermissionEnum::DENY_ACCESS
+        && $permission <= ACL\PermissionEnum::INHERIT_ACCESS;
     }
 }
