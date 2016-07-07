bd8d1fb2dd23554b0c5574b2a50cf39933974fbf
Tino Goratsch <mail@goratschwebdesign.de>
2016-07-07 20:33:25 +0200

    reworked the contact form so that it feature some more extension points

--- ACP3/Modules/ACP3/Contact/Controller/Frontend/Index/Index.php
+++ ACP3/Modules/ACP3/Contact/Controller/Frontend/Index/Index.php
@@ -21,10 +21,6 @@ class Index extends Core\Controller\AbstractFrontendAction
      */
     protected $formTokenHelper;
     /**
-     * @var \ACP3\Core\Helpers\SendEmail
-     */
-    protected $sendEmailHelper;
-    /**
      * @var \ACP3\Modules\ACP3\Contact\Validation\FormValidation
      */
     protected $formValidation;
@@ -32,27 +28,31 @@ class Index extends Core\Controller\AbstractFrontendAction
      * @var \ACP3\Core\Helpers\Forms
      */
     protected $formsHelper;
+    /**
+     * @var Contact\Model\ContactFormModel
+     */
+    protected $contactFormModel;
 
     /**
-     * @param \ACP3\Core\Controller\Context\FrontendContext        $context
-     * @param \ACP3\Core\Helpers\Forms                             $formsHelper
-     * @param \ACP3\Core\Helpers\FormToken                         $formTokenHelper
-     * @param \ACP3\Core\Helpers\SendEmail                         $sendEmailHelper
+     * @param \ACP3\Core\Controller\Context\FrontendContext $context
+     * @param \ACP3\Core\Helpers\Forms $formsHelper
+     * @param \ACP3\Core\Helpers\FormToken $formTokenHelper
      * @param \ACP3\Modules\ACP3\Contact\Validation\FormValidation $formValidation
+     * @param Contact\Model\ContactFormModel $contactFormModel
      */
     public function __construct(
         Core\Controller\Context\FrontendContext $context,
         Core\Helpers\Forms $formsHelper,
         Core\Helpers\FormToken $formTokenHelper,
-        Core\Helpers\SendEmail $sendEmailHelper,
-        Contact\Validation\FormValidation $formValidation
+        Contact\Validation\FormValidation $formValidation,
+        Contact\Model\ContactFormModel $contactFormModel
     ) {
         parent::__construct($context);
 
         $this->formsHelper = $formsHelper;
         $this->formTokenHelper = $formTokenHelper;
-        $this->sendEmailHelper = $sendEmailHelper;
         $this->formValidation = $formValidation;
+        $this->contactFormModel = $contactFormModel;
     }
 
     /**
@@ -64,6 +64,42 @@ class Index extends Core\Controller\AbstractFrontendAction
             return $this->indexPost($this->request->getPost()->all());
         }
 
+        return [
+            'form' => array_merge($this->getFormDefaults(), $this->request->getPost()->all()),
+            'copy_checked' => $this->formsHelper->selectEntry('copy', 1, 0, 'checked'),
+            'contact' => $this->config->getSettings('contact'),
+            'form_token' => $this->formTokenHelper->renderFormToken()
+        ];
+    }
+
+    /**
+     * @param array $formData
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    protected function indexPost(array $formData)
+    {
+        return $this->actionHelper->handlePostAction(
+            function () use ($formData) {
+                $bool = $this->contactFormModel->sendContactFormEmail($formData);
+
+                if (isset($formData['copy'])) {
+                    $this->contactFormModel->sendContactFormEmailCopy($formData);
+                }
+
+                $this->setTemplate($this->get('core.helpers.alerts')->confirmBox(
+                    $this->translator->t('contact', $bool === true ? 'send_mail_success' : 'send_mail_error'),
+                    $this->router->route('contact')
+                ));
+            }
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function getFormDefaults()
+    {
         $defaults = [
             'name' => '',
             'name_disabled' => '',
@@ -82,62 +118,6 @@ class Index extends Core\Controller\AbstractFrontendAction
             $defaults['mail_disabled'] = $disabled;
         }
 
-        return [
-            'form' => array_merge($defaults, $this->request->getPost()->all()),
-            'copy_checked' => $this->formsHelper->selectEntry('copy', 1, 0, 'checked'),
-            'contact' => $this->config->getSettings('contact'),
-            'form_token' => $this->formTokenHelper->renderFormToken()
-        ];
-    }
-
-    /**
-     * @param array $formData
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    protected function indexPost(array $formData)
-    {
-        return $this->actionHelper->handlePostAction(
-            function () use ($formData) {
-                $seoSettings = $this->config->getSettings('seo');
-                $settings = $this->config->getSettings('contact');
-
-                $this->formValidation->validate($formData);
-
-                $formData['message'] = $this->get('core.helpers.secure')->strEncode($formData['message'], true);
-
-                $subject = $this->translator->t('contact', 'contact_subject', ['%title%' => $seoSettings['title']]);
-                $body = $this->translator->t(
-                    'contact',
-                    'contact_body',
-                    [
-                        '%name%' => $formData['name'],
-                        '%mail%' => $formData['mail'],
-                        '%message%' => $formData['message']
-                    ]
-                );
-                $bool = $this->sendEmailHelper->execute('', $settings['mail'], $formData['mail'], $subject, $body);
-
-                // Nachrichtenkopie an Absender senden
-                if (isset($formData['copy'])) {
-                    $subjectCopy = $this->translator->t('contact', 'sender_subject',
-                        ['%title%' => $seoSettings['title']]);
-                    $bodyCopy = $this->translator->t(
-                        'contact',
-                        'sender_body',
-                        [
-                            '%title%' => $seoSettings['title'],
-                            '%message%' => $formData['message']
-                        ]
-                    );
-                    $this->sendEmailHelper->execute($formData['name'], $formData['mail'], $settings['mail'], $subjectCopy, $bodyCopy);
-                }
-
-                $this->setTemplate($this->get('core.helpers.alerts')->confirmBox(
-                    $this->translator->t('contact', $bool === true ? 'send_mail_success' : 'send_mail_error'),
-                    $this->router->route('contact')
-                ));
-            }
-        );
+        return $defaults;
     }
 }
