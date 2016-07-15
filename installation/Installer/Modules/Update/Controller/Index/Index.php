13a82f28e694bbbbde2c241d450e0e20ac005b43
Tino Goratsch <mail@goratschwebdesign.de>
2016-07-15 23:35:56 +0200

    reworked the schema updater to use the recursive module resolver

--- installation/Installer/Modules/Update/Controller/Index/Index.php
+++ installation/Installer/Modules/Update/Controller/Index/Index.php
@@ -7,9 +7,8 @@
 namespace ACP3\Installer\Modules\Update\Controller\Index;
 
 use ACP3\Core\Cache;
-use ACP3\Core\Filesystem;
-use ACP3\Core\Modules;
 use ACP3\Installer\Core;
+use ACP3\Installer\Modules\Update\Model\SchemaUpdateModel;
 
 /**
  * Class Index
@@ -18,112 +17,48 @@ use ACP3\Installer\Core;
 class Index extends Core\Controller\AbstractInstallerAction
 {
     /**
-     * @var \ACP3\Core\Modules
+     * @var SchemaUpdateModel
      */
-    protected $modules;
-    /**
-     * @var \ACP3\Core\Modules\SchemaUpdater
-     */
-    protected $schemaUpdater;
-    /**
-     * @var \ACP3\Core\Modules\Vendor
-     */
-    protected $vendors;
+    protected $schemaUpdateModel;
 
     /**
      * @param \ACP3\Installer\Core\Controller\Context\InstallerContext $context
-     * @param \ACP3\Core\Modules                                       $modules
-     * @param \ACP3\Core\Modules\Vendor                                $vendors
-     * @param \ACP3\Core\Modules\SchemaUpdater                         $schemaUpdater
+     * @param SchemaUpdateModel $schemaUpdateModel
      */
     public function __construct(
         Core\Controller\Context\InstallerContext $context,
-        Modules $modules,
-        Modules\Vendor $vendors,
-        Modules\SchemaUpdater $schemaUpdater
+        SchemaUpdateModel $schemaUpdateModel
     ) {
         parent::__construct($context);
 
-        $this->modules = $modules;
-        $this->vendors = $vendors;
-        $this->schemaUpdater = $schemaUpdater;
-    }
-
-    public function execute()
-    {
-        if ($this->request->getPost()->get('action') === 'confirmed') {
-            $this->executePost();
-        }
-    }
-
-    private function executePost()
-    {
-        $results = [];
-
-        // Zuerst die wichtigen System-Module aktualisieren...
-        $coreModules = ['system', 'users', 'permissions'];
-        foreach ($coreModules as $module) {
-            $results[$module] = $this->returnModuleUpdateResult($module);
-        }
-
-        // ...danach die Restlichen
-        foreach ($this->vendors->getVendors() as $vendor) {
-            foreach (Filesystem::scandir($this->appPath->getModulesDir() . $vendor . '/') as $module) {
-                if (in_array(strtolower($module), $coreModules) === false) {
-                    $results[$module] = $this->returnModuleUpdateResult($module);
-                }
-            }
-        }
-
-        ksort($results);
-
-        $this->view->assign('results', $results);
-
-        $this->clearCaches();
+        $this->schemaUpdateModel = $schemaUpdateModel;
     }
 
     /**
-     * @param string $moduleName
-     *
+     * @param string $action
      * @return array
      */
-    protected function returnModuleUpdateResult($moduleName)
+    public function execute($action = '')
     {
-        $result = $this->updateModule($moduleName);
-
-        return [
-            'text' => $this->translator->t('update', 'db_update_text', ['%module%' => ucfirst($moduleName)]),
-            'class' => $result === 1 ? 'success' : ($result === 0 ? 'danger' : 'info'),
-            'result_text' => $this->translator->t('update',
-                $result === 1 ? 'db_update_success' : ($result === 0 ? 'db_update_error' : 'db_update_no_update'))
-        ];
+        if ($action === 'confirmed') {
+            return $this->executePost();
+        }
     }
 
     /**
-     * Führt die Updateanweisungen eines Moduls aus
-     *
-     * @param string $module
-     *
-     * @return integer
+     * @return array
      */
-    protected function updateModule($module)
+    protected function executePost()
     {
-        $result = false;
+        $results = $this->schemaUpdateModel->updateModules($this->container);
+        ksort($results);
 
-        $serviceIdSchema = $module . '.installer.schema';
-        $serviceIdMigration = $module . '.installer.migration';
-        if ($this->container->has($serviceIdSchema) === true &&
-            $this->container->has($serviceIdMigration) === true) {
-            /** @var Modules\Installer\SchemaInterface $moduleSchema */
-            $moduleSchema = $this->container->get($serviceIdSchema);
-            /** @var Modules\Installer\MigrationInterface $moduleMigration */
-            $moduleMigration = $this->container->get($serviceIdMigration);
-            if ($this->modules->isInstalled($module) || count($moduleMigration->renameModule()) > 0) {
-                $result = $this->schemaUpdater->updateSchema($moduleSchema, $moduleMigration);
-            }
-        }
+        $this->setTemplate('Update/index.result.tpl');
+        $this->clearCaches();
 
-        return $result;
+        return [
+            'results' => $results
+        ];
     }
 
     protected function clearCaches()
