9bb98645d1524668104055cf2b70edfe8ee9219d
Tino Goratsch <mail@goratschwebdesign.de>
2016-09-01 21:25:24 +0200

    refactored the PictureSortColumnRenderer and made it more generic

--- ACP3/Modules/ACP3/Gallery/Helper/DataGrid/ColumnRenderer/PictureSortColumnRenderer.php
+++ ACP3/Modules/ACP3/Gallery/Helper/DataGrid/ColumnRenderer/PictureSortColumnRenderer.php
@@ -3,7 +3,6 @@ namespace ACP3\Modules\ACP3\Gallery\Helper\DataGrid\ColumnRenderer;
 
 use ACP3\Core\Helpers\DataGrid\ColumnRenderer\AbstractColumnRenderer;
 use ACP3\Core\I18n\Translator;
-use ACP3\Core\Router;
 use ACP3\Core\RouterInterface;
 
 /**
@@ -44,26 +43,57 @@ class PictureSortColumnRenderer extends AbstractColumnRenderer
 
         $value = '';
         if ($dbResultRow['last'] != $dbValue) {
-            $value .= '<a href="' . $this->router->route('acp/gallery/pictures/order/id_' . $dbResultRow[$this->primaryKey] . '/action_down') . '"
-                       title="' . $this->translator->t('system', 'move_down') . '"
-                       data-ajax-form="true"
-                       data-ajax-form-loading-text="' . $this->translator->t('system',
-                    'loading_please_wait') . '"><i class="glyphicon glyphicon-arrow-down" aria-hidden="true"></i></a>';
+            $value .= $this->fetchSortDirectionHtml(
+                $this->router->route(sprintf($column['custom']['route_sort_down'], $dbResultRow[$this->primaryKey])),
+                'down'
+            );
         }
         if ($dbResultRow['first'] != $dbValue) {
-            $value .= '<a href="' . $this->router->route('acp/gallery/pictures/order/id_' . $dbResultRow[$this->primaryKey] . '/action_up') . '"
-                       title="' . $this->translator->t('system', 'move_up') . '"
-                       data-ajax-form="true"
-                       data-ajax-form-loading-text="' . $this->translator->t('system',
-                    'loading_please_wait') . '"><i class="glyphicon glyphicon-arrow-up" aria-hidden="true"></i></a>';
+            $value .= $this->fetchSortDirectionHtml(
+                $this->router->route(sprintf($column['custom']['route_sort_up'], $dbResultRow[$this->primaryKey])),
+                'up'
+            );
         }
         if ($dbResultRow['first'] == $dbResultRow['last']) {
-            $value = '<i class="glyphicon glyphicon-remove-circle text-danger text-danger" aria-hidden="true" title="' . $this->translator->t('system',
-                    'move_impossible') . '"></i>';
+            $value = $this->fetchSortForbiddenHtml();
         }
 
         $column['attribute']['data-order'] = $dbResultRow['pic'];
 
         return $this->render($column, $value);
+    }
+
+    /**
+     * @param string $url
+     * @param string $direction
+     * @return string
+     */
+    protected function fetchSortDirectionHtml($url, $direction)
+    {
+        $html = <<<HTML
+<a href="%s" title="%s" data-ajax-form="true" data-ajax-form-loading-text="%s">
+    <i class="glyphicon glyphicon-arrow-%s" aria-hidden="true"></i>
+</a>
+HTML;
+
+        return sprintf(
+            $html,
+            $url,
+            $this->translator->t('system', 'move_' . $direction),
+            $this->translator->t('system', 'loading_please_wait'),
+            $direction
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function fetchSortForbiddenHtml()
+    {
+        $html = <<<HTML
+<i class="glyphicon glyphicon-remove-circle text-danger text-danger" aria-hidden="true" title="%s"></i>
+HTML;
+
+        return sprintf($html, $this->translator->t('system', 'move_impossible'));
     }
 }
