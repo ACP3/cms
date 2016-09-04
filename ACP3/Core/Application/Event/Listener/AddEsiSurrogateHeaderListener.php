3ea03be3dabecce0fbe7fa97dc236f61f732308a
Tino Goratsch <mail@goratschwebdesign.de>
2016-09-04 17:21:27 +0200

    refactored the esi surrogate header addition to use the core.application.controller_action_dispatcher.after_dispatch event

---
+++ ACP3/Core/Application/Event/Listener/AddEsiSurrogateHeaderListener.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) 2016 by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Core\Application\Event\Listener;
+
+
+use ACP3\Core\Application\Event\ControllerActionAfterDispatchEvent;
+use ACP3\Core\Controller\AreaEnum;
+use ACP3\Core\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class AddEsiSurrogateHeaderListener
+{
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * AddEsiSurrogateHeaderListener constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param ControllerActionAfterDispatchEvent $event
+     */
+    public function execute(ControllerActionAfterDispatchEvent $event)
+    {
+        $response = $event->getResponse();
+
+        if ($this->isExcludedFromEsi($response)) {
+            return;
+        }
+
+        $response->headers->set('Surrogate-Control', 'content="ESI/1.0"');
+    }
+
+    /**
+     * @param Response $response
+     * @return bool
+     */
+    private function isExcludedFromEsi(Response $response)
+    {
+        return $this->request->getArea() === AreaEnum::AREA_WIDGET
+        || $response instanceof BinaryFileResponse
+        || $response instanceof StreamedResponse;
+    }
+}
