3621ffd63e135a30cf5c1c005bf25e0e6ee0ba5e
tinogo <tino.goratsch@hotmail.com>
2016-09-23 11:38:34 +0200

    fix code coverage sending to coveralls

--- build/travis/push_code_coverage.sh
+++ build/travis/push_code_coverage.sh
@@ -4,5 +4,5 @@ TRAVIS_PHP_VERSION=$1
 
 if [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "7.1" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
 then
-    travis_retry php vendor/bin/coveralls -v
+    php vendor/bin/coveralls -v
 fi
