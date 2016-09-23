beccb97923aa96637cfbd30218bd4b1089381eb5
tinogo <tino.goratsch@hotmail.com>
2016-09-23 11:17:05 +0200

    refactored the travis.yml

---
+++ build/travis/push_code_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TRAVIS_PHP_VERSION=$1
+
+if [ "$TRAVIS_PHP_VERSION" != "hhvm" && "$TRAVIS_PHP_VERSION" != "7.1" && "$TRAVIS_PHP_VERSION" != "nightly" ]; then
+    travis_retry php vendor/bin/coveralls -v
+fi
