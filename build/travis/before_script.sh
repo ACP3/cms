8ac50be322974038c80b2dca4d6d2ed4e44d0dcd
tinogo <tino.goratsch@hotmail.com>
2016-09-23 11:21:09 +0200

    follow up improvements

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -2,7 +2,8 @@
 
 ACP3_CI_USER_GH_TOKEN=$1;
 
-if [ -n "$ACP3_CI_USER_GH_TOKEN" ]; then
+if [ -n ${ACP3_CI_USER_GH_TOKEN} ]
+then
     composer config github-oauth.github.com ${ACP3_CI_USER_GH_TOKEN}
 fi
 composer install --prefer-dist --ignore-platform-reqs -n -o
--- build/travis/push_code_coverage.sh
+++ build/travis/push_code_coverage.sh
@@ -2,6 +2,7 @@
 
 TRAVIS_PHP_VERSION=$1
 
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" && "$TRAVIS_PHP_VERSION" != "7.1" && "$TRAVIS_PHP_VERSION" != "nightly" ]; then
+if [ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "7.1" && ${TRAVIS_PHP_VERSION} != "nightly" ]
+then
     travis_retry php vendor/bin/coveralls -v
 fi
