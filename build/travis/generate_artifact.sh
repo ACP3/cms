087e5a19a9c1108b004fedaeec4227bb21a890c2
Tino Goratsch <mail@goratschwebdesign.de>
2016-10-02 16:49:31 +0200

    hopefully gets the github build artifacts working

---
+++ build/travis/generate_artifact.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TRAVIS_PHP_VERSION=$1
+
+if [[ ${TRAVIS_PHP_VERSION} = "7.0" ]]
+then
+    rm -rf build/logs/* ./vendor
+    rm -rf ./vendor
+    composer install --no-dev --prefer-dist -o -n --ignore-platform-reqs
+    zip release.zip ./*
+fi
