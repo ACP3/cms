7fa24aa63d18195cb5f22914794b494f250a1f4c
Tino Goratsch <mail@goratschwebdesign.de>
2016-10-03 18:08:06 +0200

    improved the generate_artifact.sh script

--- build/travis/generate_artifact.sh
+++ build/travis/generate_artifact.sh
@@ -4,8 +4,7 @@ TRAVIS_PHP_VERSION=$1
 
 if [[ ${TRAVIS_PHP_VERSION} = "7.0" ]]
 then
-    rm -rf build/logs/* ./vendor
-    rm -rf ./vendor
+    rm -rf build/logs/ ./vendor
     composer install --no-dev --prefer-dist -o -n --ignore-platform-reqs
-    zip -r release.zip ./*
+    zip -r release.zip . -x *.git* "auth.json" ".codeclimate.yml" ".coveralls.yml" ".gitattributes" ".travis.yml" "./build/travis/*"
 fi
