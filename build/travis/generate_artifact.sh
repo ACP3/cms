67ea157f4a6dd70a25d8ff6b4b6dde2f55dbb731
Tino Goratsch <mail@goratschwebdesign.de>
2016-10-24 00:06:39 +0200

    suppress the log spam of the zip command

--- build/travis/generate_artifact.sh
+++ build/travis/generate_artifact.sh
@@ -6,5 +6,5 @@ if [[ ${TRAVIS_PHP_VERSION} = "7.0" ]]
 then
     rm -rf build/logs/ ./vendor
     composer install --no-dev --prefer-dist -o -n --ignore-platform-reqs
-    zip -r release.zip . -x *.git* "auth.json" ".codeclimate.yml" ".coveralls.yml" ".gitattributes" ".travis.yml" "./build/travis/*"
+    zip -qr release.zip . -x *.git* "auth.json" ".codeclimate.yml" ".coveralls.yml" ".gitattributes" ".travis.yml" "./build/travis/*"
 fi
