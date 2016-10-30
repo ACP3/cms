948a5e1e3668aee98179fdf9e5351c009cb75bdc
Tino Goratsch <mail@goratschwebdesign.de>
2016-10-30 18:26:42 +0100

    do not trigger the page cache invalidation when in developer mode, as the page cache is disabled when the developer mode is engaged

--- ACP3/Modules/ACP3/System/Event/Listener/OnModelAfterSaveListener.php
+++ ACP3/Modules/ACP3/System/Event/Listener/OnModelAfterSaveListener.php
@@ -8,6 +8,7 @@ namespace ACP3\Modules\ACP3\System\Event\Listener;
 
 
 use ACP3\Core\Settings\SettingsInterface;
+use ACP3\Modules\ACP3\System\Helper\CanUsePageCache;
 use ACP3\Modules\ACP3\System\Installer\Schema;
 
 class OnModelAfterSaveListener
@@ -16,19 +17,27 @@ class OnModelAfterSaveListener
      * @var SettingsInterface
      */
     private $settings;
+    /**
+     * @var CanUsePageCache
+     */
+    private $canUsePageCache;
 
     /**
      * OnModelAfterSaveListener constructor.
      * @param SettingsInterface $settings
+     * @param CanUsePageCache $canUsePageCache
      */
-    public function __construct(SettingsInterface $settings)
+    public function __construct(
+        SettingsInterface $settings,
+        CanUsePageCache $canUsePageCache)
     {
         $this->settings = $settings;
+        $this->canUsePageCache = $canUsePageCache;
     }
 
     public function invalidatePageCache()
     {
-        if ($this->settings->getSettings(Schema::MODULE_NAME)['page_cache_is_enabled'] == 1) {
+        if ($this->canUsePageCache->canUsePageCache()) {
             $this->settings->saveSettings(['page_cache_is_valid' => false], Schema::MODULE_NAME);
         }
     }
