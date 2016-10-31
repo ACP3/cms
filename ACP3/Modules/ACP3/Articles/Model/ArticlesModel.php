c6206d7df1190e8dd5232d5305d8fbb08291dca6
Tino Goratsch <mail@goratschwebdesign.de>
2016-10-31 23:15:36 +0100

    refs #12 - added the updated_at column to the articles module - use the update_at column in the article module's SitemapAvailabilityExtension

--- ACP3/Modules/ACP3/Articles/Model/ArticlesModel.php
+++ ACP3/Modules/ACP3/Articles/Model/ArticlesModel.php
@@ -16,6 +16,21 @@ class ArticlesModel extends AbstractModel
     const EVENT_PREFIX = Schema::MODULE_NAME;
 
     /**
+     * @inheritdoc
+     */
+    public function save(array $rawData, $entryId = null)
+    {
+        $rawData = array_merge(
+            $rawData,
+            [
+                'updated_at' => 'now'
+            ]
+        );
+
+        return parent::save($rawData, $entryId);
+    }
+
+    /**
      * @return array
      */
     protected function getAllowedColumns()
@@ -23,6 +38,7 @@ class ArticlesModel extends AbstractModel
         return [
             'start' => DataProcessor\ColumnTypes::COLUMN_TYPE_DATETIME,
             'end' => DataProcessor\ColumnTypes::COLUMN_TYPE_DATETIME,
+            'updated_at' => DataProcessor\ColumnTypes::COLUMN_TYPE_DATETIME,
             'title' => DataProcessor\ColumnTypes::COLUMN_TYPE_TEXT,
             'text' => DataProcessor\ColumnTypes::COLUMN_TYPE_TEXT_WYSIWYG,
             'user_id' => DataProcessor\ColumnTypes::COLUMN_TYPE_INT
