03ede8771f0d8f958813cbdedac67c64440369b7
Tino Goratsch <mail@goratschwebdesign.de>
2016-11-07 03:41:40 +0100

    increased the unit test coverage of two classes

---
+++ ACP3/Core/Test/View/Renderer/Smarty/AbstractPluginTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2016 by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Core\Test\View\Renderer\Smarty;
+
+
+use ACP3\Core\View\Renderer\Smarty\PluginInterface;
+
+abstract class AbstractPluginTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PluginInterface
+     */
+    protected $plugin;
+
+    public function testExtensionName()
+    {
+        $this->assertEquals($this->getExpectedExtensionName(), $this->plugin->getExtensionName());
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getExpectedExtensionName();
+}
