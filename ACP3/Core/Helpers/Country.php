b56f4b396f175f82427dd3689c27947fdc8cdca0
Tino Goratsch <mail@goratschwebdesign.de>
2017-02-25 15:34:16 +0100

    secured the country list

--- ACP3/Core/Helpers/Country.php
+++ ACP3/Core/Helpers/Country.php
@@ -1,24 +1,24 @@
 <?php
 namespace ACP3\Core\Helpers;
 
-/**
- * Class Country
- * @package ACP3\Core\Helpers
- */
 class Country
 {
     /**
-     * Returns an array with all countries on earth
+     * Returns an array with all earth countries
      *
      * @param string $locale
      * @return array
      */
     public static function worldCountries($locale = 'en_US')
     {
-        $countries = file_get_contents(
-            ACP3_ROOT_DIR . 'vendor/umpirsky/country-list/data/' . $locale . '/country.json'
-        );
+        $path = ACP3_ROOT_DIR . 'vendor/umpirsky/country-list/data/' . $locale . '/country.json';
 
-        return json_decode($countries, true);
+        if (preg_match('/^[a-z]{2}_[A-Z]{2}/', $locale) && is_file($path)) {
+            $countries = file_get_contents($path);
+
+            return json_decode($countries, true);
+        }
+
+        return [];
     }
 }
