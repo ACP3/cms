6e97a752f7e583cb635b90a7e2e7e983ca8ba6ba
Tino Goratsch <mail@goratschwebdesign.de>
2017-03-26 23:24:29 +0200

    next try...

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ACP3_CI_USER_GH_TOKEN=$1;
+TRAVIS_PHP_VERSION=$2;
+
+if [[ ${TRAVIS_PHP_VERSION} = "7.0" || ${TRAVIS_PHP_VERSION} != "7.1" ]]
+then
+    phpenv config-rm xdebug.ini
+fi
 
 if [[ -n ${ACP3_CI_USER_GH_TOKEN} ]]
 then
--- build/travis/run_unit_tests.sh
+++ build/travis/run_unit_tests.sh
@@ -2,12 +2,11 @@
 
 TRAVIS_PHP_VERSION=$1
 
-if [[ ${TRAVIS_PHP_VERSION} = "5.6" ]]
+if [[ ${TRAVIS_PHP_VERSION} = "5.6" || ${TRAVIS_PHP_VERSION} = "hhvm" ]]
 then
     php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
-elif [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
+elif [[ ${TRAVIS_PHP_VERSION} != "nightly" ]]
 then
-    phpenv config-rm xdebug.ini
     phpdbg -qrr ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
 else
     php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml
