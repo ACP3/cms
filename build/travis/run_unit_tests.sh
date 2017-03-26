19d5c687aac1c78a28e5455b2190936e73283489
Tino Goratsch <mail@goratschwebdesign.de>
2017-03-26 23:09:34 +0200

    lets try phpdbg

--- build/travis/run_unit_tests.sh
+++ build/travis/run_unit_tests.sh
@@ -2,9 +2,9 @@
 
 TRAVIS_PHP_VERSION=$1
 
-if [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "7.1" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
+if [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
 then
-    php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
+    phpdbg ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
 else
     php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml
 fi
