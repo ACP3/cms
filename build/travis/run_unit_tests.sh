18efcbcaca8f3809d033791ad21401ec73a194f5
Tino Goratsch <mail@goratschwebdesign.de>
2017-03-26 23:14:41 +0200

    lets try it again

--- build/travis/run_unit_tests.sh
+++ build/travis/run_unit_tests.sh
@@ -2,9 +2,13 @@
 
 TRAVIS_PHP_VERSION=$1
 
-if [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
+if [[ ${TRAVIS_PHP_VERSION} = "5.6" ]]
 then
-    phpdbg ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
+    php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
+elif [[ ${TRAVIS_PHP_VERSION} != "hhvm" && ${TRAVIS_PHP_VERSION} != "nightly" ]]
+then
+    phpenv config-rm xdebug.ini
+    phpdbg -qrr ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
 else
     php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml
 fi
