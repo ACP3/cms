ebbdd8c5d97ed8f3d6ee0d678d0da083d796a843
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-02 21:30:30 +0200

    replaced all occurrences of "ACP3 4.0" with "ACP3 CMS"

--- ACP3/Core/Test/Helpers/StringFormatterTest.php
+++ ACP3/Core/Test/Helpers/StringFormatterTest.php
@@ -15,7 +15,7 @@ class StringFormatterTest extends \PHPUnit_Framework_TestCase
      * @var string
      */
     private $shortenEntryText = <<<HTML
-<p>It looks like that the installation of the ACP3 4.0 was successful.<br>
+<p>It looks like that the installation of the ACP3 CMS was successful.<br>
 This is just a test news, you can now edit or delete in the administration area.</p>
 <div class="panel panel-default">
     <div class="panel-heading">
@@ -116,21 +116,21 @@ HTML;
                 $this->shortenEntryText,
                 0,
                 '',
-                'It looks like that the installation of the ACP3 4.0 was successful.
+                'It looks like that the installation of the ACP3 CMS was successful.
 This is just a test news, you ca'
             ],
             'shorten_text_with_offset' => [
                 $this->shortenEntryText,
                 10,
                 '',
-                'It looks like that the installation of the ACP3 4.0 was successful.
+                'It looks like that the installation of the ACP3 CMS was successful.
 This is just a test ne'
             ],
             'append_text' => [
                 $this->shortenEntryText,
                 0,
                 '...',
-                'It looks like that the installation of the ACP3 4.0 was successful.
+                'It looks like that the installation of the ACP3 CMS was successful.
 This is just a test news, you ca...'
             ]
         ];
