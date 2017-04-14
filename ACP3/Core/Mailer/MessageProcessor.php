a25c63ef659b408fb2f2ca2364b62fa7a39c427e
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-14 17:46:27 +0200

    covered the text email sending logic with unit tests

--- ACP3/Core/Mailer/MessageProcessor.php
+++ ACP3/Core/Mailer/MessageProcessor.php
@@ -27,7 +27,7 @@ class MessageProcessor
     private $inlineStyle;
 
     /**
-     * MessageParser constructor.
+     * MessageProcessor constructor.
      * @param InlineStyle $inlineStyle
      * @param StringFormatter $stringFormatter
      * @param View $view
@@ -51,36 +51,10 @@ class MessageProcessor
      */
     public function process(\PHPMailer $phpMailer, MailerMessage $message)
     {
-        $phpMailer->Subject = $this->getSubject($message->getSubject());
+        $phpMailer->Subject = $this->encodeSubject($message->getSubject());
 
         if (!empty($message->getTemplate())) {
-            $mail = [
-                'charset' => 'UTF-8',
-                'title' => $message->getSubject(),
-                'body' => !empty($message->getHtmlBody()) ?
-                    $message->getHtmlBody() :
-                    $this->stringFormatter->nl2p($message->getBody()),
-                'signature' => $this->getHtmlSignature($message->getMailSignature()),
-                'url_web_view' => $message->getUrlWeb()
-            ];
-            $this->view->assign('mail', $mail);
-
-            $this->inlineStyle->loadHTML($this->view->fetchTemplate($message->getTemplate()));
-            $this->inlineStyle->applyStylesheet($this->inlineStyle->extractStylesheets());
-
-            $phpMailer->msgHTML($this->inlineStyle->getHTML());
-
-            // Fallback for E-mail clients which don't support HTML E-mails
-            if (!empty($message->getBody())) {
-                $phpMailer->AltBody = $this->decodeHtmlEntities(
-                    $message->getBody() . $this->getTextSignature($phpMailer, $message->getMailSignature())
-                );
-            } else {
-                $phpMailer->AltBody = $phpMailer->html2text(
-                    $message->getHtmlBody() . $this->getHtmlSignature($message->getMailSignature()),
-                    true
-                );
-            }
+            $this->processHtmlEmail($phpMailer, $message);
         } else {
             $phpMailer->Body = $this->decodeHtmlEntities(
                 $message->getBody() . $this->getTextSignature($phpMailer, $message->getMailSignature())
@@ -92,7 +66,7 @@ class MessageProcessor
      * @param string $subject
      * @return string
      */
-    private function getSubject(string $subject): string
+    private function encodeSubject(string $subject): string
     {
         return "=?utf-8?b?" . base64_encode($this->decodeHtmlEntities($subject)) . "?=";
     }
@@ -104,6 +78,41 @@ class MessageProcessor
     private function decodeHtmlEntities(string $data): string
     {
         return html_entity_decode($data, ENT_QUOTES, 'UTF-8');
+    }
+
+    /**
+     * @param \PHPMailer $phpMailer
+     * @param MailerMessage $message
+     */
+    private function processHtmlEmail(\PHPMailer $phpMailer, MailerMessage $message)
+    {
+        $mail = [
+            'charset' => 'UTF-8',
+            'title' => $message->getSubject(),
+            'body' => !empty($message->getHtmlBody()) ?
+                $message->getHtmlBody() :
+                $this->stringFormatter->nl2p($message->getBody()),
+            'signature' => $this->getHtmlSignature($message->getMailSignature()),
+            'url_web_view' => $message->getUrlWeb()
+        ];
+        $this->view->assign('mail', $mail);
+
+        $this->inlineStyle->loadHTML($this->view->fetchTemplate($message->getTemplate()));
+        $this->inlineStyle->applyStylesheet($this->inlineStyle->extractStylesheets());
+
+        $phpMailer->msgHTML($this->inlineStyle->getHTML());
+
+        // Fallback for E-mail clients which don't support HTML E-mails
+        if (!empty($message->getBody())) {
+            $phpMailer->AltBody = $this->decodeHtmlEntities(
+                $message->getBody() . $this->getTextSignature($phpMailer, $message->getMailSignature())
+            );
+        } else {
+            $phpMailer->AltBody = $phpMailer->html2text(
+                $message->getHtmlBody() . $this->getHtmlSignature($message->getMailSignature()),
+                true
+            );
+        }
     }
 
     /**
---
+++ ACP3/Core/Test/Mailer/MessageProcessorTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Core\Test\Mailer;
+
+
+use ACP3\Core\Helpers\StringFormatter;
+use ACP3\Core\Mailer\MailerMessage;
+use ACP3\Core\Mailer\MessageProcessor;
+use ACP3\Core\View;
+use InlineStyle\InlineStyle;
+
+class MessageProcessorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MessageProcessor
+     */
+    private $messageProcessor;
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\InlineStyle\InlineStyle
+     */
+    private $inlineStyleMock;
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|StringFormatter
+     */
+    private $stringFormatterMock;
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|View
+     */
+    private $viewMock;
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|\PHPMailer
+     */
+    private $phpMailerMock;
+
+    protected function setUp()
+    {
+        $this->setUpMockObjects();
+
+        $this->messageProcessor = new MessageProcessor(
+            $this->inlineStyleMock,
+            $this->stringFormatterMock,
+            $this->viewMock
+        );
+    }
+
+    private function setUpMockObjects()
+    {
+        $this->inlineStyleMock = $this->getMockBuilder(InlineStyle::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->stringFormatterMock = $this->getMockBuilder(StringFormatter::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->viewMock = $this->getMockBuilder(View::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->phpMailerMock = $this->getMockBuilder(\PHPMailer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testTextMessageWithoutSignature()
+    {
+        $message = (new MailerMessage())
+            ->setSubject('Foo')
+            ->setBody('Foobarbaz');
+
+        $this->phpMailerMock->expects($this->never())
+            ->method('html2text');
+
+        $this->messageProcessor->process($this->phpMailerMock, $message);
+
+        $this->assertEquals("Foobarbaz", $this->phpMailerMock->Body);
+    }
+
+    public function testTextMessageWithSignature()
+    {
+        $message = (new MailerMessage())
+            ->setSubject('Foo')
+            ->setBody('Foobarbaz')
+            ->setMailSignature('Mail-Signature');
+
+        $this->phpMailerMock->expects($this->once())
+            ->method('html2text')
+            ->with('Mail-Signature')
+            ->willReturn('Mail-Signature');
+
+        $this->messageProcessor->process($this->phpMailerMock, $message);
+
+        $this->assertEquals("Foobarbaz\n-- \nMail-Signature", $this->phpMailerMock->Body);
+    }
+}
