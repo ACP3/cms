98980fa4b7bfe891241b9c3569e849a3df4e37d5
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-15 13:15:19 +0200

    revert

--- build/travis/generate_api_docs.sh
+++ build/travis/generate_api_docs.sh
@@ -6,14 +6,14 @@ REPO_DIR='apidocs'
 curl -O http://get.sensiolabs.org/sami.phar
 php sami.phar update .sami.php
 
-#git clone https://github.com/ACP3/${REPO_DIR}.git ./build/${REPO_DIR}
-#cd ./build/${REPO_DIR}
-#git checkout -q -b ${BRANCH_NAME}
-#git rm -q -r .
-#cp -a ../sami/docs/* .
-#git add .
-#git commit -am "Updated the API docs"
-#git checkout -q master
-#git merge -q ${BRANCH_NAME}
-#git branch -q -d ${BRANCH_NAME}
-#git push --all
+git clone https://github.com/ACP3/${REPO_DIR}.git ./build/${REPO_DIR}
+cd ./build/${REPO_DIR}
+git checkout -q -b ${BRANCH_NAME}
+git rm -q -r .
+cp -a ../sami/docs/* .
+git add .
+git commit -am "Updated the API docs"
+git checkout -q master
+git merge -q ${BRANCH_NAME}
+git branch -q -d ${BRANCH_NAME}
+git push --all
