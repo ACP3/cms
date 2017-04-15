7210d2654896a24ee388591d8313f4a8a1f27c8b
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-15 13:05:27 +0200

    next try...

--- build/travis/generate_api_docs.sh
+++ build/travis/generate_api_docs.sh
@@ -4,13 +4,13 @@ BRANCH_NAME='sami-update'
 REPO_DIR='apidocs'
 
 curl -O http://get.sensiolabs.org/sami.phar
+php sami.phar update .sami.php
 
 git clone https://github.com/ACP3/${REPO_DIR}.git ./build/${REPO_DIR}
 cd ./build/${REPO_DIR}
 git checkout -q -b ${BRANCH_NAME}
 git rm -q -r .
-php ../../sami.phar update ../../.sami.php
-cp -r ../sami/docs/* .
+cp -a ../sami/docs/* .
 git add .
 git commit -am "Updated the API docs"
 git checkout -q master
