ddae9c3d31c8587c4835417570aa936bb1f81a56
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-22 23:07:12 +0200

    added a view block for the files data grid admin action

--- ACP3/Modules/ACP3/Files/Controller/Admin/Index/Index.php
+++ ACP3/Modules/ACP3/Files/Controller/Admin/Index/Index.php
@@ -20,20 +20,27 @@ class Index extends Core\Controller\AbstractAdminAction
      * @var \ACP3\Modules\ACP3\Files\Model\Repository\DataGridRepository
      */
     protected $dataGridRepository;
+    /**
+     * @var Core\View\Block\DataGridBlockInterface
+     */
+    private $block;
 
     /**
      * Index constructor.
      *
-     * @param \ACP3\Core\Controller\Context\FrontendContext        $context
+     * @param \ACP3\Core\Controller\Context\FrontendContext $context
+     * @param Core\View\Block\DataGridBlockInterface $block
      * @param \ACP3\Modules\ACP3\Files\Model\Repository\DataGridRepository $dataGridRepository
      */
     public function __construct(
         Core\Controller\Context\FrontendContext $context,
+        Core\View\Block\DataGridBlockInterface $block,
         Files\Model\Repository\DataGridRepository $dataGridRepository)
     {
         parent::__construct($context);
 
         $this->dataGridRepository = $dataGridRepository;
+        $this->block = $block;
     }
 
     /**
@@ -41,84 +48,8 @@ class Index extends Core\Controller\AbstractAdminAction
      */
     public function execute()
     {
-        /** @var Core\Helpers\DataGrid $dataGrid */
-        $dataGrid = $this->get('core.helpers.data_grid');
-        $dataGrid
-            ->setRepository($this->dataGridRepository)
-            ->setRecordsPerPage($this->resultsPerPage->getResultsPerPage(Schema::MODULE_NAME))
-            ->setIdentifier('#files-data-grid')
-            ->setResourcePathDelete('admin/files/index/delete')
-            ->setResourcePathEdit('admin/files/index/edit');
-
-        $this->addDataGridColumns($dataGrid);
-
-        return [
-            'grid' => $dataGrid->render(),
-            'show_mass_delete_button' => $dataGrid->countDbResults() > 0
-        ];
-    }
-
-    /**
-     * @param Core\Helpers\DataGrid $dataGrid
-     */
-    protected function addDataGridColumns(Core\Helpers\DataGrid $dataGrid)
-    {
-        $settings = $this->config->getSettings(Files\Installer\Schema::MODULE_NAME);
-
-        $dataGrid
-            ->addColumn([
-                'label' => $this->translator->t('files', 'active'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\ReplaceValueColumnRenderer::class,
-                'fields' => ['active'],
-                'custom' => [
-                    'search' => [0, 1],
-                    'replace' => [$this->translator->t('system', 'no'), $this->translator->t('system', 'yes')]
-                ]
-            ], 60)
-            ->addColumn([
-                'label' => $this->translator->t('system', 'publication_period'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\DateColumnRenderer::class,
-                'fields' => ['start', 'end'],
-                'default_sort' => $settings['order_by'] === 'date',
-                'default_sort_direction' => 'desc'
-            ], 50)
-            ->addColumn([
-                'label' => $this->translator->t('files', 'title'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
-                'fields' => ['title'],
-            ], 40)
-            ->addColumn([
-                'label' => $this->translator->t('system', 'description'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
-                'fields' => ['description'],
-            ], 30)
-            ->addColumn([
-                'label' => $this->translator->t('files', 'filesize'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
-                'fields' => ['size'],
-                'customer' => [
-                    'default_value' => $this->translator->t('files', 'unknown_filesize')
-                ]
-            ], 20)
-            ->addColumn([
-                'label' => $this->translator->t('system', 'id'),
-                'type' => Core\Helpers\DataGrid\ColumnRenderer\IntegerColumnRenderer::class,
-                'fields' => ['id'],
-                'primary' => true
-            ], 10);
-
-        if ($this->acl->hasPermission('admin/files/index/sort') && $settings['order_by'] === 'custom') {
-            $dataGrid
-                ->addColumn([
-                    'label' => $this->translator->t('system', 'order'),
-                    'type' => Core\Helpers\DataGrid\ColumnRenderer\SortColumnRenderer::class,
-                    'fields' => ['sort'],
-                    'default_sort' => $settings['order_by'] === 'custom',
-                    'custom' => [
-                        'route_sort_down' => 'acp/files/index/sort/id_%d/action_down',
-                        'route_sort_up' => 'acp/files/index/sort/id_%d/action_up',
-                    ]
-                ], 15);
-        }
+        return $this->block
+            ->setDataGridRepository($this->dataGridRepository)
+            ->render();
     }
 }
---
+++ ACP3/Modules/ACP3/Files/View/Block/Admin/FilesDataGridBlock.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Files\View\Block\Admin;
+
+use ACP3\Core;
+use ACP3\Core\Helpers\DataGrid;
+use ACP3\Core\View\Block\AbstractDataGridBlock;
+use ACP3\Core\View\Block\Context;
+use ACP3\Modules\ACP3\Files\Installer\Schema;
+
+class FilesDataGridBlock extends AbstractDataGridBlock
+{
+    /**
+     * @var Core\ACL
+     */
+    private $acl;
+    /**
+     * @var Core\Settings\SettingsInterface
+     */
+    private $settings;
+
+    /**
+     * FilesDataGridBlock constructor.
+     * @param Context\DataGridBlockContext $context
+     * @param Core\ACL $acl
+     * @param Core\Settings\SettingsInterface $settings
+     */
+    public function __construct(
+        Context\DataGridBlockContext $context,
+        Core\ACL $acl,
+        Core\Settings\SettingsInterface $settings
+    ) {
+        parent::__construct($context);
+
+        $this->acl = $acl;
+        $this->settings = $settings;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function addDataGridColumns(DataGrid $dataGrid)
+    {
+        $settings = $this->settings->getSettings($this->getModuleName());
+
+        $dataGrid
+            ->addColumn([
+                'label' => $this->translator->t('files', 'active'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\ReplaceValueColumnRenderer::class,
+                'fields' => ['active'],
+                'custom' => [
+                    'search' => [0, 1],
+                    'replace' => [$this->translator->t('system', 'no'), $this->translator->t('system', 'yes')]
+                ]
+            ], 60)
+            ->addColumn([
+                'label' => $this->translator->t('system', 'publication_period'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\DateColumnRenderer::class,
+                'fields' => ['start', 'end'],
+                'default_sort' => $settings['order_by'] === 'date',
+                'default_sort_direction' => 'desc'
+            ], 50)
+            ->addColumn([
+                'label' => $this->translator->t('files', 'title'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
+                'fields' => ['title'],
+            ], 40)
+            ->addColumn([
+                'label' => $this->translator->t('system', 'description'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
+                'fields' => ['description'],
+            ], 30)
+            ->addColumn([
+                'label' => $this->translator->t('files', 'filesize'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\TextColumnRenderer::class,
+                'fields' => ['size'],
+                'customer' => [
+                    'default_value' => $this->translator->t('files', 'unknown_filesize')
+                ]
+            ], 20)
+            ->addColumn([
+                'label' => $this->translator->t('system', 'id'),
+                'type' => Core\Helpers\DataGrid\ColumnRenderer\IntegerColumnRenderer::class,
+                'fields' => ['id'],
+                'primary' => true
+            ], 10);
+
+        if ($this->acl->hasPermission('admin/files/index/sort') && $settings['order_by'] === 'custom') {
+            $dataGrid
+                ->addColumn([
+                    'label' => $this->translator->t('system', 'order'),
+                    'type' => Core\Helpers\DataGrid\ColumnRenderer\SortColumnRenderer::class,
+                    'fields' => ['sort'],
+                    'default_sort' => $settings['order_by'] === 'custom',
+                    'custom' => [
+                        'route_sort_down' => 'acp/files/index/sort/id_%d/action_down',
+                        'route_sort_up' => 'acp/files/index/sort/id_%d/action_up',
+                    ]
+                ], 15);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function render()
+    {
+        $dataGrid = $this->getCurrentDataGrid();
+
+        $this->configureDataGrid($dataGrid, [
+            'identifier' => '#files-data-grid',
+            'resource_path_delete' => 'admin/files/index/delete',
+            'resource_path_edit' => 'admin/files/index/edit',
+        ]);
+
+        return [
+            'grid' => $dataGrid->render(),
+            'show_mass_delete_button' => $dataGrid->countDbResults() > 0
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getModuleName(): string
+    {
+        return Schema::MODULE_NAME;
+    }
+}
