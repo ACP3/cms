20118f0a344e8d37cace0431c17248de65177c22
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-22 23:34:11 +0200

    added a view block for the file details frontend action

--- ACP3/Modules/ACP3/Files/Controller/Frontend/Index/Details.php
+++ ACP3/Modules/ACP3/Files/Controller/Frontend/Index/Details.php
@@ -30,15 +30,21 @@ class Details extends Core\Controller\AbstractFrontendAction
      * @var \ACP3\Modules\ACP3\Files\Cache
      */
     protected $filesCache;
+    /**
+     * @var Core\View\Block\BlockInterface
+     */
+    private $block;
 
     /**
      * @param \ACP3\Core\Controller\Context\FrontendContext $context
+     * @param Core\View\Block\BlockInterface $block
      * @param \ACP3\Core\Date $date
      * @param \ACP3\Modules\ACP3\Files\Model\Repository\FilesRepository $filesRepository
      * @param \ACP3\Modules\ACP3\Files\Cache $filesCache
      */
     public function __construct(
         Core\Controller\Context\FrontendContext $context,
+        Core\View\Block\BlockInterface $block,
         Core\Date $date,
         Files\Model\Repository\FilesRepository $filesRepository,
         Files\Cache $filesCache
@@ -48,6 +54,7 @@ class Details extends Core\Controller\AbstractFrontendAction
         $this->date = $date;
         $this->filesRepository = $filesRepository;
         $this->filesCache = $filesCache;
+        $this->block = $block;
     }
 
     /**
@@ -60,21 +67,9 @@ class Details extends Core\Controller\AbstractFrontendAction
         if ($this->filesRepository->resultExists($id, $this->date->getCurrentDateTime()) === true) {
             $this->setCacheResponseCacheable($this->config->getSettings(Schema::MODULE_NAME)['cache_lifetime']);
 
-            $file = $this->filesCache->getCache($id);
-
-            $this->breadcrumb
-                ->append($this->translator->t('files', 'files'), 'files')
-                ->append($file['category_title'], 'files/index/files/cat_' . $file['category_id'])
-                ->append($file['title']);
-
-            $settings = $this->config->getSettings(Files\Installer\Schema::MODULE_NAME);
-            $file['text'] = $this->view->fetchStringAsTemplate($file['text']);
-
-            return [
-                'file' => $file,
-                'dateformat' => $settings['dateformat'],
-                'comments_allowed' => $settings['comments'] == 1 && $file['comments'] == 1
-            ];
+            return $this->block
+                ->setData($this->filesCache->getCache($id))
+                ->render();
         }
 
         throw new Core\Controller\Exception\ResultNotExistsException();
---
+++ ACP3/Modules/ACP3/Files/View/Block/Frontend/FileDetailsBlock.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Files\View\Block\Frontend;
+
+
+use ACP3\Core\Settings\SettingsInterface;
+use ACP3\Core\View\Block\AbstractBlock;
+use ACP3\Core\View\Block\Context\BlockContext;
+use ACP3\Modules\ACP3\Files\Installer\Schema;
+
+class FileDetailsBlock extends AbstractBlock
+{
+    /**
+     * @var SettingsInterface
+     */
+    private $settings;
+
+    /**
+     * FileDetailsBlock constructor.
+     * @param BlockContext $context
+     * @param SettingsInterface $settings
+     */
+    public function __construct(BlockContext $context, SettingsInterface $settings)
+    {
+        parent::__construct($context);
+
+        $this->settings = $settings;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function render()
+    {
+        $file = $this->getData();
+
+        $this->breadcrumb
+            ->append($this->translator->t('files', 'files'), 'files')
+            ->append($file['category_title'], 'files/index/files/cat_' . $file['category_id'])
+            ->append($file['title']);
+
+        $settings = $this->settings->getSettings(Schema::MODULE_NAME);
+        $file['text'] = $this->view->fetchStringAsTemplate($file['text']);
+
+        return [
+            'file' => $file,
+            'dateformat' => $settings['dateformat'],
+            'comments_allowed' => $settings['comments'] == 1 && $file['comments'] == 1
+        ];
+    }
+}
