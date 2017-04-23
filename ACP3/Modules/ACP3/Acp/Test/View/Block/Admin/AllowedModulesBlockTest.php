e8523bce6919d22f8761b34c5bb23ced40f5aa2b
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-23 12:25:04 +0200

    some more unit test coverage

---
+++ ACP3/Modules/ACP3/Acp/Test/View/Block/Admin/AllowedModulesBlockTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Acp\Test\View\Block\Admin;
+
+use ACP3\Core\ACL;
+use ACP3\Core\Modules;
+use ACP3\Core\Test\View\Block\AbstractBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Acp\View\Block\Admin\AllowedModulesBlock;
+
+class AllowedModulesBlockTest extends AbstractBlockTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        $acl = $this->getMockBuilder(ACL::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['hasPermission'])
+            ->getMock();
+
+        $modules = $this->getMockBuilder(Modules::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getActiveModules'])
+            ->getMock();
+
+        $modules->expects($this->once())
+            ->method('getActiveModules')
+            ->willReturn([
+                'foo' => [
+                    'dir' => 'foo/'
+                ]
+            ]);
+
+        return new AllowedModulesBlock($this->context, $acl, $modules);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'modules'
+        ];
+    }
+}
---
+++ ACP3/Modules/ACP3/Captcha/Test/View/Block/Admin/CaptchaSettingsFormBlockTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: tinog
+ * Date: 23.04.2017
+ * Time: 12:17
+ */
+
+namespace ACP3\Modules\ACP3\Captcha\Test\View\Block\Admin;
+
+use ACP3\Core\Settings\SettingsInterface;
+use ACP3\Core\Test\View\Block\AbstractFormBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Captcha\Extension\CaptchaExtensionInterface;
+use ACP3\Modules\ACP3\Captcha\Utility\CaptchaRegistrar;
+use ACP3\Modules\ACP3\Captcha\View\Block\Admin\CaptchaSettingsFormBlock;
+
+class CaptchaSettingsFormBlockTest extends AbstractFormBlockTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        $settings = $this->getMockBuilder(SettingsInterface::class)
+            ->setMethods(['getSettings', 'saveSettings'])
+            ->getMock();
+
+        $settings->expects($this->once())
+            ->method('getSettings')
+            ->with('captcha')
+            ->willReturn(['captcha' => 'captcha.extension.foo']);
+
+        $captchaRegistrar = $this->getMockBuilder(CaptchaRegistrar::class)
+            ->getMock();
+
+        $captchaMock = $this->getMockBuilder(CaptchaExtensionInterface::class)
+            ->setMethods(['getCaptchaName', 'isCaptchaValid', 'getCaptcha'])
+            ->getMock();
+
+        $captchaRegistrar->expects($this->once())
+            ->method('getAvailableCaptchas')
+            ->willReturn([
+                'captcha.extension.foo' => $captchaMock
+            ]);
+
+        return new CaptchaSettingsFormBlock($this->context, $settings, $captchaRegistrar);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'captchas',
+            'form',
+            'form_token'
+        ];
+    }
+}
