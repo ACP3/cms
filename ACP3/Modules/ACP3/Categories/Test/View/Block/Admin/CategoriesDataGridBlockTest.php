a406d319980641d2035cad4a3b4e45c802462365
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-23 12:34:31 +0200

    covered the view block of the categories module with unit tests

---
+++ ACP3/Modules/ACP3/Categories/Test/View/Block/Admin/CategoriesDataGridBlockTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Categories\Test\View\Block\Admin;
+
+use ACP3\Core\Test\View\Block\AbstractDataGridBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Categories\View\Block\Admin\CategoriesDataGridBlock;
+
+class CategoriesDataGridBlockTest extends AbstractDataGridBlockTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        return new CategoriesDataGridBlock($this->context);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'grid',
+            'show_mass_delete_button'
+        ];
+    }
+}
---
+++ ACP3/Modules/ACP3/Categories/Test/View/Block/Admin/CategoryFormBlockTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Categories\Test\View\Block\Admin;
+
+use ACP3\Core\Modules;
+use ACP3\Core\Test\View\Block\AbstractFormBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Categories\View\Block\Admin\CategoryFormBlock;
+
+class CategoryFormBlockTest extends AbstractFormBlockTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        $modules = $this->getMockBuilder(Modules::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $modules->expects($this->once())
+            ->method('getActiveModules')
+            ->willReturn([
+                'foo' => [
+                    'id' => 1,
+                    'active' => true,
+                    'dependencies' => ['categories']
+                ]
+            ]);
+
+        return new CategoryFormBlock($this->context, $modules);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'form',
+            'mod_list',
+            'form_token'
+        ];
+    }
+}
