564c0b315f29937d0ea0d6463a811bf4ec74d9d4
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-23 17:37:20 +0200

    covered the view blocks of the feeds module with unit tests

---
+++ ACP3/Modules/ACP3/Feeds/Test/View/Block/Admin/FeedsSettingsFormBlockTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Feeds\Test\View\Block\Admin;
+
+use ACP3\Core\Settings\SettingsInterface;
+use ACP3\Core\Test\View\Block\AbstractFormBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Feeds\View\Block\Admin\FeedsSettingsFormBlock;
+
+class FeedsSettingsFormBlockTest extends AbstractFormBlockTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        $settings = $this->getMockBuilder(SettingsInterface::class)
+            ->setMethods(['getSettings', 'saveSettings'])
+            ->getMock();
+
+        $settings->expects($this->once())
+            ->method('getSettings')
+            ->with('feeds')
+            ->willReturn([
+                'feed_type' => 'RSS 2.0'
+            ]);
+
+        return new FeedsSettingsFormBlock($this->context, $settings);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'feed_types',
+            'form',
+            'form_token'
+        ];
+    }
+}
