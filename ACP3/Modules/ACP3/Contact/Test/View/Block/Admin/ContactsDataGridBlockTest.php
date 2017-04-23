0f380b47db775e8904e22c8f821f649e3124af63
Tino Goratsch <mail@goratschwebdesign.de>
2017-04-23 17:23:28 +0200

    covered the view blocks of the contact module with unit tests

---
+++ ACP3/Modules/ACP3/Contact/Test/View/Block/Admin/ContactsDataGridBlockTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Contact\Test\View\Block\Admin;
+
+use ACP3\Core\Test\View\Block\AbstractDataGridBlockTest;
+use ACP3\Core\View\Block\BlockInterface;
+use ACP3\Modules\ACP3\Contact\View\Admin\ContactsDataGridBlock;
+
+class ContactsDataGridBlockTest extends AbstractDataGridBlockTest
+{
+    /**
+     * @inheritdoc
+     */
+    protected function instantiateBlock(): BlockInterface
+    {
+        return new ContactsDataGridBlock($this->context);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getExpectedArrayKeys(): array
+    {
+        return [
+            'grid'
+        ];
+    }
+}
