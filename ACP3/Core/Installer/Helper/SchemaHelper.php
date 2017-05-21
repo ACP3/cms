ba6f6b7a24108a2aabd31c47cfa2c845f7589e33
Tino Goratsch <mail@goratschwebdesign.de>
2017-05-21 18:39:46 +0200

    added the copyright message

--- ACP3/Core/Installer/Helper/SchemaHelper.php
+++ ACP3/Core/Installer/Helper/SchemaHelper.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
 namespace ACP3\Core\Installer\Helper;
 
 use ACP3\Core;
