c5644036c02e27b7e33d75d82e2830c6fd1b1da8
Tino Goratsch <mail@goratschwebdesign.de>
2017-06-02 22:49:38 +0200

    added a command for warming up the caches of the ACP3 including the HTTP cache

---
+++ ACP3/Modules/ACP3/Seo/Command/HttpCacheWarmupCommand.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Seo\Command;
+
+
+use ACP3\Core\Environment\ApplicationPath;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HttpCacheWarmupCommand extends Command
+{
+    private $defaultSitemapName = ACP3_ROOT_DIR . 'sitemap.xml';
+
+    private $splitSitemapNames = [
+        ACP3_ROOT_DIR . 'sitemap_http.xml',
+        ACP3_ROOT_DIR . 'sitemap_https.xml'
+    ];
+
+    /**
+     * @var ApplicationPath
+     */
+    private $applicationPath;
+
+    /**
+     * ClearCacheCommand constructor.
+     * @param ApplicationPath $applicationPath
+     */
+    public function __construct(ApplicationPath $applicationPath) {
+        parent::__construct();
+
+        $this->applicationPath = $applicationPath;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('acp3:http-cache:warmup')
+            ->setDescription('Warms up the HTTP cache.')
+            ->setHelp('This command reads the sitemap.xml and crawls the URLs in it, so that the HTTP cache gets a warming after a deployment.');
+
+        $this
+            ->addOption(
+                'sleep',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'How many seconds should the crawler pause after crawling an URL?',
+                0
+            )
+            ->addOption(
+                'limit',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'How many URLs per sitemap should be crawled?',
+                0
+            );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Warming up the HTTP cache...');
+        $output->writeln('==================');
+
+        if (is_file($this->defaultSitemapName)) {
+            $this->processSitemap($this->defaultSitemapName, $input, $output);
+        } else {
+            foreach ($this->splitSitemapNames as $sitemap) {
+                if (is_file($sitemap)) {
+                    $this->processSitemap($sitemap, $input, $output);
+                }
+            }
+        }
+    }
+
+    /**
+     * Parses the XML sitemap and crawls the URLs of it
+     *
+     * @param string $sitemap
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    private function processSitemap(string $sitemap, InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Crawling URLs of file {$sitemap}...");
+
+        $xml = simplexml_load_file($sitemap);
+
+        $progress = new ProgressBar($output, count($xml->url));
+        ProgressBar::setFormatDefinition('custom', ' %current%/%max% -- %message%: %result%');
+        $progress->setFormat('custom');
+
+        $sleep = $this->getSleepTime($input);
+        $limit = (int)$input->getOption('limit');
+
+        $i = 1;
+        foreach ($xml->url as $url) {
+            $progress->setMessage($url->loc);
+            $progress->setMessage($this->crawlUrl($url->loc) ? 'Done!' : 'Error!', 'result');
+
+            $progress->advance();
+
+            if ($limit > 0 && $i === $limit) {
+                break;
+            }
+
+            ++$i;
+
+            if ($sleep > 0) {
+                usleep($sleep);
+            }
+        }
+
+        $progress->finish();
+
+        $output->writeln('');
+    }
+
+    /**
+     * Crawls the given URL
+     *
+     * @param string $url
+     * @return bool Whether the crawl was success or not
+     */
+    private function crawlUrl(string $url): bool
+    {
+        try {
+            return file_get_contents($url) !== false;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return int
+     */
+    private function getSleepTime(InputInterface $input): int
+    {
+        return 1000000 * (int)$input->getOption('sleep');
+    }
+}
