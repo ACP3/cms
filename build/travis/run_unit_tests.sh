9bff7c4ec6034a392a07a82ec1f46142e048c4e9
Tino Goratsch <mail@goratschwebdesign.de>
2017-09-01 09:27:17 +0200

    added some scripts

--- build/travis/run_unit_tests.sh
+++ build/travis/run_unit_tests.sh
@@ -7,9 +7,7 @@ then
     php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
 elif [[ ${TRAVIS_PHP_VERSION} != "nightly" ]]
 then
-    composer run-script test
-    # phpdbg -qrr ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
+    phpdbg -qrr ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml --coverage-clover ./build/logs/clover.xml
 else
-    composer run-script test-without-coverage
-    # php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml
+    php ./vendor/bin/phpunit -c ./tests/phpunit.dist.xml
 fi
