04685e9f254a07f2b46b9d440f42a2a1ee5cd39b
Tino Goratsch <mail@goratschwebdesign.de>
2017-12-12 20:37:56 +0100

    rework the breadcrumb, so that structure from the menus is taking precedence

--- ACP3/Modules/ACP3/Menus/Core/Breadcrumb/Steps.php
+++ ACP3/Modules/ACP3/Menus/Core/Breadcrumb/Steps.php
@@ -75,27 +75,15 @@ class Steps extends Core\Breadcrumb\Steps
         }
 
         if (!empty($this->stepsFromDb)) {
-            $dbStepsCount = count($this->stepsFromDb);
-            $stepsCount = count($this->steps);
-            $lastDbStepUri = $this->stepsFromDb[$dbStepsCount - 1]['uri'];
+            $sliceIndex = $this->findMatchingUriIndexInSteps();
 
-            if ($stepsCount === 1 && empty($this->steps[0]['uri'])) {
-                $this->copyTitleFromFirstStepToLastDbStep();
-                $this->steps[0]['uri'] = $lastDbStepUri;
-            }
-
-            if ($dbStepsCount > 1 && $dbStepsCount >= $stepsCount) {
-                $this->breadcrumbCache = $this->stepsFromDb;
-            } elseif ($lastDbStepUri === $this->steps[0]['uri']) {
-                $this->copyTitleFromFirstStepToLastDbStep();
-
-                $this->breadcrumbCache = array_merge($this->stepsFromDb, array_slice($this->steps, 1));
-            }
+            $this->breadcrumbCache = array_merge($this->stepsFromDb, array_slice($this->steps, $sliceIndex));
         }
     }
 
     /**
      * Initializes and pre populates the breadcrumb
+     * @throws \Doctrine\DBAL\DBALException
      */
     private function prePopulate()
     {
@@ -158,8 +146,22 @@ class Steps extends Core\Breadcrumb\Steps
         return $this;
     }
 
-    private function copyTitleFromFirstStepToLastDbStep()
+    /**
+     * @return int
+     */
+    private function findMatchingUriIndexInSteps(): int
     {
-        $this->stepsFromDb[count($this->stepsFromDb) - 1]['title'] = $this->steps[0]['title'];
+        $steps = array_reverse($this->steps);
+        $lastDbStep = end($this->stepsFromDb);
+
+        $offset = 0;
+        foreach ($steps as $index => $step) {
+            if ($step['uri'] === $lastDbStep['uri']) {
+                $offset = $index;
+                break;
+            }
+        }
+
+        return count($steps) - $offset;
     }
 }
