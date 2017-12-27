398dc0b7b59b1c83908a61021fad2ae4569ece48
Tino Goratsch <mail@goratschwebdesign.de>
2017-12-27 14:45:40 +0100

    - add a Gitlab CI job which updates the latest version file - cleanup the travis.yml from now obsolete jobs

--- build/gitlab/before_script.sh
+++ build/gitlab/before_script.sh
@@ -4,6 +4,7 @@
 [[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
 
 apt-get update -yqq
+apt-get install git
 which ssh-agent || (apt-get install openssh-client -y )
 
 eval $(ssh-agent -s)
