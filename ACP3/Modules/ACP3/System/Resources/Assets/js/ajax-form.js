1ae92cb89e21ff8cbb59458a3e04697d0430ddb9
Tino Goratsch <mail@goratschwebdesign.de>
2018-01-02 18:24:54 +0100

    fixed a bug with the AJAX forms

--- ACP3/Modules/ACP3/System/Resources/Assets/js/ajax-form.js
+++ ACP3/Modules/ACP3/System/Resources/Assets/js/ajax-form.js
@@ -241,6 +241,8 @@
             } else {
                 $(this.settings.targetElement).html(responseData);
             }
+
+            this.findSubmitButton();
         },
         hideLoadingLayer: function ($submitButton) {
             $('#loading-layer').removeClass('loading-layer__active');
