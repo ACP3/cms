c0b945c4bc562c97b3b4fee9afd480dcd57cb306
Tino Goratsch <mail@goratschwebdesign.de>
2018-01-02 18:29:01 +0100

    fixed a PHP type error when trying to create a new URI alias

--- ACP3/Modules/ACP3/Seo/Event/Listener/InsertUriAliasOnModelAfterSaveListener.php
+++ ACP3/Modules/ACP3/Seo/Event/Listener/InsertUriAliasOnModelAfterSaveListener.php
@@ -6,7 +6,7 @@
 
 namespace ACP3\Modules\ACP3\Seo\Event\Listener;
 
-use ACP3\Core\ACL;
+use ACP3\Core\ACL\ACLInterface;
 use ACP3\Core\Model\Event\ModelSaveEvent;
 use ACP3\Modules\ACP3\Seo\Helper\UriAliasManager;
 use ACP3\Modules\ACP3\Seo\Installer\Schema;
