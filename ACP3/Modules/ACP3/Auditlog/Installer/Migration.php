ec677fdb94fa0aab71872ec6948ea8ed10c1b86d
Tino Goratsch <mail@goratschwebdesign.de>
2018-02-17 00:22:56 +0100

    add a stub for the auditlog module

---
+++ ACP3/Modules/ACP3/Auditlog/Installer/Migration.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Auditlog\Installer;
+
+use ACP3\Core\Modules\Installer\MigrationInterface;
+
+class Migration implements MigrationInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
+    public function schemaUpdates()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
+    public function renameModule()
+    {
+        return [];
+    }
+}
---
+++ ACP3/Modules/ACP3/Auditlog/Installer/Schema.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENCE file at the top-level module directory for licencing details.
+ */
+
+namespace ACP3\Modules\ACP3\Auditlog\Installer;
+
+use ACP3\Core\ACL\PrivilegeEnum;
+use ACP3\Core\Modules;
+
+class Schema implements Modules\Installer\SchemaInterface
+{
+    const MODULE_NAME = 'auditlog';
+
+    /**
+     * @return array
+     */
+    public function createTables()
+    {
+        return [
+            'CREATE TABLE `{pre}auditlog` (
+                `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `date` DATETIME NOT NULL,
+                `module_id` INT(10) UNSIGNED NOT NULL,
+                `entry_id` INT(10) UNSIGNED NOT NULL,
+                `data` MEDIUMTEXT NOT NULL,
+                `user_id` INT(10) UNSIGNED,
+                PRIMARY KEY (`id`),
+                INDEX (`module_id`),
+                INDEX (`user_id`),
+                FOREIGN KEY (`module_id`) REFERENCES `{pre}modules` (`id`) ON DELETE CASCADE,
+                FOREIGN KEY (`user_id`) REFERENCES `{pre}users` (`id`) ON DELETE SET NULL
+            ) {ENGINE} {CHARSET};',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function removeTables()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function settings()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function schemaUpdates()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function specialResources()
+    {
+        return [
+            'admin' => [
+                'index' => [
+                    'index' => PrivilegeEnum::ADMIN_VIEW,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getModuleName()
+    {
+        return static::MODULE_NAME;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSchemaVersion()
+    {
+        return 1;
+    }
+}
