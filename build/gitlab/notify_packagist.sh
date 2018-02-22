ae2a0ee8ea8ed17b6bb924554d57d96bf73f5923
Tino Goratsch <mail@goratschwebdesign.de>
2018-02-22 21:01:51 +0100

    extend the packagist notification with the new audit log module

--- build/gitlab/notify_packagist.sh
+++ build/gitlab/notify_packagist.sh
@@ -6,6 +6,7 @@ declare -a repositories
 
 repositories[0]="https://gitlab.com/ACP3/core.git"
 repositories[1]="https://gitlab.com/ACP3/module-acp.git"
+repositories[1]="https://gitlab.com/ACP3/module-audit-log.git"
 repositories[2]="https://gitlab.com/ACP3/module-articles.git"
 repositories[3]="https://gitlab.com/ACP3/module-captcha.git"
 repositories[4]="https://gitlab.com/ACP3/module-categories.git"
