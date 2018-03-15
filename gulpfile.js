9789fe278d76d12bfb634d867c4ea3f86f9cef38
Tino Goratsch <mail@goratschwebdesign.de>
2018-03-15 18:42:04 +0100

    run the "less" gulp task before the "watch" task

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@
     gulp.task('copy', getTask('copy'));
     gulp.task('bump-version', getTask('bump-version'));
     gulp.task('less', getTask('less'));
-    gulp.task('watch', getTask('watch'));
+    gulp.task('watch', ['less'], getTask('watch'));
 
     gulp.task('default', ['watch']);
 })();
