7c696e884ae2754c019841a37c9583e7276fcdbf
Tino Goratsch <mail@goratschwebdesign.de>
2018-03-15 18:21:24 +0100

    minor code optimization

--- ACP3/Modules/ACP3/Share/Resources/Assets/js/partials/rating.js
+++ ACP3/Modules/ACP3/Share/Resources/Assets/js/partials/rating.js
@@ -6,11 +6,9 @@
 jQuery(document).ready(function ($) {
     $('#rating-wrapper')
         .on('change', ':radio', function () {
+            $('.rating__average').remove();
             $('.rating__already-rated').remove();
             $(this).closest('form').submit();
-        })
-        .on('click', '.rating_mobile .rating__star', function () {
-            $('.rating__average').remove();
         })
         .on('click', '.rating__average .rating__star', function () {
             // .rating__average sets the direction back to ltr, so we have to account for this
