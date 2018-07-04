9fbd5fe559c78f9c1b06572732512e2600578568
Tino Goratsch <mail@goratschwebdesign.de>
2018-07-04 23:33:40 +0200

    remove unused imports

--- ACP3/Modules/ACP3/Comments/Controller/Admin/Details/Index.php
+++ ACP3/Modules/ACP3/Comments/Controller/Admin/Details/Index.php
@@ -9,7 +9,6 @@ namespace ACP3\Modules\ACP3\Comments\Controller\Admin\Details;
 
 use ACP3\Core;
 use ACP3\Modules\ACP3\Comments;
-use ACP3\Modules\ACP3\System;
 use ACP3\Modules\ACP3\System\Installer\Schema;
 
 class Index extends Core\Controller\AbstractFrontendAction
