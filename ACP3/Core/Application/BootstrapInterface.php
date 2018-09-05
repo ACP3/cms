d8165eadfc1046aa5bde5998f0f314c62b4a6db5
Tino Goratsch <mail@goratschwebdesign.de>
2018-09-05 23:16:55 +0200

    remove the ext-* nodes from the composer.json

--- ACP3/Core/Application/BootstrapInterface.php
+++ ACP3/Core/Application/BootstrapInterface.php
@@ -16,7 +16,7 @@ interface BootstrapInterface extends HttpKernelInterface
     /**
      * Contains the current ACP3 version string.
      */
-    const VERSION = '4.29.3';
+    const VERSION = '4.29.4';
 
     /**
      * Performs some startup checks.
