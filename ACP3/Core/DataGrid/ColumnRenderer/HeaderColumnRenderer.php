31ee49ab3abe9f5f66d21ad00c187f2251a26e06
Tino Goratsch <mail@goratschwebdesign.de>
2019-01-15 22:51:16 +0100

    add one more PHP 7.3 compatibility fix

--- ACP3/Core/DataGrid/ColumnRenderer/HeaderColumnRenderer.php
+++ ACP3/Core/DataGrid/ColumnRenderer/HeaderColumnRenderer.php
@@ -34,7 +34,7 @@ class HeaderColumnRenderer extends AbstractColumnRenderer
     public function fetchDataAndRenderColumn(array $column, array $dbResultRow)
     {
         if ($column['type'] === MassActionColumnRenderer::class) {
-            $id = \preg_replace('=[^\w\d-_]=', '', $column['label']) . '-mark-all';
+            $id = \preg_replace('=[^\w\d\-_]=', '', $column['label']) . '-mark-all';
             $value = '<input type="checkbox" id="' . $id . '" value="1" ' . $this->markEntriesHelper->execute('entries', $id) . '>';
         } else {
             $value = $column['label'];
