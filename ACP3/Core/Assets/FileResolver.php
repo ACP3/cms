cc1f97fd3ee480803dd8ceb40be2f0fb07e563f6
Tino Goratsch <mail@goratschwebdesign.de>
2019-03-08 19:25:21 +0100

    backport the file checker strategies

--- ACP3/Core/Assets/FileResolver.php
+++ ACP3/Core/Assets/FileResolver.php
@@ -8,29 +8,18 @@
 namespace ACP3\Core\Assets;
 
 use ACP3\Core;
+use ACP3\Core\Assets\FileResolver\FileCheckerStrategyInterface;
 
 class FileResolver
 {
     /**
      * @var \ACP3\Core\Environment\ApplicationPath
      */
-    protected $appPath;
+    private $appPath;
     /**
      * @var \ACP3\Core\Assets\Cache
      */
-    protected $resourcesCache;
-    /**
-     * @var array
-     */
-    protected $cachedPaths = [];
-    /**
-     * @var bool
-     */
-    protected $newAssetPathsAdded = false;
-    /**
-     * @var string
-     */
-    protected $designAssetsPath;
+    private $resourcesCache;
     /**
      * @var \ACP3\Core\Modules
      */
@@ -39,6 +28,23 @@ class FileResolver
      * @var \ACP3\Core\Environment\Theme
      */
     private $theme;
+    /**
+     * @var \ACP3\Core\Assets\FileResolver\FileCheckerStrategyInterface[]
+     */
+    private $strategies = [];
+
+    /**
+     * @var array
+     */
+    private $cachedPaths = [];
+    /**
+     * @var bool
+     */
+    private $newAssetPathsAdded = false;
+    /**
+     * @var string
+     */
+    private $designAssetsPath;
     /**
      * @var string
      */
@@ -61,6 +67,9 @@ class FileResolver
         $this->cachedPaths = $resourcesCache->getCache();
         $this->modules = $modules;
         $this->theme = $theme;
+
+        $this->addStrategy(new Core\Assets\FileResolver\MinifiedAwareFileCheckerStrategy());
+        $this->addStrategy(new Core\Assets\FileResolver\StraightFileCheckerStrategy());
     }
 
     /**
@@ -71,6 +80,35 @@ class FileResolver
         if ($this->newAssetPathsAdded === true) {
             $this->resourcesCache->saveCache($this->cachedPaths);
         }
+    }
+
+    public function addStrategy(FileCheckerStrategyInterface $strategy): void
+    {
+        $this->strategies[] = $strategy;
+    }
+
+    /**
+     * @param string $templatePath
+     *
+     * @return string
+     */
+    public function resolveTemplatePath(string $templatePath): string
+    {
+        // A path without any slash was given -> has to be a layout file of the current design
+        if (\strpos($templatePath, '/') === false) {
+            return $this->getStaticAssetPath('', '', '', $templatePath);
+        }
+        // Split the template path in its components
+        $fragments = \explode('/', \ucfirst($templatePath));
+
+        if (isset($fragments[2])) {
+            $fragments[1] = \ucfirst($fragments[1]);
+        }
+        $modulesPath = $fragments[0] . '/Resources/';
+        $designPath = $fragments[0];
+        $templatePath = \implode('/', \array_slice($fragments, 1));
+
+        return $this->getStaticAssetPath($modulesPath, $designPath, 'View', $templatePath);
     }
 
     /**
@@ -98,13 +136,13 @@ class FileResolver
         }
 
         $systemAssetPath = $this->appPath->getModulesDir() . $modulePath . $dir . $file;
+        if (!isset($this->cachedPaths[$systemAssetPath])) {
+            $this->cachedPaths[$systemAssetPath] = $this->resolveAssetPath($modulePath, $designPath, $dir, $file);
 
-        // Return early, if the path has been already cached
-        if (isset($this->cachedPaths[$systemAssetPath])) {
-            return $this->cachedPaths[$systemAssetPath];
+            $this->newAssetPathsAdded = true;
         }
 
-        return $this->resolveAssetPath($modulePath, $designPath, $dir, $file);
+        return $this->cachedPaths[$systemAssetPath] ?: '';
     }
 
     /**
@@ -123,84 +161,85 @@ class FileResolver
      * @param string $dir
      * @param string $file
      *
-     * @return string
+     * @return string|null
      */
-    private function resolveAssetPath(string $modulePath, string $designPath, string $dir, string $file): string
+    private function resolveAssetPath(string $modulePath, string $designPath, string $dir, string $file): ?string
     {
         if ($this->designAssetsPath === null) {
-            $this->resetDesignAssetPath();
+            $this->resetTheme();
         }
 
-        $assetPath = '';
-        $designAssetPath = $this->designAssetsPath . $designPath . $dir . $file;
+        $assetPath = $this->findAssetInInheritedThemes($modulePath, $designPath, $dir, $file);
 
-        // A theme has overridden a static asset of a module
-        if (\is_file($designAssetPath) === true) {
-            $assetPath = $designAssetPath;
-        } else {
-            $parentThemes = $this->theme->getThemeDependencies($this->currentTheme);
-            $parentTheme = \next($parentThemes);
-
-            // Recursively iterate over the nested themes
-            if ($parentTheme !== false) {
-                $this->modifyDesignAssetPath($parentTheme);
-                $assetPath = $this->getStaticAssetPath($modulePath, $designPath, $dir, $file);
-                $this->resetDesignAssetPath();
-
-                return $assetPath;
-            }
-
-            // No overrides have been found -> iterate over all possible module namespaces
-            $moduleName = \substr($modulePath, 0, \strpos($modulePath, '/'));
-            $moduleInfo = $this->modules->getModuleInfo($moduleName);
-
-            if (!empty($moduleInfo)) {
-                $moduleAssetPath = $this->appPath->getModulesDir() . $moduleInfo['vendor'] . '/' . $modulePath . $dir . $file;
-                if (\is_file($moduleAssetPath) === true) {
-                    $assetPath = $moduleAssetPath;
-                }
-            }
-        }
-
-        $systemAssetPath = $this->appPath->getModulesDir() . $modulePath . $dir . $file;
-        $this->cachedPaths[$systemAssetPath] = $assetPath;
-        $this->newAssetPathsAdded = true;
-
-        return $assetPath;
+        return $assetPath ?: $this->findAssetInModules($modulePath, $dir, $file);
     }
 
     /**
-     * @param string $template
+     * @param string $modulePath
+     * @param string $designPath
+     * @param string $dir
+     * @param string $file
      *
-     * @return string
+     * @return string|null
      */
-    public function resolveTemplatePath(string $template): string
+    private function findAssetInInheritedThemes(string $modulePath, string $designPath, string $dir, string $file): ?string
     {
-        // A path without any slash was given -> has to be a layout file of the current design
-        if (\strpos($template, '/') === false) {
-            return $this->getStaticAssetPath('', '', '', $template);
-        }
-        // Split the template path in its components
-        $fragments = \explode('/', \ucfirst($template));
+        $designAssetPath = $this->designAssetsPath . $designPath . $dir . $file;
 
-        if (isset($fragments[2])) {
-            $fragments[1] = \ucfirst($fragments[1]);
+        if (null !== ($resourcePath = $this->findAssetInStrategies($designAssetPath))) {
+            return $resourcePath;
         }
-        $modulesPath = $fragments[0] . '/Resources/';
-        $designPath = $fragments[0];
-        $template = \implode('/', \array_slice($fragments, 1));
 
-        return $this->getStaticAssetPath($modulesPath, $designPath, 'View', $template);
+        $parentThemes = $this->theme->getThemeDependencies($this->currentTheme);
+        $parentTheme = \next($parentThemes);
+
+        // Recursively iterate over the nested themes
+        if ($parentTheme !== false) {
+            $this->changeTheme($parentTheme);
+            $assetPath = $this->getStaticAssetPath($modulePath, $designPath, $dir, $file);
+            $this->resetTheme();
+
+            return $assetPath;
+        }
+
+        return null;
     }
 
-    private function modifyDesignAssetPath(string $themeName): void
+    private function findAssetInStrategies(string $resourcePath): ?string
+    {
+        foreach ($this->strategies as $strategy) {
+            if ($strategy->isAllowed($resourcePath) && (null !== ($resource = $strategy->findResource($resourcePath)))) {
+                return $resource;
+            }
+        }
+
+        return null;
+    }
+
+    private function changeTheme(string $themeName): void
     {
         $this->currentTheme = $themeName;
         $this->designAssetsPath = $this->appPath->getDesignRootPathInternal() . $themeName . '/';
     }
 
-    private function resetDesignAssetPath(): void
+    private function resetTheme(): void
     {
-        $this->modifyDesignAssetPath($this->theme->getCurrentTheme());
+        $this->changeTheme($this->theme->getCurrentTheme());
+    }
+
+    private function findAssetInModules(string $modulePath, string $dir, string $file): ?string
+    {
+        $moduleName = \substr($modulePath, 0, \strpos($modulePath, '/'));
+        $moduleInfo = $this->modules->getModuleInfo($moduleName);
+
+        if (!empty($moduleInfo)) {
+            $moduleAssetPath = $this->appPath->getModulesDir() . $moduleInfo['vendor'] . '/' . $modulePath . $dir . $file;
+
+            if (null !== ($resourcePath = $this->findAssetInStrategies($moduleAssetPath))) {
+                return $resourcePath;
+            }
+        }
+
+        return null;
     }
 }
