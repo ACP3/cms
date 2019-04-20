0d2a46f19055ee2ccbbca172d23935e71a8c8161
Tino Goratsch <mail@goratschwebdesign.de>
2019-04-20 21:20:27 +0200

    fix the reCaptcha

--- ACP3/Modules/ACP3/Captcha/Resources/Assets/js/partials/recaptcha.onload.js
+++ ACP3/Modules/ACP3/Captcha/Resources/Assets/js/partials/recaptcha.onload.js
@@ -4,7 +4,7 @@
  */
 
 /* global onloadCallback:true */
-onloadCallback = () => {
+const onloadCallback = () => {
     jQuery('.recaptcha-placeholder').each(function () {
         if (jQuery(this).children().length === 0) {
             const widgetId = grecaptcha.render(this.id, {
