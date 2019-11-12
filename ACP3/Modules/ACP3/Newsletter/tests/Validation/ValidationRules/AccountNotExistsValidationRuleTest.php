3ed9b98946b4b0e354cfddd567e5e7acb0a467c2
Tino Goratsch <mail@goratschwebdesign.de>
2019-11-12 22:09:49 +0100

    restructure the modules

---
+++ ACP3/Modules/ACP3/Newsletter/tests/Validation/ValidationRules/AccountNotExistsValidationRuleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENSE file at the top-level module directory for licensing details.
+ */
+
+namespace ACP3\Modules\ACP3\Newsletter\Validation\ValidationRules;
+
+class AccountNotExistsValidationRuleTest extends AccountExistsValidationRuleTest
+{
+    /**
+     * @return array
+     */
+    public function validationRuleProvider()
+    {
+        return [
+            'valid-data-simple' => ['info@example.com', '', [], false],
+            'valid-data-complex' => [['mail' => 'info@example.com'], 'mail', [], false],
+            'invalid-data-simple' => ['info@example.de', '', [], true],
+            'invalid-data-complex' => [['mail' => 'info@example.de'], 'mail', [], true],
+        ];
+    }
+}
---
+++ ACP3/Modules/ACP3/Seo/tests/Core/Helpers/PageBreaksTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENSE file at the top-level module directory for licensing details.
+ */
+
+namespace ACP3\Modules\ACP3\Seo\Core\Helpers;
+
+use ACP3\Modules\ACP3\Seo\Helper\MetaStatements;
+
+class PageBreaksTest extends \ACP3\Core\Helpers\PageBreaksTest
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $metaStatements;
+
+    protected function setUp()
+    {
+        $this->initializeMockObjects();
+
+        $this->pageBreaks = new PageBreaks(
+            $this->requestMock,
+            $this->routerMock,
+            $this->tocMock,
+            $this->metaStatements
+        );
+    }
+
+    protected function initializeMockObjects()
+    {
+        parent::initializeMockObjects();
+
+        $this->metaStatements = $this->createMock(MetaStatements::class);
+    }
+
+    /**
+     * @dataProvider splitTextIntoPagesDataProvider
+     *
+     * @param string $sourceText
+     * @param int    $currentPage
+     * @param string $currentPageText
+     * @param string $baseUrlPath
+     * @param string $nextPageUrl
+     * @param string $prevPageUrl
+     */
+    public function testSplitTextIntoPages(
+        $sourceText,
+        $currentPage,
+        $currentPageText,
+        $baseUrlPath,
+        $nextPageUrl,
+        $prevPageUrl
+    ) {
+        $this->setUpMetaStatementsMockExpectations($nextPageUrl, $prevPageUrl);
+
+        parent::testSplitTextIntoPages(
+            $sourceText,
+            $currentPage,
+            $currentPageText,
+            $baseUrlPath,
+            $nextPageUrl,
+            $prevPageUrl
+        );
+    }
+
+    /**
+     * @param string $nextPageUrl
+     * @param string $prevPageUrl
+     */
+    private function setUpMetaStatementsMockExpectations($nextPageUrl, $prevPageUrl)
+    {
+        $this->metaStatements->expects($this->once())
+            ->method('setNextPage')
+            ->with($nextPageUrl)
+            ->willReturnSelf();
+        $this->metaStatements->expects($this->once())
+            ->method('setPreviousPage')
+            ->with($prevPageUrl)
+            ->willReturnSelf();
+    }
+}
