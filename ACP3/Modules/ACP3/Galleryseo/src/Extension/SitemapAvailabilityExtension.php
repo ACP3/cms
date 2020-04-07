78a238def65015dbe6a67f54e08b0b6c30f231de
Tino Goratsch <mail@goratschwebdesign.de>
2020-04-08 00:02:50 +0200

    do not add the gallery pictures to the sitemap, if the picture overlay is active

--- ACP3/Modules/ACP3/Galleryseo/src/Extension/SitemapAvailabilityExtension.php
+++ ACP3/Modules/ACP3/Galleryseo/src/Extension/SitemapAvailabilityExtension.php
@@ -10,6 +10,7 @@ namespace ACP3\Modules\ACP3\Galleryseo\Extension;
 use ACP3\Core\Date;
 use ACP3\Core\Router\RouterInterface;
 use ACP3\Core\SEO\MetaStatementsServiceInterface;
+use ACP3\Core\Settings\SettingsInterface;
 use ACP3\Modules\ACP3\Gallery\Helpers;
 use ACP3\Modules\ACP3\Gallery\Installer\Schema;
 use ACP3\Modules\ACP3\Gallery\Model\Repository\GalleryRepository;
@@ -30,10 +31,15 @@ class SitemapAvailabilityExtension extends AbstractSitemapAvailabilityExtension
      * @var PictureRepository
      */
     protected $pictureRepository;
+    /**
+     * @var \ACP3\Core\Settings\SettingsInterface
+     */
+    private $settings;
 
     public function __construct(
         Date $date,
         RouterInterface $router,
+        SettingsInterface $settings,
         GalleryRepository $galleryRepository,
         PictureRepository $pictureRepository,
         MetaStatementsServiceInterface $metaStatements
@@ -43,6 +49,7 @@ class SitemapAvailabilityExtension extends AbstractSitemapAvailabilityExtension
         $this->date = $date;
         $this->galleryRepository = $galleryRepository;
         $this->pictureRepository = $pictureRepository;
+        $this->settings = $settings;
     }
 
     /**
@@ -60,6 +67,8 @@ class SitemapAvailabilityExtension extends AbstractSitemapAvailabilityExtension
      */
     protected function fetchSitemapUrls($isSecure = null)
     {
+        $gallerySettings = $this->settings->getSettings(Schema::MODULE_NAME);
+
         $this->addUrl('gallery/index/index', null, $isSecure);
 
         foreach ($this->galleryRepository->getAll($this->date->getCurrentDateTime()) as $result) {
@@ -69,12 +78,14 @@ class SitemapAvailabilityExtension extends AbstractSitemapAvailabilityExtension
                 $isSecure
             );
 
-            foreach ($this->pictureRepository->getPicturesByGalleryId($result['id']) as $picture) {
-                $this->addUrl(
-                    \sprintf(Helpers::URL_KEY_PATTERN_PICTURE, $picture['id']),
-                    $this->date->toDateTime($result['updated_at']),
-                    $isSecure
-                );
+            if (((int) $gallerySettings['overlay']) === 0) {
+                foreach ($this->pictureRepository->getPicturesByGalleryId($result['id']) as $picture) {
+                    $this->addUrl(
+                        \sprintf(Helpers::URL_KEY_PATTERN_PICTURE, $picture['id']),
+                        $this->date->toDateTime($result['updated_at']),
+                        $isSecure
+                    );
+                }
             }
         }
     }
