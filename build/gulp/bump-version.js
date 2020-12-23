8f33ed24c9b602bdf1c1423540f6829dabe414e2
Tino Goratsch <mail@goratschwebdesign.de>
2020-12-23 01:34:53 +0100

    rework and extend the bump-version gulp script

--- build/gulp/bump-version.js
+++ build/gulp/bump-version.js
@@ -5,115 +5,89 @@
 
 const argv = require('yargs').argv;
 const moment = require('moment');
-const git = require('simple-git/promise');
+const git = require('simple-git');
 const semver = require('semver');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 module.exports = (gulp, plugins) => {
     'use strict';
 
-    const modules = {
-        'ACP3/Core': 'acp3/core',
-        'designs/acp3': 'acp3/theme-default',
-        'designs/acp3-installer': 'acp3/theme-installer',
-        'installation': 'acp3/setup',
-        'tests': 'acp3/test',
-        'ACP3/Modules/ACP3/Acp': 'acp3/module-acp',
-        'ACP3/Modules/ACP3/Articles': 'acp3/module-articles',
-        'ACP3/Modules/ACP3/Articlesmenus': 'acp3/module-articles-menus',
-        'ACP3/Modules/ACP3/Articlessearch': 'acp3/module-articles-search',
-        'ACP3/Modules/ACP3/Articlesseo': 'acp3/module-articles-seo',
-        'ACP3/Modules/ACP3/Articlesshare': 'acp3/module-articles-share',
-        'ACP3/Modules/ACP3/Auditlog': 'acp3/module-audit-log',
-        'ACP3/Modules/ACP3/Captcha': 'acp3/module-captcha',
-        'ACP3/Modules/ACP3/Categories': 'acp3/module-categories',
-        'ACP3/Modules/ACP3/Comments': 'acp3/module-comments',
-        'ACP3/Modules/ACP3/Contact': 'acp3/module-contact',
-        'ACP3/Modules/ACP3/Contactseo': 'acp3/module-contact-seo',
-        'ACP3/Modules/ACP3/Cookieconsent': 'acp3/module-cookie-consent',
-        'ACP3/Modules/ACP3/Emoticons': 'acp3/module-emoticons',
-        'ACP3/Modules/ACP3/Errors': 'acp3/module-errors',
-        'ACP3/Modules/ACP3/Feeds': 'acp3/module-feeds',
-        'ACP3/Modules/ACP3/Filemanager': 'acp3/module-filemanager',
-        'ACP3/Modules/ACP3/Files': 'acp3/module-files',
-        'ACP3/Modules/ACP3/Filescomments': 'acp3/module-files-comments',
-        'ACP3/Modules/ACP3/Filesfeed': 'acp3/module-files-feed',
-        'ACP3/Modules/ACP3/Filessearch': 'acp3/module-files-search',
-        'ACP3/Modules/ACP3/Filesseo': 'acp3/module-files-seo',
-        'ACP3/Modules/ACP3/Filesshare': 'acp3/module-files-share',
-        'ACP3/Modules/ACP3/Gallery': 'acp3/module-gallery',
-        'ACP3/Modules/ACP3/Gallerycomments': 'acp3/module-gallery-comments',
-        'ACP3/Modules/ACP3/Galleryseo': 'acp3/module-gallery-seo',
-        'ACP3/Modules/ACP3/Galleryshare': 'acp3/module-gallery-share',
-        'ACP3/Modules/ACP3/Guestbook': 'acp3/module-guestbook',
-        'ACP3/Modules/ACP3/Guestbooknewsletter': 'acp3/module-guestbook-newsletter',
-        'ACP3/Modules/ACP3/Installer': 'acp3/module-installer',
-        'ACP3/Modules/ACP3/Menus': 'acp3/module-menus',
-        'ACP3/Modules/ACP3/News': 'acp3/module-news',
-        'ACP3/Modules/ACP3/Newscomments': 'acp3/module-news-comments',
-        'ACP3/Modules/ACP3/Newsfeed': 'acp3/module-news-feed',
-        'ACP3/Modules/ACP3/Newssearch': 'acp3/module-news-search',
-        'ACP3/Modules/ACP3/Newsseo': 'acp3/module-news-seo',
-        'ACP3/Modules/ACP3/Newsshare': 'acp3/module-news-share',
-        'ACP3/Modules/ACP3/Newsletter': 'acp3/module-newsletter',
-        'ACP3/Modules/ACP3/Permissions': 'acp3/module-permissions',
-        'ACP3/Modules/ACP3/Polls': 'acp3/module-polls',
-        'ACP3/Modules/ACP3/Search': 'acp3/module-search',
-        'ACP3/Modules/ACP3/Seo': 'acp3/module-seo',
-        'ACP3/Modules/ACP3/Share': 'acp3/module-social-sharing',
-        'ACP3/Modules/ACP3/System': 'acp3/module-system',
-        'ACP3/Modules/ACP3/Users': 'acp3/module-users',
-        'ACP3/Modules/ACP3/Wysiwygckeditor': 'acp3/module-wysiwyg-ckeditor',
-        'ACP3/Modules/ACP3/Wysiwygtinymce': 'acp3/module-wysiwyg-tinymce',
-    };
+    function loadComponents() {
+        const document = yaml.safeLoad(fs.readFileSync(__dirname + '/../../.gitsplit.yml', 'utf8'));
+
+        const componentPathMap = new Map();
+        const componentPaths = document.splits.map((split) => split.prefix);
+
+        for (const componentPath of componentPaths) {
+            const composerJson = require(__dirname + '/../../' + componentPath + '/composer.json');
+
+            componentPathMap.set(componentPath, composerJson.name);
+        }
+
+        return componentPathMap;
+    }
+
+    /**
+     * Returns the name of the current branch
+     *
+     * @returns {Promise<string>}
+     */
+    async function getNameOfCurrentBranch() {
+        return (await git().raw(['branch', '--show-current'])).trim();
+    }
 
     /**
      * Returns the latest tag of the current branch
      */
-    function getCurrentVersion() {
-        return git()
-            .raw(['describe', '--abbrev=0'])
-            .then((latestTagInBranch) => {
-                if (latestTagInBranch.indexOf('v') === 0) {
-                    return latestTagInBranch.substring(1).trim();
-                }
+    async function getCurrentVersion() {
+        const latestTagInBranch = await git().raw(['describe', '--abbrev=0']);
 
-                return latestTagInBranch.trim();
-            });
+        if (latestTagInBranch.indexOf('v') === 0) {
+            return latestTagInBranch.substring(1).trim();
+        }
+
+        return latestTagInBranch.trim();
     }
 
     /**
+     * Returns an Array of the changed ACP3 components between the current version and the current branches HEAD
      *
+     * @param {Map<string, string>} componentMap
      * @param {boolean} isMajorUpdate
      * @param {string} currentVersion
-     * @returns {Promise<*>}
+     * @returns {Promise<string[]>}
      */
-    async function findChangedModules(isMajorUpdate, currentVersion) {
+    async function findChangedComponents(componentMap, isMajorUpdate, currentVersion) {
         // If we are dealing with a major version, return all modules
         if (isMajorUpdate) {
-            return Object.values(modules);
+            return Array.from(componentMap.values());
         }
 
         const diffSummary = await git().diffSummary(['v' + currentVersion]);
-        const changedModules = [];
+        const changedComponents = new Set();
 
         for (const diffFile of diffSummary.files) {
-            for (const pathPrefix in modules) {
-                if (!modules.hasOwnProperty(pathPrefix)) {
-                    continue;
-                }
-
-                if (diffFile.file.indexOf(pathPrefix) === 0 && !changedModules.includes(modules[pathPrefix])) {
-                    changedModules.push(modules[pathPrefix]);
+            for (const [ componentPath, composerPackageName ] of componentMap) {
+                if (diffFile.file.indexOf(componentPath) === 0 && !changedComponents.has(composerPackageName)) {
+                    changedComponents.push(composerPackageName);
                 }
             }
         }
 
-        return changedModules;
+        return Array.from(changedComponents);
     }
 
-    function bumpVersions(changedModules, newVersion) {
-        bumpCore(changedModules, newVersion);
-        bumpModules(changedModules, newVersion);
+    /**
+     * Bumps the version number of the various ACP3 components
+     *
+     * @param {string[]} changedComponents
+     * @param {Map<string, string>} componentMap
+     * @param {string} newVersion
+     */
+    function bumpVersions(changedComponents, componentMap, newVersion) {
+        bumpCore(changedComponents, newVersion);
+        bumpComponents(changedComponents, componentMap, newVersion);
     }
 
     /**
@@ -135,7 +109,14 @@ module.exports = (gulp, plugins) => {
         throw new Error('Error: Please specify the arguments "major", "minor" or "patch"!');
     }
 
-    function bumpCore(changedModules, newVersion) {
+    /**
+     * Bumps the versions number of various files of the acp3/core component
+     *
+     * @param {string[]} changedComponents
+     * @param {string} newVersion
+     * @returns {*}
+     */
+    function bumpCore(changedComponents, newVersion) {
         gulp
             .src(
                 ['./package.json', './package-lock.json'],
@@ -146,7 +127,7 @@ module.exports = (gulp, plugins) => {
             .pipe(plugins.bump({version: newVersion}))
             .pipe(gulp.dest('./'));
 
-        if (changedModules.includes('acp3/core')) {
+        if (changedComponents.includes('acp3/core')) {
             return gulp.src(
                 './ACP3/Core/src/Application/BootstrapInterface.php',
                 {
@@ -161,22 +142,42 @@ module.exports = (gulp, plugins) => {
         }
     }
 
+    /**
+     *
+     * @param {string} str
+     * @param {string} find
+     * @param {string} replace
+     * @returns {string}
+     */
     function replaceAll(str, find, replace) {
         return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
     }
 
+    /**
+     *
+     * @param {string} str
+     * @returns {string}
+     */
     function escapeRegExp(str) {
         return str.replace(/([*?^=!:${}()|\/\\])/g, '\\$1');
     }
 
-    function bumpModules(changedModules, newVersion) {
+    /**
+     * Bumps the version number within the composer.json files of the various ACP3 components.
+     *
+     * @param {string[]} changedComponents
+     * @param {Map<string, string>} componentMap
+     * @param {string} newVersion
+     * @returns {*}
+     */
+    function bumpComponents(changedComponents, componentMap, newVersion) {
         const changedPaths = [];
-        for (const pathPrefix in modules) {
-            if (!modules.hasOwnProperty(pathPrefix) || !changedModules.includes(modules[pathPrefix])) {
+        for (const [componentPath, composerPackageName] of componentMap) {
+            if (!changedComponents.includes(composerPackageName)) {
                 continue;
             }
 
-            changedPaths.push(pathPrefix + '/composer.json');
+            changedPaths.push(componentPath + '/composer.json');
         }
 
         return gulp.src(
@@ -185,7 +186,7 @@ module.exports = (gulp, plugins) => {
                 base: './'
             }
         ).pipe(plugins.change((content) => {
-            for (const packageName of changedModules) {
+            for (const packageName of changedComponents) {
                 const search = '"' + packageName + '": "^[~0-9.]+"';
                 const replace = '"' + packageName + '": "^' + newVersion + '"';
 
@@ -196,32 +197,78 @@ module.exports = (gulp, plugins) => {
         })).pipe(gulp.dest('./'));
     }
 
-    function bumpChangelog(currentVersion, newVersion) {
+    /**
+     * Finalizes the changelog for the to be released version
+     *
+     * @param {string} nameOfCurrentBranch
+     * @param {string} currentVersion
+     * @param {string} newVersion
+     */
+    function bumpChangelog(nameOfCurrentBranch, currentVersion, newVersion) {
+        const changelogName = 'CHANGELOG-' + nameOfCurrentBranch + '.md';
+        const changelogPath = __dirname + '/../../' + changelogName;
+
+        if (!fs.existsSync(changelogPath)) {
+            throw new Error(
+                `Could not find the changelog with the name "${changelogName}". Please create it at first!`
+            );
+        }
+        if (!fs.readFileSync(changelogPath).includes('## [Unreleased]')) {
+            throw new Error(
+                `Could not find an "## [Unreleased]" section within the changelog. Please create one at first!`
+            );
+        }
+
         gulp.src(
             [
-                './CHANGELOG.md',
+                './' + changelogName,
             ]
         ).pipe(plugins.change((content) => {
             const currentDate = moment().format('YYYY-MM-DD');
             return content
                 .replace('## [Unreleased]', `## [${newVersion}] - ${currentDate}`)
                 .replace(
-                    `[Unreleased]: https://gitlab.com/ACP3/cms/compare/v${currentVersion}...5.x`,
-                    `[Unreleased]: https://gitlab.com/ACP3/cms/compare/v${newVersion}...5.x\n` +
+                    `[Unreleased]: https://gitlab.com/ACP3/cms/compare/v${currentVersion}...${nameOfCurrentBranch}`,
+                    `[Unreleased]: https://gitlab.com/ACP3/cms/compare/v${newVersion}...${nameOfCurrentBranch}\n` +
                     `[${newVersion}]: https://gitlab.com/ACP3/cms/compare/v${currentVersion}...v${newVersion}`
                 );
         })).pipe(gulp.dest('./'));
     }
 
+    /**
+     * Check, whether the version bumping can take place at all
+     *
+     * @param {string} nameOfCurrentBranch
+     * @param {boolean} isMajorUpdate
+     */
+    function checkVersionBumpConstraints(nameOfCurrentBranch, isMajorUpdate) {
+        if (!nameOfCurrentBranch.match(/^\d+\.x$/)) {
+            throw new Error(
+                `Can't bump the version outside a version branch. Please switch to one of them!`
+            );
+        } else if (isMajorUpdate) {
+            const nextVersionBranch = Number(nameOfCurrentBranch.split('.')[0]) + 1;
+
+            throw new Error(
+                `Can't do a major version bump within branch "${nameOfCurrentBranch}". Please switch to branch "${nextVersionBranch}.x"!`
+            );
+        }
+    }
+
     return async () => {
         try {
+            const nameOfCurrentBranch = await getNameOfCurrentBranch();
             const currentVersion = await getCurrentVersion();
             const newVersion = getNewVersion(argv, currentVersion);
+            const components = loadComponents();
 
-            bumpChangelog(currentVersion, newVersion);
+            checkVersionBumpConstraints(nameOfCurrentBranch, argv.major);
+
+            bumpChangelog(nameOfCurrentBranch, currentVersion, newVersion);
 
             bumpVersions(
-                await findChangedModules(argv.major, currentVersion),
+                await findChangedComponents(components, argv.major, currentVersion),
+                components,
                 newVersion
             );
         } catch (e) {
