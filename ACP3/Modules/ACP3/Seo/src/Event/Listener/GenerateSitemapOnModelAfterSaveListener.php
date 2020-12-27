06fe0f8e335e49853db75c014c520566d7aadb46
Tino Goratsch <mail@goratschwebdesign.de>
2020-12-27 23:04:40 +0100

    move rebuilding the sitemap.xml into the TerminateEvent

--- ACP3/Modules/ACP3/Seo/src/Event/Listener/GenerateSitemapOnModelAfterSaveListener.php
+++ ACP3/Modules/ACP3/Seo/src/Event/Listener/GenerateSitemapOnModelAfterSaveListener.php
@@ -15,8 +15,12 @@ use ACP3\Modules\ACP3\Seo\Installer\Schema;
 use ACP3\Modules\ACP3\Seo\Model\SitemapGenerationModel;
 use ACP3\Modules\ACP3\Seo\Utility\SitemapAvailabilityRegistrar;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
-class GenerateSitemapOnModelAfterSaveListener
+class GenerateSitemapOnModelAfterSaveListener implements EventSubscriberInterface
 {
     /**
      * @var LoggerInterface
@@ -38,56 +42,79 @@ class GenerateSitemapOnModelAfterSaveListener
      * @var \ACP3\Core\Modules
      */
     private $modules;
-
     /**
-     * GenerateSitemapOnModelAfterSaveListener constructor.
+     * @var \Symfony\Component\HttpFoundation\RequestStack
      */
+    private $requestStack;
+    /**
+     * @var bool
+     */
+    private $scheduleSitemapRebuild = false;
+
     public function __construct(
         LoggerInterface $logger,
         Modules $modules,
         SettingsInterface $settings,
         SitemapAvailabilityRegistrar $sitemapRegistrar,
-        SitemapGenerationModel $sitemapGenerationModel
+        SitemapGenerationModel $sitemapGenerationModel,
+        RequestStack $requestStack
     ) {
         $this->logger = $logger;
         $this->settings = $settings;
         $this->sitemapRegistrar = $sitemapRegistrar;
         $this->sitemapGenerationModel = $sitemapGenerationModel;
         $this->modules = $modules;
+        $this->requestStack = $requestStack;
     }
 
-    public function __invoke(ModelSaveEvent $event)
+    public function onModelAfterSave(ModelSaveEvent $event): void
     {
         if (!$this->modules->isActive(Schema::MODULE_NAME)) {
             return;
         }
 
         if ($this->canGenerateSitemapAutomatically() && $this->isAllowedModule($event->getModuleName())) {
-            try {
-                $this->sitemapGenerationModel->save();
-            } catch (SitemapGenerationException $e) {
-                $this->logger->warning($e->getMessage());
-            }
+            $this->scheduleSitemapRebuild = true;
         }
     }
 
-    /**
-     * @return bool
-     */
-    private function canGenerateSitemapAutomatically()
+    private function canGenerateSitemapAutomatically(): bool
     {
         $seoSettings = $this->settings->getSettings(Schema::MODULE_NAME);
 
         return $seoSettings['sitemap_is_enabled'] == 1 && $seoSettings['sitemap_save_mode'] == 1;
     }
 
-    /**
-     * @param string $moduleName
-     *
-     * @return bool
-     */
-    private function isAllowedModule($moduleName)
+    private function isAllowedModule(string $moduleName): bool
     {
         return \array_key_exists($moduleName, $this->sitemapRegistrar->getAvailableModules());
+    }
+
+    public function onKernelTerminate(TerminateEvent $event): void
+    {
+        if (!$this->scheduleSitemapRebuild) {
+            return;
+        }
+
+        $this->requestStack->push($event->getRequest());
+
+        try {
+            $this->sitemapGenerationModel->save();
+        } catch (SitemapGenerationException $e) {
+            $this->logger->warning($e->getMessage());
+        }
+
+        $this->requestStack->pop();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'core.model.after_save' => ['onModelAfterSave', -255],
+            KernelEvents::TERMINATE => 'onKernelTerminate',
+        ];
     }
 }
