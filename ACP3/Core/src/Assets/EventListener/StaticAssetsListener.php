67b6d056e6f81e76ab6e8e477e76c30f309bd558
Tino Goratsch <mail@goratschwebdesign.de>
2020-12-31 01:24:52 +0100

    move injecting the CSS and JS assets to the very end of the output pipeline
    Although this bypasses the HTTP cache, it doesn't seem to hurt the performance. :)

--- ACP3/Core/src/Assets/EventListener/StaticAssetsListener.php
+++ ACP3/Core/src/Assets/EventListener/StaticAssetsListener.php
@@ -7,15 +7,47 @@
 
 namespace ACP3\Core\Assets\EventListener;
 
-use ACP3\Core\View\Renderer\Smarty\Filters\MoveToBottom;
+use ACP3\Core\Assets\Minifier\CSS;
+use ACP3\Core\Assets\Minifier\DeferrableCSS;
+use ACP3\Core\Assets\Minifier\JavaScript;
 use FOS\HttpCache\SymfonyCache\CacheEvent;
 use FOS\HttpCache\SymfonyCache\Events;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class StaticAssetsListener implements EventSubscriberInterface
 {
-    private const JAVASCRIPTS_REGEX_PATTERN = MoveToBottom::ELEMENT_CATCHER_REGEX_PATTERN;
-    private const PLACEHOLDER = '</body>';
+    private const REGEX_PATTERN_CSS = '!@@@SMARTY:STYLESHEETS:BEGIN@@@(.*?)@@@SMARTY:STYLESHEETS:END@@@!is';
+    private const REGEX_PATTERN_JS = '!@@@SMARTY:JAVASCRIPTS:BEGIN@@@(.*?)@@@SMARTY:JAVASCRIPTS:END@@@!is';
+    private const PLACEHOLDER_CSS = '<!-- STYLESHEETS -->';
+    private const PLACEHOLDER_JS = '<!-- JAVASCRIPTS -->';
+
+    /**
+     * @var \ACP3\Core\Assets\Minifier\CSS
+     */
+    private $cssMinifier;
+    /**
+     * @var \ACP3\Core\Assets\Minifier\DeferrableCSS
+     */
+    private $deferrableCssMinifier;
+    /**
+     * @var \ACP3\Core\Assets\Minifier\JavaScript
+     */
+    private $javaScriptMinifier;
+    /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(CSS $cssMinifier, DeferrableCSS $deferrableCssMinifier, JavaScript $javaScriptMinifier, RequestStack $requestStack)
+    {
+        $this->cssMinifier = $cssMinifier;
+        $this->deferrableCssMinifier = $deferrableCssMinifier;
+        $this->javaScriptMinifier = $javaScriptMinifier;
+        $this->requestStack = $requestStack;
+    }
 
     /**
      * {@inheritdoc}
@@ -27,7 +59,25 @@ class StaticAssetsListener implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * @throws \MJS\TopSort\CircularDependencyException
+     * @throws \MJS\TopSort\ElementNotFoundException
+     */
     public function postHandle(CacheEvent $event)
+    {
+        if ($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
+            return;
+        }
+
+        $this->moveCssBlocksToHead($event);
+        $this->moveJavaScriptBlocksToBodyEnd($event);
+    }
+
+    /**
+     * @throws \MJS\TopSort\CircularDependencyException
+     * @throws \MJS\TopSort\ElementNotFoundException
+     */
+    private function moveCssBlocksToHead(CacheEvent $event): void
     {
         $response = $event->getResponse();
 
@@ -36,28 +86,89 @@ class StaticAssetsListener implements EventSubscriberInterface
         }
 
         $content = $response->getContent();
-        if (\strpos($content, static::PLACEHOLDER) !== false) {
+
+        if (\strpos($content, self::PLACEHOLDER_CSS) !== false) {
+            $this->requestStack->push($event->getRequest());
+
             $content = \str_replace(
-                static::PLACEHOLDER,
-                $this->addElementsFromTemplates($content) . "\n" . static::PLACEHOLDER,
-                $this->getCleanedUpTemplateOutput($content)
+                self::PLACEHOLDER_CSS,
+                $this->addCssLibraries() . $this->addElementsFromTemplates($content, self::REGEX_PATTERN_CSS),
+                $this->getCleanedUpTemplateOutput($content, self::REGEX_PATTERN_CSS)
             );
 
             $response->setContent($content);
             $response->headers->set('Content-Length', \strlen($content));
+
+            $this->requestStack->pop();
         }
     }
 
-    private function getCleanedUpTemplateOutput(string $tplOutput): string
+    /**
+     * @throws \MJS\TopSort\CircularDependencyException
+     * @throws \MJS\TopSort\ElementNotFoundException
+     */
+    private function moveJavaScriptBlocksToBodyEnd(CacheEvent $event): void
     {
-        return \preg_replace(static::JAVASCRIPTS_REGEX_PATTERN, '', $tplOutput);
+        $response = $event->getResponse();
+
+        if (!$response) {
+            return;
+        }
+
+        $content = $response->getContent();
+
+        if (\strpos($content, self::PLACEHOLDER_JS) !== false) {
+            $this->requestStack->push($event->getRequest());
+
+            $content = \str_replace(
+                self::PLACEHOLDER_JS,
+                $this->addJavaScriptLibraries($event->getRequest()) . $this->addElementsFromTemplates($content, self::REGEX_PATTERN_JS),
+                $this->getCleanedUpTemplateOutput($content, self::REGEX_PATTERN_JS)
+            );
+
+            $response->setContent($content);
+            $response->headers->set('Content-Length', \strlen($content));
+
+            $this->requestStack->pop();
+        }
     }
 
-    private function addElementsFromTemplates(string $tplOutput): string
+    private function getCleanedUpTemplateOutput(string $tplOutput, string $regexPattern): string
+    {
+        return \preg_replace($regexPattern, '', $tplOutput);
+    }
+
+    private function addElementsFromTemplates(string $tplOutput, string $regexPattern): string
     {
         $matches = [];
-        \preg_match_all(static::JAVASCRIPTS_REGEX_PATTERN, $tplOutput, $matches);
+        \preg_match_all($regexPattern, $tplOutput, $matches);
 
         return \implode("\n", \array_unique($matches[1])) . "\n";
+    }
+
+    /**
+     * @throws \MJS\TopSort\CircularDependencyException
+     * @throws \MJS\TopSort\ElementNotFoundException
+     */
+    private function addCssLibraries(): string
+    {
+        $deferrableCssUri = $this->deferrableCssMinifier->getURI();
+
+        return '<link rel="stylesheet" type="text/css" href="' . $this->cssMinifier->getURI() . '">' . "\n"
+            . '<link rel="stylesheet" href="' . $deferrableCssUri . '" media="print" onload="this.media=\'all\'; this.onload=null;">' . "\n"
+            . '<noscript><link rel="stylesheet" href="' . $deferrableCssUri . '"></noscript>' . "\n";
+    }
+
+    /**
+     * @throws \MJS\TopSort\CircularDependencyException
+     * @throws \MJS\TopSort\ElementNotFoundException
+     */
+    private function addJavaScriptLibraries(Request $request): string
+    {
+        if ($request->isXmlHttpRequest() === true) {
+            return '';
+        }
+
+        return "<script defer src=\"{$this->javaScriptMinifier->getURI()}\"></script>\n";
     }
 }
