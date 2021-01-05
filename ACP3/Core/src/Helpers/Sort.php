5fa3fd5cfa6ab457f3dc3d8155536f8305bb21b2
Tino Goratsch <mail@goratschwebdesign.de>
2021-01-05 15:28:25 +0100

    optimize the SortHelper

--- ACP3/Core/src/Helpers/Sort.php
+++ ACP3/Core/src/Helpers/Sort.php
@@ -55,27 +55,26 @@ class Sort
         try {
             $this->db->getConnection()->beginTransaction();
 
-            $table = $this->db->getPrefix() . $table;
+            $table = $this->db->getPrefixedTableName($table);
 
             // Zusätzliche WHERE-Bedingung
             $where = !empty($where) ? 'a.' . $where . ' = b.' . $where . ' AND ' : '';
 
             // Aktuelles Element und das vorherige Element selektieren
-            $query = 'SELECT a.%2$s AS other_id, a.%3$s AS other_sort, b.%3$s AS elem_sort FROM %1$s AS a, %1$s AS b WHERE %4$sb.%2$s = :id AND a.%3$s %5$s b.%3$s ORDER BY a.%3$s %6$s LIMIT 1';
+            $query = 'SELECT a.%2$s AS other_id FROM %1$s AS a, %1$s AS b WHERE %4$sb.%2$s = :id AND a.%3$s %5$s b.%3$s ORDER BY a.%3$s %6$s LIMIT 1';
 
             if ($action === 'up') {
-                $result = $this->db->getConnection()->fetchAssoc(\sprintf($query, $table, $idField, $sortField, $where, '<', 'DESC'), ['id' => $id]);
+                $otherId = $this->db->getConnection()->fetchColumn(\sprintf($query, $table, $idField, $sortField, $where, '<', 'DESC'), ['id' => $id]);
             } else {
-                $result = $this->db->getConnection()->fetchAssoc(\sprintf($query, $table, $idField, $sortField, $where, '>', 'ASC'), ['id' => $id]);
+                $otherId = $this->db->getConnection()->fetchColumn(\sprintf($query, $table, $idField, $sortField, $where, '>', 'ASC'), ['id' => $id]);
             }
 
-            if (!empty($result)) {
-                // Sortierreihenfolge des aktuellen Elementes zunächst auf 0 setzen
-                // um Probleme mit möglichen Duplicate-Keys zu umgehen
-                $this->db->getConnection()->update($table, [$sortField => 0], [$idField => $id]);
-                $this->db->getConnection()->update($table, [$sortField => $result['elem_sort']], [$idField => $result['other_id']]);
-                // Element nun den richtigen Wert zuweisen
-                $this->db->getConnection()->update($table, [$sortField => $result['other_sort']], [$idField => $id]);
+            if ($otherId !== null) {
+                $swapSortSql = 'UPDATE %1$s t1 INNER JOIN %1$s t2 ON (t1.%2$s, t2.%2$s) IN ((:id, :otherId),(:otherId,:id)) SET t1.%3$s = t2.%3$s';
+
+                $this->db->getConnection()->executeUpdate(
+                    \sprintf($swapSortSql, $table, $idField, $sortField), ['id' => $id, 'otherId' => $otherId]
+                );
 
                 $this->db->getConnection()->commit();
 
