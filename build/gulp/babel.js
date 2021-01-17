630c8ea695f53f7af15576b6360844fa83418f4a
Tino Goratsch <mail@goratschwebdesign.de>
2021-01-17 16:58:23 +0100

    introduce browserify to the gulp babel task

--- build/gulp/babel.js
+++ build/gulp/babel.js
@@ -8,8 +8,11 @@ module.exports = (gulp) => {
 
     const plumber = require('gulp-plumber');
     const rename = require('gulp-rename');
-    const babel = require("gulp-babel");
     const componentPaths = require('./component-paths');
+    const browserify = require('browserify');
+    const buffer = require('gulp-buffer');
+    const sourcemaps = require('gulp-sourcemaps');
+    const tap = require('gulp-tap');
 
     return () => {
         return gulp
@@ -19,13 +22,22 @@ module.exports = (gulp) => {
                     './designs/*/*/Assets/js/{admin,frontend,partials,widget}/!(*.min).js',
                     './designs/*/Assets/js/!(*.min).js',
                 ],
-                {base: './', since: gulp.lastRun('babel'), sourcemaps: true}
+                {base: './', read: false, since: gulp.lastRun('babel')}
             )
             .pipe(plumber())
-            .pipe(babel())
+            .pipe(tap(function (file) {
+                // replace file contents with browserify's bundle stream
+                file.contents = browserify(file.path, {ignoreMissing: true})
+                    .plugin('tinyify')
+                    .transform('babelify')
+                    .bundle();
+            }))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
             .pipe(rename((path) => {
                 path.basename += '.min';
             }))
-            .pipe(gulp.dest('./', { sourcemaps: '.' }));
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./'));
     };
 };
