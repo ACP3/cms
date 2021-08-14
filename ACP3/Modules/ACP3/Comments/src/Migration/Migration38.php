15ba2e23425b1a3c64118dc5d03e7dab7f606dfc
tinogo <mail@goratschwebdesign.de>
2021-08-14 13:30:58 +0200

    rework the migrations of the comments module

---
+++ ACP3/Modules/ACP3/Comments/src/Migration/Migration38.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENSE file at the top-level module directory for licensing details.
+ */
+
+namespace ACP3\Modules\ACP3\Comments\Migration;
+
+use ACP3\Core\Database\Connection;
+use ACP3\Core\Migration\MigrationInterface;
+
+class Migration38 implements MigrationInterface
+{
+    /**
+     * @var Connection
+     */
+    private $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getSchemaVersion(): int
+    {
+        return 38;
+    }
+
+    public function up(): void
+    {
+        $this->db->executeQuery("DELETE FROM `{$this->db->getPrefix()}settings` WHERE `module_id` = (select id from `{$this->db->getPrefix()}modules` WHERE name = 'comments') AND `name` = 'emoticons';");
+    }
+
+    public function down(): void
+    {
+    }
+}
