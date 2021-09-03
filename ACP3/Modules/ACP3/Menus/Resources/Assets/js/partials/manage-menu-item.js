c8ecd5449bc8a1390467020cbb069c4d58890562
tinogo <mail@goratschwebdesign.de>
2021-09-04 00:57:30 +0200

    fix a possible JS error

--- ACP3/Modules/ACP3/Menus/Resources/Assets/js/partials/manage-menu-item.js
+++ ACP3/Modules/ACP3/Menus/Resources/Assets/js/partials/manage-menu-item.js
@@ -4,14 +4,17 @@
   const blockIdFormField = document.getElementById("block-id");
 
   const createMenuItemFormField = document.querySelector('[name="create_menu_item"]');
-  createMenuItemFormField.addEventListener("click", (event) => {
-    if (event.detail?.init === true) {
-      event.preventDefault();
-    }
 
-    manageMenuItemContainer.classList.toggle("d-none", !createMenuItemFormField.checked);
-  });
-  createMenuItemFormField.dispatchEvent(new CustomEvent("click", { detail: { init: true } }));
+  if (createMenuItemFormField) {
+    createMenuItemFormField.addEventListener("click", (event) => {
+      if (event.detail?.init === true) {
+        event.preventDefault();
+      }
+
+      manageMenuItemContainer.classList.toggle("d-none", !createMenuItemFormField.checked);
+    });
+    createMenuItemFormField.dispatchEvent(new CustomEvent("click", { detail: { init: true } }));
+  }
 
   blockIdFormField.addEventListener("change", () => {
     const blockName = blockIdFormField.options.item(blockIdFormField.selectedIndex).text;
