4939c5aa4cfa902b36b3060f83e1f226150114fe
tinogo <mail@goratschwebdesign.de>
2022-01-03 03:40:50 +0100

    run rector in parallel mode

--- rector.php
+++ rector.php
@@ -8,7 +8,22 @@ declare(strict_types = 1);
  */
 
 use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\ArrayDimFetch\CurlyToSquareBracketArrayStringRector;
+use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php74\Rector\Double\RealToFloatTypeCastRector;
+use Rector\Php74\Rector\FuncCall\ArrayKeyExistsOnPropertyRector;
+use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\Php74\Rector\FuncCall\FilterVarToAddSlashesRector;
+use Rector\Php74\Rector\FuncCall\GetCalledClassToStaticClassRector;
+use Rector\Php74\Rector\FuncCall\MbStrrposEncodingArgumentPositionRector;
+use Rector\Php74\Rector\Function_\ReservedFnFunctionRector;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\Php74\Rector\MethodCall\ChangeReflectionTypeToStringToGetNameRector;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Php74\Rector\StaticCall\ExportToReflectionFunctionRector;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -18,6 +33,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [
         __DIR__ . '/ACP3',
     ]);
+    $parameters->set(Option::PARALLEL, true);
 
     // Define what rule sets will be applied
     $containerConfigurator->import(SetList::DEAD_CODE);
@@ -25,28 +41,28 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::PHP_80);
 
     $services = $containerConfigurator->services();
-    $services->set(\Rector\Renaming\Rector\FuncCall\RenameFunctionRector::class)->call('configure', [[\Rector\Renaming\Rector\FuncCall\RenameFunctionRector::OLD_FUNCTION_TO_NEW_FUNCTION => [
+    $services->set(RenameFunctionRector::class)->call('configure', [[
         //the_real_type
         // https://wiki.php.net/rfc/deprecations_php_7_4
         'is_real' => 'is_float',
         //apache_request_headers_function
         // https://wiki.php.net/rfc/deprecations_php_7_4
         'apache_request_headers' => 'getallheaders',
-    ]]]);
-    $services->set(\Rector\Php74\Rector\FuncCall\ArrayKeyExistsOnPropertyRector::class);
-    $services->set(\Rector\Php74\Rector\FuncCall\FilterVarToAddSlashesRector::class);
-    $services->set(\Rector\Php74\Rector\StaticCall\ExportToReflectionFunctionRector::class);
-    $services->set(\Rector\Php74\Rector\FuncCall\GetCalledClassToStaticClassRector::class);
-    $services->set(\Rector\Php74\Rector\FuncCall\MbStrrposEncodingArgumentPositionRector::class);
-    $services->set(\Rector\Php74\Rector\Double\RealToFloatTypeCastRector::class);
-    $services->set(\Rector\Php74\Rector\Assign\NullCoalescingOperatorRector::class);
-    $services->set(\Rector\Php74\Rector\Function_\ReservedFnFunctionRector::class);
-    $services->set(\Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class);
-    $services->set(\Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector::class);
-    $services->set(\Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class);
-    $services->set(\Rector\Php74\Rector\MethodCall\ChangeReflectionTypeToStringToGetNameRector::class);
-    $services->set(\Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector::class);
-    $services->set(\Rector\Php74\Rector\ArrayDimFetch\CurlyToSquareBracketArrayStringRector::class);
+    ]]);
+    $services->set(ArrayKeyExistsOnPropertyRector::class);
+    $services->set(FilterVarToAddSlashesRector::class);
+    $services->set(ExportToReflectionFunctionRector::class);
+    $services->set(GetCalledClassToStaticClassRector::class);
+    $services->set(MbStrrposEncodingArgumentPositionRector::class);
+    $services->set(RealToFloatTypeCastRector::class);
+    $services->set(NullCoalescingOperatorRector::class);
+    $services->set(ReservedFnFunctionRector::class);
+    $services->set(ClosureToArrowFunctionRector::class);
+    $services->set(ArraySpreadInsteadOfArrayMergeRector::class);
+    $services->set(AddLiteralSeparatorToNumberRector::class);
+    $services->set(ChangeReflectionTypeToStringToGetNameRector::class);
+    $services->set(RestoreDefaultNullToNullableTypePropertyRector::class);
+    $services->set(CurlyToSquareBracketArrayStringRector::class);
 
     // get services (needed for register a single rule)
     // $services = $containerConfigurator->services();
