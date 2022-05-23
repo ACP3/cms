ab91e9cf95fc96b777c6fd7bd3ade2ee0901095a
Tino Goratsch <mail@goratschwebdesign.de>
2022-05-23 18:22:03 +0200

    bump the version to 6.0.4

--- ACP3/Core/src/Application/BootstrapInterface.php
+++ ACP3/Core/src/Application/BootstrapInterface.php
@@ -16,7 +16,7 @@ interface BootstrapInterface extends HttpKernelInterface
     /**
      * Contains the current ACP3 version string.
      */
-    public const VERSION = '6.0.3';
+    public const VERSION = '6.0.4';
 
     /**
      * Checks whether the ACP3 is correctly installed.
