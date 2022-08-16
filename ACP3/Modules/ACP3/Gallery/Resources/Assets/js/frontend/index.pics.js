94b51b76fc9c72cc0c61807e3ae3b30926765b11
Tino Goratsch <mail@goratschwebdesign.de>
2022-08-16 20:34:43 +0200

    add back the possibility to state captions for photoswipe again

--- ACP3/Modules/ACP3/Gallery/Resources/Assets/js/frontend/index.pics.js
+++ ACP3/Modules/ACP3/Gallery/Resources/Assets/js/frontend/index.pics.js
@@ -5,5 +5,29 @@
     children: ".gallery-picture-thumb",
     pswpModule: PhotoSwipe,
   });
+  lightbox.on("uiRegister", () => {
+    lightbox.pswp.ui.registerElement({
+      name: "caption",
+      order: 9,
+      isButton: false,
+      appendTo: "root",
+      html: "",
+      onInit: (el) => {
+        lightbox.pswp.on("change", () => {
+          const currSlideElement = lightbox.pswp.currSlide.data.element;
+          const captionHTML = currSlideElement ? currSlideElement.querySelector("img").getAttribute("alt") : "";
+
+          const captionInner = document.createElement("div");
+
+          if (captionHTML) {
+            captionInner.classList.add("pswp__caption-inner");
+            captionInner.innerHTML = captionHTML || "";
+          }
+
+          el.replaceChildren(captionInner);
+        });
+      },
+    });
+  });
   lightbox.init();
 })();
