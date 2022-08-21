578df29976b7360adf4205315f3987ee54bd0067
Tino Goratsch <mail@goratschwebdesign.de>
2022-08-21 14:02:38 +0200

    remove some obsolete code from the MenuService

--- ACP3/Modules/ACP3/Menus/src/Services/MenuService.php
+++ ACP3/Modules/ACP3/Menus/src/Services/MenuService.php
@@ -7,13 +7,11 @@
 
 namespace ACP3\Modules\ACP3\Menus\Services;
 
-use ACP3\Core\I18n\Translator;
-use ACP3\Modules\ACP3\Menus\Enum\PageTypeEnum;
 use ACP3\Modules\ACP3\Menus\Repository\MenuItemRepository;
 
 class MenuService implements MenuServiceInterface
 {
-    public function __construct(private readonly Translator $translator, private readonly MenuItemRepository $menuItemRepository)
+    public function __construct(private readonly MenuItemRepository $menuItemRepository)
     {
     }
 
@@ -24,57 +22,7 @@ class MenuService implements MenuServiceInterface
      */
     public function getAllMenuItems(): array
     {
-        $menuItems = $this->menuItemRepository->getAllMenuItems();
-
-        $modeSearch = PageTypeEnum::values();
-        $modeReplace = [
-            $this->translator->t('menus', 'module'),
-            $this->translator->t('menus', 'dynamic_page'),
-            $this->translator->t('menus', 'hyperlink'),
-        ];
-
-        foreach ($menuItems as $i => $menuItem) {
-            $menuItems[$i]['mode_formatted'] = str_replace($modeSearch, $modeReplace, (string) $menuItem['mode']);
-            $menuItems[$i]['first'] = $this->isFirstItemInSet($i, $menuItems);
-            $menuItems[$i]['last'] = $this->isLastItemInSet($i, $menuItems);
-        }
-
-        return $menuItems;
-    }
-
-    /**
-     * @param array<array<string, mixed>> $menuItems
-     */
-    private function isFirstItemInSet(int $index, array $menuItems): bool
-    {
-        if ($index > 0) {
-            for ($j = $index - 1; $j >= 0; --$j) {
-                if ($menuItems[$j]['parent_id'] === $menuItems[$index]['parent_id']
-                    && $menuItems[$j]['block_name'] === $menuItems[$index]['block_name']
-                ) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * @param array<array<string, mixed>> $menuItems
-     */
-    private function isLastItemInSet(int $index, array $menuItems): bool
-    {
-        $cItems = \count($menuItems);
-        for ($j = $index + 1; $j < $cItems; ++$j) {
-            if ($menuItems[$index]['parent_id'] === $menuItems[$j]['parent_id']
-                && $menuItems[$j]['block_name'] === $menuItems[$index]['block_name']
-            ) {
-                return false;
-            }
-        }
-
-        return true;
+        return $this->menuItemRepository->getAllMenuItems();
     }
 
     /**
