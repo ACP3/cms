b7aab46ffcd9f43cdd45ec6efb8f36dd17ac1f5d
Tino Goratsch <mail@goratschwebdesign.de>
2022-08-21 13:53:45 +0200

    cover the MenuItemsList class with unit tests + simplify the code a bit

--- ACP3/Modules/ACP3/Menus/src/Helpers/MenuItemsList.php
+++ ACP3/Modules/ACP3/Menus/src/Helpers/MenuItemsList.php
@@ -7,7 +7,7 @@
 
 namespace ACP3\Modules\ACP3\Menus\Helpers;
 
-use ACP3\Core;
+use ACP3\Core\Helpers\Forms;
 use ACP3\Modules\ACP3\Menus\Services\MenuServiceInterface;
 
 class MenuItemsList
@@ -17,18 +17,17 @@ class MenuItemsList
      */
     private ?array $menuItems = null;
 
-    public function __construct(private readonly Core\Helpers\Forms $formsHelper, private readonly MenuServiceInterface $menuService)
+    public function __construct(private readonly Forms $formsHelper, private readonly MenuServiceInterface $menuService)
     {
     }
 
     /**
      * List all available menu items.
      *
-     * @return array<string, array<string, mixed>>
+     * @return array<string, array{title: string, menu_id: int, items: array<array<string, mixed>>}>
      */
     public function menuItemsList(int $parentId = 0, int $leftId = 0, int $rightId = 0): array
     {
-        // Menüpunkte einbinden
         if ($this->menuItems === null) {
             $this->menuItems = $this->menuService->getAllMenuItems();
         }
@@ -36,15 +35,18 @@ class MenuItemsList
         $output = [];
 
         foreach ($this->menuItems as $row) {
-            if (!($row['left_id'] >= $leftId && $row['right_id'] <= $rightId)) {
-                $row['selected'] = $this->formsHelper->selectEntry('parent_id', $row['id'], $parentId);
-                $row['spaces'] = str_repeat('&nbsp;&nbsp;', $row['level']);
-
-                // Titel für den aktuellen Block setzen
-                $output[$row['block_name']]['title'] = $row['block_title'];
-                $output[$row['block_name']]['menu_id'] = $row['block_id'];
-                $output[$row['block_name']]['items'][] = $row;
+            // A menu item shouldn't be allowed to be a descendant of its own descendants, therefore we filter them out.
+            if ($row['left_id'] >= $leftId && $row['right_id'] <= $rightId) {
+                continue;
             }
+
+            $row['selected'] = $this->formsHelper->selectEntry('parent_id', $row['id'], $parentId);
+            $row['spaces'] = str_repeat('&nbsp;&nbsp;', $row['level']);
+
+            // Titel für den aktuellen Block setzen
+            $output[$row['block_name']]['title'] = $row['block_title'];
+            $output[$row['block_name']]['menu_id'] = $row['block_id'];
+            $output[$row['block_name']]['items'][] = $row;
         }
 
         return $output;
---
+++ ACP3/Modules/ACP3/Menus/tests/Helpers/MenuItemsListTest.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENSE file at the top-level module directory for licensing details.
+ */
+
+namespace ACP3\Modules\ACP3\Menus\Helpers;
+
+use ACP3\Core\Helpers\Forms;
+use ACP3\Modules\ACP3\Menus\Services\MenuServiceInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class MenuItemsListTest extends TestCase
+{
+    private MenuItemsList $menuItemsList;
+
+    private Forms&MockObject $formsHelper;
+
+    private MenuServiceInterface&MockObject $menuService;
+
+    /**
+     * @return array<string, array<mixed>>
+     */
+    public function menuItemsListProvider(): array
+    {
+        $menuItemList = [
+            [
+                'id' => 1,
+                'level' => 0,
+                'parent_id' => null,
+                'left_id' => 1,
+                'right_id' => 2,
+                'block_name' => 'foo-block',
+                'block_title' => 'Foo-Block',
+                'block_id' => 10,
+            ],
+            [
+                'id' => 2,
+                'level' => 0,
+                'parent_id' => null,
+                'left_id' => 3,
+                'right_id' => 6,
+                'block_name' => 'foo-block',
+                'block_title' => 'Foo-Block',
+                'block_id' => 10,
+            ],
+            [
+                'id' => 3,
+                'level' => 1,
+                'parent_id' => 2,
+                'left_id' => 4,
+                'right_id' => 5,
+                'block_name' => 'foo-block',
+                'block_title' => 'Foo-Block',
+                'block_id' => 10,
+            ],
+        ];
+
+        return [
+            'entry-without-superior-page' => [
+                0,
+                0,
+                0,
+                $menuItemList,
+                [
+                    'foo-block' => [
+                        'title' => 'Foo-Block',
+                        'menu_id' => 10,
+                        'items' => [
+                            [
+                                'id' => 1,
+                                'level' => 0,
+                                'parent_id' => null,
+                                'left_id' => 1,
+                                'right_id' => 2,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '',
+                                'selected' => '',
+                            ],
+                            [
+                                'id' => 2,
+                                'level' => 0,
+                                'parent_id' => null,
+                                'left_id' => 3,
+                                'right_id' => 6,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '',
+                                'selected' => '',
+                            ],
+                            [
+                                'id' => 3,
+                                'level' => 1,
+                                'parent_id' => 2,
+                                'left_id' => 4,
+                                'right_id' => 5,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '&nbsp;&nbsp;',
+                                'selected' => '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'entry-with-superior-page' => [
+                2,
+                4,
+                5,
+                $menuItemList,
+                [
+                    'foo-block' => [
+                        'title' => 'Foo-Block',
+                        'menu_id' => 10,
+                        'items' => [
+                            [
+                                'id' => 1,
+                                'level' => 0,
+                                'parent_id' => null,
+                                'left_id' => 1,
+                                'right_id' => 2,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '',
+                                'selected' => '',
+                            ],
+                            [
+                                'id' => 2,
+                                'level' => 0,
+                                'parent_id' => null,
+                                'left_id' => 3,
+                                'right_id' => 6,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '',
+                                'selected' => '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'entry-with-descendant-page' => [
+                0,
+                3,
+                6,
+                $menuItemList,
+                [
+                    'foo-block' => [
+                        'title' => 'Foo-Block',
+                        'menu_id' => 10,
+                        'items' => [
+                            [
+                                'id' => 1,
+                                'level' => 0,
+                                'parent_id' => null,
+                                'left_id' => 1,
+                                'right_id' => 2,
+                                'block_name' => 'foo-block',
+                                'block_title' => 'Foo-Block',
+                                'block_id' => 10,
+                                'spaces' => '',
+                                'selected' => '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->formsHelper = $this->createMock(Forms::class);
+        $this->menuService = $this->createMock(MenuServiceInterface::class);
+
+        $this->menuItemsList = new MenuItemsList($this->formsHelper, $this->menuService);
+    }
+
+    /**
+     * @dataProvider menuItemsListProvider
+     *
+     * @param array<array<string, mixed>>                                                           $menuItemList
+     * @param array<string, array{title: string, menu_id: int, items: array<array<string, mixed>>}> $expected
+     */
+    public function testMenuItemsList(int $parentId, int $leftId, int $rightId, array $menuItemList, array $expected): void
+    {
+        $this->formsHelper->method('selectEntry')->willReturn('');
+
+        $this->menuService->expects(self::once())
+            ->method('getAllMenuItems')
+            ->willReturn($menuItemList);
+
+        self::assertEquals($expected, $this->menuItemsList->menuItemsList($parentId, $leftId, $rightId));
+    }
+}
