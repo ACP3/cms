bf7994662bb1f2b721b122dc23ba924c81bab2f2
Tino Goratsch <mail@goratschwebdesign.de>
2022-08-30 23:02:22 +0200

    fix the gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@
  * See the LICENSE file at the top-level module directory for licencing details.
  */
 
+const gulp = require("gulp");
+const fs = require("fs");
+const path = require("path");
+
 (() => {
   "use strict";
-
-  const gulp = require("gulp");
-  const fs = require("fs");
-  const path = require("path");
 
   function getTask(task) {
     return require("./build/gulp/" + task)(gulp);
@@ -27,12 +27,12 @@
   gulp.task("copy-assets", gulp.series("clean", getTask("copy-assets")));
   gulp.task("babel", getTask("webpack")); // @deprecated since ACP3 version 6.7.0, to be removed with version 7.0.0. Use `gulp webpack` instead.
   gulp.task("lint", gulp.parallel("stylelint", "eslint"));
-  gulp.task("default", gulp.series("copy-assets", "lint", gulp.parallel("scss", "webpack")));
+  gulp.task("default", gulp.series(gulp.parallel("copy-assets", "lint"), gulp.parallel("scss", "webpack")));
 
   gulp.task("watch", (done) => {
     // This environment variable instructs the webpack gulp task to run in watch mode
     process.env.GULP_MODE = "watch";
 
-    return gulp.series("default", getTask("watch"))(done);
+    gulp.series(gulp.parallel("copy-assets", "lint"), gulp.parallel("scss", "webpack", getTask("watch")))(done);
   });
 })();
