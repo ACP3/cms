1575f6e5d055ab4d3647bf6b312f13e2a338c68f
Tino Goratsch <mail@goratschwebdesign.de>
2023-02-03 23:07:55 +0100

    add a json_encode smarty modifier to fix a deprecation notice

---
+++ ACP3/Core/src/View/Renderer/Smarty/Modifiers/JsonEncode.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Copyright (c) by the ACP3 Developers.
+ * See the LICENSE file at the top-level module directory for licensing details.
+ */
+
+namespace ACP3\Core\View\Renderer\Smarty\Modifiers;
+
+use ACP3\Core\Helpers\StringFormatter;
+
+class JsonEncode extends AbstractModifier
+{
+    public function __construct(protected StringFormatter $stringFormatter)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(mixed $value): string
+    {
+        return json_encode($value, JSON_THROW_ON_ERROR);
+    }
+}
