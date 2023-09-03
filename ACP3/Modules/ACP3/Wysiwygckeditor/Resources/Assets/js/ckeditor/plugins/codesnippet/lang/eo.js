ff523faf2fa9fb55a6adb3a59fba6d697f6c26d2
Tino Goratsch <mail@goratschwebdesign.de>
2023-09-03 13:57:21 +0200

    update the npm dependencies

--- ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor/plugins/codesnippet/lang/eo.js
+++ ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor/plugins/codesnippet/lang/eo.js
@@ -1,5 +1,5 @@
 /*
  Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
 */
 CKEDITOR.plugins.setLang("codesnippet","eo",{button:"Enmeti kodaĵeron",codeContents:"Kodenhavo",emptySnippetError:"Kodaĵero ne povas esti malplena.",language:"Lingvo",title:"Kodaĵero",pathName:"kodaĵero"});
