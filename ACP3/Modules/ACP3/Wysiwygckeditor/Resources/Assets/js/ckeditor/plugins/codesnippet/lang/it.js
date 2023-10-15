fcbf47ae56f13538145ab1d0d0bab53447440738
Tino Goratsch <mail@goratschwebdesign.de>
2023-10-15 17:53:15 +0200

    constrain CKEditor version to 4.22.x
    This has been done, as CKEditor 4.23.x will need a paid license to work properly.

--- ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor/plugins/codesnippet/lang/it.js
+++ ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor/plugins/codesnippet/lang/it.js
@@ -1,5 +1,5 @@
 /*
  Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
- CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
+ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang("codesnippet","it",{button:"Inserisci frammento di codice",codeContents:"Contenuto del codice",emptySnippetError:"Un frammento di codice non può essere vuoto.",language:"Lingua",title:"Frammento di codice",pathName:"frammento di codice"});
