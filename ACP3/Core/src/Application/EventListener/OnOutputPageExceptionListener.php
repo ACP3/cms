d4fb3e5b1774807a15691b6fa8a52ae245746363
Tino Goratsch <mail@goratschwebdesign.de>
2024-03-13 21:25:55 +0100

    feat: Do not log some exceptions when in production mode

--- ACP3/Core/src/Application/EventListener/OnOutputPageExceptionListener.php
+++ ACP3/Core/src/Application/EventListener/OnOutputPageExceptionListener.php
@@ -8,18 +8,31 @@
 namespace ACP3\Core\Application\EventListener;
 
 use ACP3\Core\Application\Event\OutputPageExceptionEvent;
+use ACP3\Core\Controller\Exception\ForwardControllerActionAwareExceptionInterface;
+use ACP3\Core\Environment\ApplicationMode;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class OnOutputPageExceptionListener implements EventSubscriberInterface
 {
-    public function __construct(private readonly LoggerInterface $logger)
+    public function __construct(private readonly LoggerInterface $logger, private readonly ApplicationMode $applicationMode)
     {
     }
 
     public function __invoke(OutputPageExceptionEvent $event): void
     {
-        $this->logger->error($event->getThrowable());
+        $exception = $event->getThrowable();
+
+        // Handle exceptions of known application-state a little different, i.e.  add them to the debug log,
+        // as they are usually only relevant for development purposes.
+        if ($this->applicationMode !== ApplicationMode::PRODUCTION
+            && $exception instanceof ForwardControllerActionAwareExceptionInterface) {
+            $this->logger->debug($exception);
+
+            return;
+        }
+
+        $this->logger->error($exception);
     }
 
     public static function getSubscribedEvents(): array
