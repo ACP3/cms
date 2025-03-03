17600dc7635659e4ebce0447fb5a01468162da99
Tino Goratsch <mail@goratschwebdesign.de>
2025-03-03 19:08:24 +0100

    feat: get the image upload/selection via the rich-filemanager working

--- ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor5-richfilemanager/richfilemanagercommand.js
+++ ACP3/Modules/ACP3/Wysiwygckeditor/Resources/Assets/js/ckeditor5-richfilemanager/richfilemanagercommand.js
@@ -1,5 +1,7 @@
 import { Command } from "ckeditor5";
 
+let editorInstance;
+
 export default class RichFilemanagerCommand extends Command {
   /**
    * @inheritDoc
@@ -28,21 +30,33 @@ export default class RichFilemanagerCommand extends Command {
   execute() {
     const options = this.editor.config.get("richfilemanager.options") || {};
 
-    window.open(options.url, "richfilemanager-popup", "popup=true");
+    editorInstance = this.editor;
+
+    const width = screen.width * 0.7;
+    const height = screen.height * 0.7;
+    const left = (screen.width - width) / 2;
+    const top = (screen.height - height) / 2;
+    let windowOptions = "toolbar=no,status=no,resizable=yes,dependent=yes";
+    windowOptions += ",width=" + width;
+    windowOptions += ",height=" + height;
+    windowOptions += ",left=" + left;
+    windowOptions += ",top=" + top;
+
+    window.open(options.url, "richfilemanager-popup", windowOptions);
   }
 }
 
-// function insertImages(editor, urls) {
-//   const imageCommand = editor.commands.get("insertImage");
-//   // Check if inserting an image is actually possible - it might be possible to only insert a link.
-//   if (!imageCommand.isEnabled) {
-//     const notification = editor.plugins.get("Notification");
-//     const t = editor.locale.t;
-//     notification.showWarning(t("Could not insert image at the current position."), {
-//       title: t("Inserting image failed"),
-//       namespace: "richfilemanager",
-//     });
-//     return;
-//   }
-//   editor.execute("insertImage", { source: urls });
-// }
+window.SetUrl = (url) => {
+  const imageCommand = editorInstance.commands.get("insertImage");
+  // Check if inserting an image is actually possible - it might be possible to only insert a link.
+  if (!imageCommand.isEnabled) {
+    const notification = editorInstance.plugins.get("Notification");
+    const t = editorInstance.locale.t;
+    notification.showWarning(t("Could not insert image at the current position."), {
+      title: t("Inserting image failed"),
+      namespace: "richfilemanager",
+    });
+    return;
+  }
+  editorInstance.execute("insertImage", { source: url });
+};
