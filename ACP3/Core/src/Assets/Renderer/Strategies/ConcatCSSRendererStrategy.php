810f61901980c065e0bdd744ad3d762563d8b2e3
Tino Goratsch <mail@goratschwebdesign.de>
2025-06-29 20:58:08 +0200

    refactor: Fold the AbstractConcatRendererStrategy into the ConcatCSSRendererStrategy
    this has been done, as it is now the only implementation

--- ACP3/Core/src/Assets/Renderer/Strategies/ConcatCSSRendererStrategy.php
+++ ACP3/Core/src/Assets/Renderer/Strategies/ConcatCSSRendererStrategy.php
@@ -19,7 +19,7 @@ use ACP3\Core\Modules;
 use Psr\Cache\CacheItemPoolInterface;
 use tubalmartin\CssMin\Minifier;
 
-class ConcatCSSRendererStrategy extends AbstractConcatRendererStrategy implements CSSRendererStrategyInterface
+class ConcatCSSRendererStrategy implements CSSRendererStrategyInterface
 {
     protected const ASSETS_PATH_CSS = 'Assets/css';
 
@@ -31,16 +31,15 @@ class ConcatCSSRendererStrategy extends AbstractConcatRendererStrategy implement
     public function __construct(
         private readonly RequestInterface $request,
         private readonly Minifier $minifier,
-        UserModelInterface $userModel,
-        Assets $assets,
-        Assets\Libraries $libraries,
-        ApplicationPath $appPath,
-        CacheItemPoolInterface $coreCachePool,
-        Modules $modules,
-        FileResolver $fileResolver,
-        ThemePathInterface $themePath,
+        private readonly UserModelInterface $userModel,
+        private readonly Assets $assets,
+        private readonly Assets\Libraries $libraries,
+        private readonly ApplicationPath $appPath,
+        private readonly CacheItemPoolInterface $coreCachePool,
+        private readonly Modules $modules,
+        private readonly FileResolver $fileResolver,
+        private readonly ThemePathInterface $themePath,
     ) {
-        parent::__construct($request, $userModel, $assets, $libraries, $appPath, $coreCachePool, $modules, $fileResolver, $themePath);
     }
 
     protected function getAssetGroup(): string
@@ -60,6 +59,102 @@ class ConcatCSSRendererStrategy extends AbstractConcatRendererStrategy implement
     protected function getEnabledLibrariesAsString(): string
     {
         return implode(',', array_map(static fn (LibraryEntity $library) => $library->getLibraryIdentifier(), $this->getEnabledLibraries()));
+    }
+
+    protected function buildCacheId(): string
+    {
+        return 'assets_' . $this->generateFilenameHash();
+    }
+
+    /**
+     * The generated filename hash needs to take the area and the authentication status of the current user into account,
+     * as the filenames of the enabled can differ because of these settings.
+     */
+    private function generateFilenameHash(): string
+    {
+        return md5(implode(
+            '_',
+            [
+                $this->request->getArea()->value,
+                $this->userModel->isAuthenticated(),
+                $this->themePath->getCurrentTheme(),
+                $this->getEnabledLibrariesAsString(),
+                $this->getAssetGroup(),
+            ]
+        ));
+    }
+
+    public function getURI(): ?string
+    {
+        // We have to initialize the theme here,
+        // i.e., enabling the required libraries of the theme + adding theme-specific stylesheets.
+        // It has to be called before the "generateFilenameHash" method, otherwise we would get incorrect results!
+        $this->assets->initializeTheme();
+
+        $filenameHash = $this->generateFilenameHash();
+        $cacheId = 'assets-last-generated-' . $filenameHash;
+
+        $cacheItem = $this->coreCachePool->getItem($cacheId);
+        $cachedAssetPath = $cacheItem->get();
+
+        if ($cacheItem->isHit()) {
+            return $cachedAssetPath;
+        }
+
+        $lastGenerated = time(); // Assets are not cached -> set the current time as the new timestamp
+        $path = $this->buildAssetPath($filenameHash, $lastGenerated);
+
+        // Get the enabled libraries and filter out empty entries
+        $files = array_filter(
+            $this->processLibraries(),
+            static fn ($var) => !empty($var)
+        );
+
+        if (\count($files) === 0) {
+            $webRootPath = null;
+        } else {
+            $this->saveMinifiedAsset($files, $this->appPath->getUploadsDir() . $path);
+
+            $webRootPath = $this->appPath->getWebRoot() . 'uploads/' . $path;
+        }
+
+        $cacheItem->set($webRootPath);
+        $this->coreCachePool->saveDeferred($cacheItem);
+
+        return $webRootPath;
+    }
+
+    /**
+     * @param string[] $files
+     */
+    private function saveMinifiedAsset(array $files, string $path): void
+    {
+        $content = [];
+        foreach ($files as $file) {
+            $content[] = file_get_contents($file) . "\n";
+        }
+
+        if (\count($content) === 0) {
+            return;
+        }
+
+        $this->createAssetsDirectory();
+
+        // Write the contents of the file to the uploads folder
+        file_put_contents($path, $this->compress(implode("\n", $content)), LOCK_EX);
+    }
+
+    private function buildAssetPath(string $filenameHash, int $lastGenerated): string
+    {
+        return 'assets/' . $filenameHash . '-' . $lastGenerated . '.' . $this->getFileExtension();
+    }
+
+    private function createAssetsDirectory(): void
+    {
+        $concurrentDirectory = $this->appPath->getUploadsDir() . 'assets';
+        if (!is_dir($concurrentDirectory) && !mkdir($concurrentDirectory, 0755) && !is_dir($concurrentDirectory)) {
+            throw new \RuntimeException(\sprintf('Directory "%s" was not created', $concurrentDirectory));
+        }
     }
 
     /**
