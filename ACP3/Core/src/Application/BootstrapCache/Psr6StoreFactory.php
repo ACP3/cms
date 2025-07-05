6cbdd8194171be53b8741fa7ebc16738287922c4
Tino Goratsch <mail@goratschwebdesign.de>
2025-07-05 11:29:24 +0200

    refactor: Combine the Psr6StoreFactories

--- ACP3/Core/src/Application/BootstrapCache/Psr6StoreFactory.php
+++ ACP3/Core/src/Application/BootstrapCache/Psr6StoreFactory.php
@@ -7,20 +7,32 @@
 
 namespace ACP3\Core\Application\BootstrapCache;
 
+use ACP3\Core\Environment\ApplicationMode;
 use ACP3\Core\Environment\ApplicationPath;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\Store\InMemoryStore;
 use Toflar\Psr6HttpCacheStore\Psr6Store;
 
 class Psr6StoreFactory
 {
-    public function __construct(private readonly ApplicationPath $applicationPath)
+    public function __construct(private readonly ApplicationPath $applicationPath, private readonly ApplicationMode $applicationMode)
     {
     }
 
     public function __invoke(): Psr6Store
     {
+        if ($this->applicationMode === ApplicationMode::PRODUCTION) {
+            return new Psr6Store([
+                'cache_directory' => $this->applicationPath->getCacheDir() . 'http',
+                'generate_content_digests' => false,
+            ]);
+        }
+
         return new Psr6Store([
-            'cache_directory' => $this->applicationPath->getCacheDir() . 'http',
+            'cache' => new ArrayAdapter(),
             'generate_content_digests' => false,
+            'lock_factory' => new LockFactory(new InMemoryStore()),
         ]);
     }
 }
